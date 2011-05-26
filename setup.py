9e33187deb3ec43c77546e0c9b5ad5367063e43e
Łukasz Nowak <luke@nexedi.com>
2011-05-26 19:19:38 +0200

    Merge erp5.recipe.testnode into slapos.recipebox:erp5testnode
    Also drop extras because of https://bugs.launchpad.net/zc.buildout/+bug/85604

--- setup.py
+++ setup.py
@@ -12,11 +12,8 @@ for f in glob.glob(os.path.join('slapos', 'recipe', 'README.*.txt')):
   long_description += subname + '\n' + '=' * len(subname) + '\n\n' \
     + open(f).read() + '\n'
 
-extras_require = {
-  'build': [],
-  'buildcmmi': [],
-}
-
+# extras_requires are not used because of
+#   https://bugs.launchpad.net/zc.buildout/+bug/85604
 setup(name=name,
       version=version,
       description="Box full of slapos recipes.",
@@ -33,12 +30,17 @@ setup(name=name,
       install_requires=[
         'setuptools', # for namespace and internal usage
         'zc.buildout', # needed to play internally
+        'PyXML', # for full blown python interpreter
+        'lxml', # for full blown python interpreter
+        'xml_marshaller', # need to communication with slapgrid
+        'slapos.lib.recipe', # makes instantiation recipes simpler
+        'zc.recipe.egg', # allows to generate scripts
         ],
       zip_safe=True,
       entry_points={
         'zc.buildout': [
           'build = slapos.recipe.build:Script',
           'buildcmmi = slapos.recipe.build:Cmmi',
+          'erp5testnode = slapos.recipe.erp5testnode:Recipe',
       ]},
-      extras_require=extras_require,
     )
