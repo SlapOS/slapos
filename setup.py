8271f674a78194bc9d83ac118b0d3cfcf8102b35
Łukasz Nowak <luke@nexedi.com>
2011-05-26 18:42:56 +0200

    Merge slapos.recipe.build into slapos.recipebox.

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
+import glob
+import os
 
 version = '0.1'
 name = 'slapos.recipebox'
 long_description = open("README.txt").read() + "\n" + \
-    open("CHANGES.txt").read()
+    open("CHANGES.txt").read() + "\n"
+
+for f in glob.glob(os.path.join('slapos', 'recipe', 'README.*.txt')):
+  subname = os.path.basename(f)
+  long_description += subname + '\n' + '=' * len(subname) + '\n\n' \
+    + open(f).read() + '\n'
 
 setup(name=name,
       version=version,
@@ -23,4 +30,9 @@ setup(name=name,
         'zc.buildout', # needed to play internally
         ],
       zip_safe=True,
+      entry_points={
+        'zc.buildout': [
+          'build = slapos.recipe.build:Script',
+          'buildcmmi = slapos.recipe.build:Cmmi',
+      ]},
     )
