3ac4547dab58b24dc072d86183977c7cd43d2a10
Łukasz Nowak <luke@nexedi.com>
2011-05-30 14:01:50 +0200

    Moved to http://git.erp5.org/repos/slapos.recipe.build

--- setup.py
+++ setup.py
@@ -41,8 +41,6 @@ setup(name=name,
       zip_safe=True,
       entry_points={
         'zc.buildout': [
-          'build = slapos.recipe.build:Script',
-          'buildcmmi = slapos.recipe.build:Cmmi',
           'download = slapos.recipe.download:Recipe',
           'erp5 = slapos.recipe.erp5:Recipe',
           'erp5testnode = slapos.recipe.erp5testnode:Recipe',
