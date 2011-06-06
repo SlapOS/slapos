d775d8be6766fb4225336cd8a628a401f4e4084b
Sebastien Robin <seb@nexedi.com>
2011-06-07 00:00:32 +0200

    it is required to pass test_suite_title to runTestSuite

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.3-dev2'
+version = '0.3-dev3'
 name = 'slapos.cookbook'
 long_description = open("README.txt").read() + "\n" + \
     open("CHANGES.txt").read() + "\n"
