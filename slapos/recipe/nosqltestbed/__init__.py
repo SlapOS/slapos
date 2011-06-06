928b74ecf631643ed84a65e1b5872641e94a36a6
Guillaume Bottex <guillaume.bottex@nexedi.com>
2011-06-06 18:05:10 +0900

    Now specify computer_guid when requesting a server.

--- slapos/recipe/nosqltestbed/__init__.py
+++ slapos/recipe/nosqltestbed/__init__.py
@@ -58,13 +58,16 @@ class NoSQLTestBed(BaseSlapRecipe):
       kumo_cloud_config['nb_server_max'] = 3
     
     if 'nb_tester_max' not in kumo_cloud_config:
-      kumo_cloud_config['nb_tester_max'] = 3
+      kumo_cloud_config['nb_tester_max'] = 1 # 3
     
     if 'nb_thread' not in kumo_cloud_config:
-      kumo_cloud_config['nb_thread'] = 1
+      kumo_cloud_config['nb_thread'] = 32 # 1
     
     if 'nb_request' not in kumo_cloud_config:
-      kumo_cloud_config['nb_request'] = 1000
+      kumo_cloud_config['nb_request'] = 1024000 # 1000
+    
+    if 'computer_guid_list' not in kumo_cloud_config:
+      kumo_cloud_config['computer_guid_list'] = "COMP-9:COMP-10:COMP-11"
     
     kumo_cloud_config['software_release_url'] = self.software_release_url
     kumo_cloud_config['server_url'] = self.server_url
@@ -76,6 +79,7 @@ class NoSQLTestBed(BaseSlapRecipe):
 
     kumo_cloud_connection = {}
     kumo_cloud_connection['url'] = "http://["+kumo_cloud_config['address']+"]:5000/"
+    kumo_cloud_connection['computer_guid_list'] = kumo_cloud_config['computer_guid_list']
     self.computer_partition.setConnectionDict(kumo_cloud_connection)
 
     nosqltester_manager_wrapper_template_location = pkg_resources.resource_filename(
