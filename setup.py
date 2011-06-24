5d159968ac244c78518137be69d815e9793d2a34
Łukasz Nowak <luke@nexedi.com>
2011-06-24 09:20:17 +0200

    Advertise 0.5 development.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.4'
+version = '0.5-dev'
 name = 'slapos.core'
 long_description = open("README.txt").read() + "\n" + \
     open("CHANGES.txt").read() + "\n"
