051b220d8a3eea98a98b213994f26565f151ca3a
Łukasz Nowak <luke@nexedi.com>
2011-06-27 14:07:18 +0200

    Prepare release 0.8.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.8-dev'
+version = '0.8'
 name = 'slapos.core'
 long_description = open("README.txt").read() + "\n" + \
     open("CHANGES.txt").read() + "\n"
