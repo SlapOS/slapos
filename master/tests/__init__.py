4519a527a4453b1aeeeab5da7c54540caa0bc7f4
Łukasz Nowak <luke@nexedi.com>
2011-07-07 13:18:32 +0200

    Define Vifib test suite.

---
+++ master/tests/__init__.py
@@ -0,0 +1,4 @@
+from test_suite import SavedTestSuite, ProjectTestSuite
+class VIFIB(SavedTestSuite, ProjectTestSuite):
+  _product_list = ['Vifib']
+  _saved_test_id = 'Products.Vifib.tests.VifibMixin'
