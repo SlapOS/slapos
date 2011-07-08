700051f5032a14b51e44a4fc7ad1e5cd948ad093
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2011-07-08 15:39:53 +0200

    Do not parse arguments twice for slapos-request.
    This commit also  fixes other arguments which were read  and parsed as regular
    configuration files.

--- slapos/console.py
+++ slapos/console.py
@@ -129,8 +129,8 @@ def request():
   usage = """usage: %s [options] CONFIGURATION_FILE SOFTWARE_INSTANCE INSTANCE_REFERENCE
 slapos-request allows you to request slapos instances.""" % sys.argv[0]
   config = Config()
-  arguments = Parser(usage=usage).check_args()[1]
-  config.setConfig(*Parser(usage=usage).check_args())
+  options, arguments = Parser(usage=usage).check_args()
+  config.setConfig(options, arguments[0])
   
   local = init(config)
   
