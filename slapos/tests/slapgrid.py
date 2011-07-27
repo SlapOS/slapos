9e247a41516e450c2cb8a402c1b94f7745125307
Łukasz Nowak <luke@nexedi.com>
2011-07-27 16:55:09 +0200

    Initiate slapgrid testing.

---
+++ slapos/tests/slapgrid.py
@@ -0,0 +1,38 @@
+from slapos.grid import slapgrid
+import os
+import shutil
+import tempfile
+import unittest
+import socket
+
+class TestSlapgridCP(unittest.TestCase):
+  def setUp(self):
+    self._tempdir = tempfile.mkdtemp()
+    self.software_root = os.path.join(self._tempdir, 'software')
+    self.instance_root = os.path.join(self._tempdir, 'instance')
+    self.master_url = 'http://127.0.0.1:0/'
+    self.computer_id = 'computer'
+    self.supervisord_socket = os.path.join(self._tempdir, 'supervisord.sock')
+    self.supervisord_configuration_path = os.path.join(self._tempdir,
+      'supervisord')
+    self.usage_report_periodicity = 1
+    self.buildout = None
+    self.grid = slapgrid.Slapgrid(self.software_root, self.instance_root,
+      self.master_url, self.computer_id, self.supervisord_socket,
+      self.supervisord_configuration_path, self.usage_report_periodicity,
+      self.buildout)
+
+  def tearDown(self):
+    shutil.rmtree(self._tempdir)
+
+  def test_no_software_root(self):
+    self.assertRaises(OSError, self.grid.processComputerPartitionList)
+
+  def test_no_instance_root(self):
+    os.mkdir(self.software_root)
+    self.assertRaises(OSError, self.grid.processComputerPartitionList)
+
+  def test_no_master(self):
+    os.mkdir(self.software_root)
+    os.mkdir(self.instance_root)
+    self.assertRaises(socket.error, self.grid.processComputerPartitionList)
