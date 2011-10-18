ede5ed342232f001169d96d42e9640f659671dc3
Łukasz Nowak <luke@nexedi.com>
2011-10-18 18:52:27 +0200

    WIP
    Dirty fast way to instantiate ERP5: TidStorage, Zope, Zeo, haproxy, apache on
    one partition.

--- slapos/recipe/generate_erp5_tidstorage/__init__.py
+++ slapos/recipe/generate_erp5_tidstorage/__init__.py
@@ -26,6 +26,13 @@
 ##############################################################################
 from slapos.recipe.librecipe import GenericSlapRecipe
 import os
+import json
+import pkg_resources
+
+ZOPE_PORT_BASE = 12000
+ZEO_PORT_BASE = 15000
+HAPROXY_PORT_BASE = 11000
+APACHE_PORT_BASE = 10000
 
 class Recipe(GenericSlapRecipe):
   def _options(self, options):
@@ -34,6 +41,32 @@ class Recipe(GenericSlapRecipe):
     options['output'] = os.path.join(self.dirname, self.name)
 
   def _generateRealTemplate(self):
+    # always one distribution node
+    # always one admin node
+    current_zeo_port = ZEO_PORT_BASE
+    current_zope_port = ZOPE_PORT_BASE
+    json_data = json.loads(self.parameter_dict['json'])
+    site_id = str(json_data['site-id'])
+    # prepare zeo
+    output = ''
+    part_list = []
+    for zeo_id, zeo_configuration in json_data['zeo'].iteritems():
+      current_zeo_port += 1
+      output += pkg_resources.resource_string(__name__,
+        'template/snippet-zeo.cfg') % dict(
+        zeo_id=zeo_id,
+        zeo_port=current_zeo_port,
+        storage_list=' '.join([str(q['zodb']) for q in zeo_configuration])
+      ) + '\n'
+      part_list.extend([
+        "zeo-instance-%s" % zeo_id,
+        "logrotate-entry-zeo-%s" % zeo_id
+      ])
+    prepend = pkg_resources.resource_string(__name__,
+        'template/instance.cfg') % dict(
+        part_list='  \n'.join(['  '+q for q in part_list]))
+    output = prepend + output
+    print output
     raise NotImplementedError
 
   def _install(self):
