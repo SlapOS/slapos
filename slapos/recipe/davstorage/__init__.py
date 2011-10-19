2e7caf943ee0646ba62060b31e626e621df6684c
Antoine Catton <acatton@tiolive.com>
2011-10-19 18:15:52 +0200

    Working davstorage
    With generic recipe

--- slapos/recipe/davstorage/__init__.py
+++ slapos/recipe/davstorage/__init__.py
@@ -24,102 +24,62 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 ##############################################################################
-from slapos.recipe.librecipe import BaseSlapRecipe
-import os
 import subprocess
-import pkg_resources
-import zc.buildout
-import zc.recipe.egg
-import sys
 
-class Recipe(BaseSlapRecipe):
-  def getTemplateFilename(self, template_name):
-    return pkg_resources.resource_filename(__name__,
-        'template/%s' % template_name)
+from slapos.recipe.librecipe import GenericBaseRecipe
 
-  def _install(self):
-    self.path_list = []
-    self.requirements, self.ws = self.egg.working_set()
-    document_root = self.createDataDirectory('www')
-    apache_config = self.installApache(document_root)
-    self.setConnectionUrl(scheme='webdavs',
-                          host=apache_config['ip'],
-                          port=apache_config['port'],
-                          auth=(apache_config['user'],
-                                apache_config['password']))
-    return self.path_list
+class Recipe(GenericBaseRecipe):
 
-  def installApache(self, document_root, ip=None, port=None):
-    if ip is None:
-      ip=self.getGlobalIPv6Address()
-    if port is None:
-      port = '9080'
+  def _options(self, options):
+    options['password'] = self.generatePassword()
 
-    htpasswd_config = self.createHtpasswd()
-    ssl_config = self.createCertificate(size=2048)
+  def install(self):
+    path_list = []
 
-    apache_config = dict(
-      pid_file=os.path.join(self.run_directory, 'httpd.pid'),
-      lock_file=os.path.join(self.run_directory, 'httpd.lock'),
-      davlock_db=os.path.join(self.run_directory, 'davdb.lock'),
-      ip=ip,
-      port=port,
-      error_log=os.path.join(self.log_directory, 'httpd-error.log'),
-      access_log=os.path.join(self.log_directory, 'httpd-access.log'),
-      document_root=document_root,
-      modules_dir=self.options['apache_modules_dir'],
-      mime_types=self.options['apache_mime_file'],
-      server_root=self.work_directory,
-      email_address='admin@vifib.net',
-      htpasswd_file=htpasswd_config['htpasswd_file'],
-      ssl_certificate=ssl_config['certificate'],
-      ssl_key=ssl_config['key'],
-    )
-    httpd_config_file = self.createConfigurationFile('httpd.conf',
-      self.substituteTemplate(self.getTemplateFilename('httpd.conf.in'),
-                              apache_config))
-    self.path_list.append(httpd_config_file)
-    apache_runner = zc.buildout.easy_install.scripts(
-      [('httpd', 'slapos.recipe.librecipe.execute', 'execute')],
-      self.ws, sys.executable, self.wrapper_directory,
-      arguments=[self.options['apache_binary'],
-                 '-f', httpd_config_file,
-                 '-DFOREGROUND',
-                ]
-    )[0]
-    self.path_list.append(apache_runner)
-    return dict(ip=apache_config['ip'],
-                port=apache_config['port'],
-                user=htpasswd_config['user'],
-                password=htpasswd_config['password']
-               )
+    htpasswd_file = self.options['htpasswd-file']
+    # Create or empty the file
+    open(htpasswd_file, 'w').close()
+    path_list.append(htpasswd_file)
 
-  def createHtpasswd(self):
-    htpasswd = self.createConfigurationFile('htpasswd', '')
-    self.path_list.append(htpasswd)
-    password = self.generatePassword()
-    user = 'user'
-    subprocess.check_call([self.options['apache_htpasswd'],
-                           '-bc', htpasswd,
+    user = self.options['user']
+    password = self.options['password']
+    subprocess.check_call([self.options['apache-htpasswd'],
+                           '-bc', htpasswd_file,
                            user, password
                           ])
-    return dict(htpasswd_file=htpasswd,
-                user=user,
-                password=password)
 
-  def createCertificate(self, size=1024, subject='/C=FR/L=Marcq-en-Baroeul/O=Nexedi'):
-    key_file = os.path.join(self.etc_directory, 'httpd.key')
-    self.path_list.append(key_file)
 
-    certificate_file = os.path.join(self.etc_directory, 'httpd.crt')
-    self.path_list.append(certificate_file)
+    apache_config = dict(
+      pid_file=self.options['pid-file'],
+      lock_file=self.options['lock-file'],
+      davlock_db=self.options['davdb-lock'],
+      ip=self.options['ip'],
+      port=self.options['port'],
+      error_log=self.options['error-log'],
+      access_log=self.options['access-log'],
+      document_root=self.options['htdocs'],
+      modules_dir=self.options['apache-modules-dir'],
+      mime_types=self.options['apache-mime-file'],
+      server_root=self.options['root'],
+      email_address=self.options['email-address'],
+      htpasswd_file=htpasswd_file,
+      ssl_certificate=self.options['cert-file'],
+      ssl_key=self.options['key-file'],
+    )
 
-    subprocess.check_call([self.options['openssl_binary'],
-                           'req', '-x509', '-nodes',
-                           '-newkey', 'rsa:%s' % size,
-                           '-subj', str(subject),
-                           '-out', certificate_file,
-                           '-keyout', key_file
-                          ])
-    return dict(key=key_file,
-                certificate=certificate_file)
+    # Create logfiles
+    for log in [self.options['error-log'], self.options['access-log']]:
+      open(log, 'a').close()
+
+    config_file = self.createFile(self.options['conf-file'],
+       self.substituteTemplate(self.getTemplateFilename('httpd.conf.in'),
+                               apache_config)
+    )
+    path_list.append(config_file)
+
+    wrapper = self.createPythonScript(self.options['wrapper'],
+      'slapos.recipe.librecipe.execute.execute',
+      [self.options['apache-binary'], '-f', config_file, '-DFOREGROUND'])
+    path_list.append(wrapper)
+
+    return path_list
