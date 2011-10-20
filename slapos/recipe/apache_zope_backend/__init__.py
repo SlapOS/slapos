81113478994f969a8f2f8f1d92c95d96d238fb14
Romain Courteaud <romain@nexedi.com>
2011-10-20 13:05:32 +0200

    Typo

--- slapos/recipe/apache_zope_backend/__init__.py
+++ slapos/recipe/apache_zope_backend/__init__.py
@@ -51,7 +51,7 @@ class Recipe(GenericBaseRecipe):
     apache_conf['key'] = key
     apache_conf['path'] = '/'
     apache_conf['access_control_string'] = access_control_string
-    apache_conf['rewrite_rule'] = "RewriteRule (.*) http://%s$1 [L,P]" % backend
+    apache_conf['rewrite_rule'] = "RewriteRule (.*) %s$1 [L,P]" % backend
     apache_conf_string = pkg_resources.resource_string(__name__,
           'template/apache.zope.conf.in') % apache_conf
     apache_config_file = self.createFile(self.options['configuration-file'],
