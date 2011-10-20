c4989d693ec5aebf85f0b5afe892d18eb1398d66
Romain Courteaud <romain@nexedi.com>
2011-10-20 13:01:24 +0200

    Typo

--- slapos/recipe/logrotate.py
+++ slapos/recipe/logrotate.py
@@ -57,7 +57,7 @@ class Recipe(GenericBaseRecipe):
 
     logrotate = self.createPythonScript(
       self.options['wrapper'],
-      'slapos.recipe.librecipe.exceute.execute',
+      'slapos.recipe.librecipe.execute.execute',
       [self.options['logrotate-binary'], '-s', state_file, logrotate_conf_file, ]
     )
 
