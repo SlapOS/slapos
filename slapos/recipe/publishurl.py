a88bf1825d2da40be2b54f0ccb58643f8c2cadea
Antoine Catton <acatton@tiolive.com>
2011-10-21 18:29:55 +0200

    Use meanful parameters in publishurl recipe

--- slapos/recipe/publishurl.py
+++ slapos/recipe/publishurl.py
@@ -57,9 +57,9 @@ class Recipe(GenericSlapRecipe):
           self.urlparts[option] = self.options[option]
 
       if 'user' in self.options:
-        self.urlparts.update(auth=(self.options['user'],))
+        self.urlparts.update(auth=(self.options['username'],))
         if 'password' in self.options:
-          self.urlparts.update(auth=(self.options['user'],
+          self.urlparts.update(auth=(self.options['username'],
                                      self.options['password']))
 
       self.setConnectionUrl(**self.urlparts)
