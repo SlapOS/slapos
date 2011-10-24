d89a6a5be17ddee0cdf7587e474d55e19eb9ae8d
Mayoro Diagne <mayoro@nexedi.com>
2011-10-24 13:31:06 +0000

    first release of phpmyfaq

---
+++ software/phpmyfaq/template/database.php
@@ -0,0 +1,7 @@
+<?php
+$DB["server"] = '%(mysql_host)s';
+$DB["user"] = '%(mysql_user)s';
+$DB["password"] = '%(mysql_password)s';
+$DB["db"] = '%(mysql_database)s';
+$DB["prefix"] = 'faq_';
+$DB["type"] = 'mysql';
