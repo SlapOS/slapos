cb5e3573e80ff8061a33f3c10f414544e0affaad
Antoine Catton <acatton@tiolive.com>
2011-11-09 12:47:42 +0100

    Use part name for install.cfg in software type recipe

--- slapos/recipe/softwaretype.py
+++ slapos/recipe/softwaretype.py
@@ -96,7 +96,7 @@ class Recipe:
     with open(instance_file_path) as instance_path:
       buildout.readfp(instance_path)
 
-    buildout.set('buildout', 'installed', '.installed2.cfg')
+    buildout.set('buildout', 'installed', '.installed-%s.cfg' % self.name)
 
     buildout.add_section('slap-parameter')
     for parameter, value in self.parameter_dict.items():
