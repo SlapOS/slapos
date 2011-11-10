e4bd7efa8942b21510a5f6404cc695c981693cd2
Łukasz Nowak <luke@nexedi.com>
2011-11-10 11:57:38 +0100

    Proof that stopped is no-op on destruction in progress.

--- master/product/Vifib/tests/testVifibSlapComputerPartitionStopped.py
+++ master/product/Vifib/tests/testVifibSlapComputerPartitionStopped.py
@@ -10,6 +10,60 @@ class TestVifibSlapComputerPartitionStopped(TestVifibSlapWebServiceMixin):
     sequence_list.addSequenceString(sequence_string)
     sequence_list.play(self)
 
+  def test_ComputerPartition_stopped_destroy_requested(self):
+    """Checks that stopped does not raise in case if destroy was requested"""
+    sequence_list = SequenceList()
+    sequence_string = self\
+        .prepare_stopped_computer_partition_sequence_string + """
+      LoginTestVifibCustomer
+      RequestSoftwareInstanceDestroy
+      Tic
+      Logout
+
+      LoginDefaultUser
+      CheckComputerPartitionInstanceCleanupSalePackingListConfirmed
+      CheckComputerPartitionInstanceHostingSalePackingListDelivered
+      Logout
+
+      SlapLoginCurrentComputer
+      SoftwareInstanceStopped
+      Tic
+      SlapLogout
+
+      LoginDefaultUser
+      CheckComputerPartitionInstanceCleanupSalePackingListConfirmed
+      Logout
+    """
+    sequence_list.addSequenceString(sequence_string)
+    sequence_list.play(self)
+
+  def test_ComputerPartition_stopped_destroy_requested_started(self):
+    """Checks that stopped does not raise in case if destroy was requested
+    on started one"""
+    sequence_list = SequenceList()
+    sequence_string = self\
+        .prepare_started_computer_partition_sequence_string + """
+      LoginTestVifibCustomer
+      RequestSoftwareInstanceDestroy
+      Tic
+      Logout
+
+      LoginDefaultUser
+      CheckComputerPartitionInstanceCleanupSalePackingListConfirmed
+      Logout
+
+      SlapLoginCurrentComputer
+      SoftwareInstanceStopped
+      Tic
+      SlapLogout
+
+      LoginDefaultUser
+      CheckComputerPartitionInstanceCleanupSalePackingListConfirmed
+      Logout
+    """
+    sequence_list.addSequenceString(sequence_string)
+    sequence_list.play(self)
+
 def test_suite():
   suite = unittest.TestSuite()
   suite.addTest(unittest.makeSuite(TestVifibSlapComputerPartitionStopped))
