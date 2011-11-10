af40cef043e3a43847b824222789d51ba38de991
Łukasz Nowak <luke@nexedi.com>
2011-11-10 15:25:53 +0100

    Check damaged instance_xml.

--- master/product/Vifib/tests/testVifibSlapComputerGetComputerPartitionList.py
+++ master/product/Vifib/tests/testVifibSlapComputerGetComputerPartitionList.py
@@ -589,6 +589,30 @@ class TestVifibSlapComputerGetComputerPartitionList(TestVifibSlapWebServiceMixin
     sequence_list.addSequenceString(sequence_string)
     sequence_list.play(self)
 
+  def stepDamageSoftwareInstanceXml(self, sequence, **kw):
+    software_instance = self.portal.portal_catalog.getResultValue(
+        uid=sequence['software_instance_uid'])
+    software_instance.edit(text_content="""
+    DAMAGED<BAD?xml XMLversion="1.0" encoding="utf-8"?>""")
+
+  def test_Computer_getComputerPartitionList_damaged_instance_xml(self):
+    """Check that getComputerPartitionList works in case of damaged XML on instance."""
+    sequence_list = SequenceList()
+    sequence_string = self\
+      .prepare_install_requested_computer_partition_sequence_string + """
+      LoginDefaultUser
+      DamageSoftwareInstanceXml
+      Logout
+
+      SlapLoginCurrentComputer
+      CheckSuccessComputerGetComputerPartitionCall
+      Tic
+      SlapLogout
+    """
+    sequence_list.addSequenceString(sequence_string)
+    sequence_list.play(self)
+
+
 def test_suite():
   suite = unittest.TestSuite()
   suite.addTest(unittest.makeSuite(TestVifibSlapComputerGetComputerPartitionList))
