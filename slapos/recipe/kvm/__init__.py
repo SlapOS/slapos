bf1bb980e7760d4de8cadce729a93e10b3e9cf5d
Cédric de Saint Martin <cedric.dsm@tiolive.com>
2011-11-23 09:58:14 +0100

    ca_email was meant to be ca_company

--- slapos/recipe/kvm/__init__.py
+++ slapos/recipe/kvm/__init__.py
@@ -54,9 +54,10 @@ class Recipe(BaseSlapRecipe):
     self.requirements, self.ws           = self.egg.working_set()
     self.cron_d                          = self.installCrond()
 
-    ca_email = binascii.hexlify(os.urandom(10))
+    # XXX-Cedric: Cert is self-signed and issuer is randomly generated.
+    ca_company = binascii.hexlify(os.urandom(10))
     self.ca_conf                         = self.installCertificateAuthority(
-                                               ca_email = ca_email)
+                                               ca_company = ca_company)
     self.key_path, self.certificate_path = self.requestCertificate('noVNC')
 
     # Install the socket_connection_attempt script
