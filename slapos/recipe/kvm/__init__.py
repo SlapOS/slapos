3cee01e310ccc62331c3b6397cec1091d36a7a99
Cédric de Saint Martin <cedric.dsm@tiolive.com>
2011-11-24 13:06:35 +0100

    Revert "Clever way to generate random CommonName only once"
    Yet Another Revert And Hopefully The Last One
    
    This reverts commit 7b3f3d012547e113d8d28208961cb5f77432550e.

--- slapos/recipe/kvm/__init__.py
+++ slapos/recipe/kvm/__init__.py
@@ -34,7 +34,6 @@ import zc.buildout
 import pkg_resources
 import ConfigParser
 import hashlib
-import uuid
 
 class Recipe(BaseSlapRecipe):
 
@@ -322,16 +321,13 @@ class Recipe(BaseSlapRecipe):
       certificate_authority_path=config['ca_dir']
     )
 
-  def requestCertificateRandomName(self, name):
-    """Add certificate to be generated, using 'name + random number' as Common
-       Name. If certificate already exists, it won't be regenerated.
-    """
+  def requestCertificate(self, name):
     hash = hashlib.sha512(name).hexdigest()
     key = os.path.join(self.ca_private, hash + self.ca_key_ext)
     certificate = os.path.join(self.ca_certs, hash + self.ca_crt_ext)
     parser = ConfigParser.RawConfigParser()
     parser.add_section('certificate')
-    parser.set('certificate', 'name', name + uuid.uuid1())
+    parser.set('certificate', 'name', name)
     parser.set('certificate', 'key_file', key)
     parser.set('certificate', 'certificate_file', certificate)
     parser.write(open(os.path.join(self.ca_request_dir, hash), 'w'))
