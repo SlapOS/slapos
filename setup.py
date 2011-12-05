ee0d136417d608d1c84066c43327994404595417
Łukasz Nowak <luke@nexedi.com>
2011-12-05 12:10:42 +0100

    Advertise 0.39 development.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.38'
+version = '0.39-dev'
 name = 'slapos.cookbook'
 long_description = open("README.txt").read() + "\n" + \
     open("CHANGES.txt").read() + "\n"
