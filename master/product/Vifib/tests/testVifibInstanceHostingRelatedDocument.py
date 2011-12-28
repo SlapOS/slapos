3647a8ed1b091d5aee2e0a737b0ab458119cbcdd
Łukasz Nowak <luke@nexedi.com>
2011-12-28 13:44:11 +0100

    Use already selected invoice.

--- master/product/Vifib/tests/testVifibInstanceHostingRelatedDocument.py
+++ master/product/Vifib/tests/testVifibInstanceHostingRelatedDocument.py
@@ -536,7 +536,6 @@ class TestVifibInstanceHostingRelatedDocument(TestVifibSlapWebServiceMixin):
 
         # Lets check the payment
 
-        SelectPlannedInvoice
         StartInvoice
         Tic
 
