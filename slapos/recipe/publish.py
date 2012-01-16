ec216f2eddf4e104be6478c089168745d0e0accd
Antoine Catton <acatton@tiolive.com>
2012-01-16 10:18:10 +0100

    Remove unused import on publish recipe.

--- slapos/recipe/publish.py
+++ slapos/recipe/publish.py
@@ -24,8 +24,6 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 ##############################################################################
-import zc.buildout
-
 from slapos.recipe.librecipe import GenericSlapRecipe
 
 class Recipe(GenericSlapRecipe):
