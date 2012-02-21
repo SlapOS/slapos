5227882cd742ab964dc829099bca76dbc36f13c6
Tatuya Kamada <tatuya@nexedi.com>
2012-02-21 17:00:29 +0900

    Add varnish recipe.

--- setup.py
+++ setup.py
@@ -95,6 +95,7 @@ setup(name=name,
           'tidstorage = slapos.recipe.tidstorage:Recipe',
           'erp5.update = slapos.recipe.erp5_update:Recipe',
           'erp5.test = slapos.recipe.erp5_test:Recipe',
+          'generic.varnish = slapos.recipe.generic_varnish:Recipe',
         ],
         'slapos.recipe.nosqltestbed.plugin': [
           'kumo = slapos.recipe.nosqltestbed.kumo:KumoTestBed',
