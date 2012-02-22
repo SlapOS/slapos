18cc501173aeeee817a1901ee7bd053458364867
Łukasz Nowak <luke@nexedi.com>
2012-02-22 17:21:54 +0100

    Disable interactions while migrating.

---
+++ master/bt5/vifib_upgrader/ExtensionTemplateItem/VifibUpgrader.py
@@ -0,0 +1,16 @@
+from Products.ERP5Type.Base import WorkflowMethod
+
+@WorkflowMethod.disable
+def DeliveryLineSetZeroPriceAndOrUpdateAppliedRule(self):
+  portal_type = self.getPortalType()
+  assert( portal_type in self.getPortalDeliveryMovementTypeList())
+  common_specialise = 'sale_trade_condition_module/vifib_trade_condition'
+  specialise = self.getParentValue().getSpecialise()
+  if common_specialise != specialise:
+    self.getParentValue().setSpecialise(common_specialise)
+  self.setPrice(0.0)
+  if self.getSimulationState() == 'cancelled':
+    # force no simulation
+    self.setQuantity(0.0)
+  else:
+    self.setQuantity(1.0)
