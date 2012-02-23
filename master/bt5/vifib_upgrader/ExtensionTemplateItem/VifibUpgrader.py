a15edce55f57c11f2e6fe781a7222f6bae5bf074
Łukasz Nowak <luke@nexedi.com>
2012-02-23 12:46:28 +0100

    Migrate Open Sale Order Lines.

--- master/bt5/vifib_upgrader/ExtensionTemplateItem/VifibUpgrader.py
+++ master/bt5/vifib_upgrader/ExtensionTemplateItem/VifibUpgrader.py
@@ -16,3 +16,22 @@ def DeliveryLineSetZeroPriceAndOrUpdateAppliedRule(self):
   else:
     self.setQuantity(1.0)
     self.Delivery_updateAppliedRule()
+
+from DateTime import DateTime
+@WorkflowMethod.disable
+def OpenSaleOrderLine_migrate(self):
+  now = DateTime().earliestTime()
+  self.setStartDate(now)
+  self.setStopDate(now)
+  self.setPrice(0.0)
+  self.setQuantity(1.0)
+  self.setResource(self.getPortalObject().portal_preferences.getPreferredInstanceSubscriptionResource())
+  resource_value = self.getResourceValue()
+  self.setBaseContributionList(resource_value.getBaseContributionList())
+  self.setUseList(resource_value.getUseList())
+  self.setQuantityUnit(resource_value.getQuantityUnit())
+  self.setSpecialise('sale_trade_condition_module/vifib_trade_condition')
+  self.setSourceSection('organisation_module/vifib_internet')
+  self.setSource('organisation_module/vifib_internet')
+  self.setDestination(self.getParentValue().getDestinationSection())
+  self.setDestinationSection(self.getParentValue().getDestinationSection())
