8ee03078582ccc2410836ea90e30959d216480bc
Mayoro Diagne <mayoro@nexedi.com>
2012-02-24 23:47:30 +0100

    remove install directories for all modules after installation of phpbms

--- software/phpbms/configure-phpbms.py
+++ software/phpbms/configure-phpbms.py
@@ -8,8 +8,17 @@ def setup(args):
   
   install_php = os.path.join(htdocs, "install")
   shutil.rmtree(install_php, 1)
-  
-  
+  mod_api = os.path.join(htdocs, "modules/api/install")
+  shutil.rmtree(mod_api, 1)
+  mod_bms = os.path.join(htdocs, "modules/bms/install")
+  shutil.rmtree(mod_bms, 1)
+  mod_mailchimp = os.path.join(htdocs, "modules/mailchimp/install")
+  shutil.rmtree(mod_mailchimp, 1)
+  mod_recurringinvoices = os.path.join(htdocs, "modules/recurringinvoices/install")
+  shutil.rmtree(mod_recurringinvoices, 1)
+  mod_sample = os.path.join(htdocs, "modules/sample/install")
+  shutil.rmtree(mod_sample, 1)
+
 if __name__ == '__main__':
   setup(sys.argv[1:])
 
