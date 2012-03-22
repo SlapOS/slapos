72c9a15dd009d6bb01e3422b833bd07962dcfcb5
Cédric de Saint Martin <cedric.dsm@tiolive.com>
2012-03-22 13:07:05 +0100

    Go back to development

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.41'
+version = '0.42-dev'
 name = 'slapos.cookbook'
 long_description = open("README.txt").read() + "\n" + \
     open("CHANGES.txt").read() + "\n"
