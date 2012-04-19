8cd32a72e1770bb6ee7f2b28d3344d127f52eed9
Yingjie Xu <yxu@nexedi.com>
2012-04-19 13:58:21 +0200

    SlapOS Test Agent Packaging.

--- slapos/recipe/agent/__init__.py
+++ slapos/recipe/agent/__init__.py
@@ -28,9 +28,92 @@
 import os
 import sys
 import zc.buildout
-from slapos.recipe.librecipe import GenericBaseRecipe
+import slapos.slap
+from slapos.recipe.librecipe import BaseSlapRecipe
+from slapos.recipe.librecipe import GenericSlapRecipe
+import json
+import ConfigParser
 
-class Recipe(GenericBaseRecipe):
+class Recipe(BaseSlapRecipe, GenericSlapRecipe):
   def install(self):
+    self.path_list = []
+    self.crond = self.installCrond()
 
-    return []
+    slap = slapos.slap.slap()
+    computer_id = self.computer_id
+    computer_partition_id = self.computer_partition_id
+    server_url = self.server_url
+    key_file = self.key_file
+    cert_file = self.cert_file
+    slap.initializeConnection(server_url, key_file, cert_file)
+    self.computer_partition = slap.registerComputerPartition(
+        computer_id, computer_partition_id)
+    self.parameter_dict = self.computer_partition.getInstanceParameterDict()
+
+    configuration_path = os.path.join(self.work_directory, "agent.cfg")
+    configuration = ConfigParser.SafeConfigParser()
+    configuration.add_section("agent")
+    configuration.set("agent", "portal_url", self.parameter_dict["portal_url"])
+    configuration.set("agent", "master_url", self.parameter_dict["master_url"])
+    key_filepath = os.path.join(self.work_directory, "key")
+    key_file = open(key_filepath, "w")
+    key_file.write(self.parameter_dict["key"])
+    key_file.close()
+    configuration.set("agent", "key_file", key_filepath)
+    cert_filepath = os.path.join(self.work_directory, "cert")
+    cert_file = open(cert_filepath, "w")
+    cert_file.write(self.parameter_dict["cert"])
+    cert_file.close()
+    configuration.set("agent", "cert_file", cert_filepath)
+    configuration.set("agent", "maximum_software_installation_duration",
+        self.parameter_dict["maximum_software_installation_duration"])
+    configuration.set("agent", "software_live_duration",
+        self.parameter_dict["software_live_duration"])
+    configuration.set("agent", "computer_list",
+        self.parameter_dict["computer_list"])
+    configuration.set("agent", "software_list",
+        self.parameter_dict["software_list"])
+    configuration.set("agent", "log_directory", self.options["log_directory"])
+    configuration.set("agent", "state_file", self.options["state_file"])
+    state = open(self.options["state_file"], "w")
+    state.write('')
+    state.close()
+    configuration.add_section("software_uri")
+    software_list = json.loads(self.parameter_dict["software_list"])
+    for software in software_list:
+      configuration.set("software_uri", software, self.parameter_dict[software])
+
+    configuration.write(open(configuration_path, "w"))
+
+    agent_crond_path = os.path.join(self.crond, "agent")
+    agent_crond = open(agent_crond_path, "w")
+    agent_crond.write("*/1 * * * * %s -S %s" % \
+      (self.options["python_binary"],
+        "%s --pidfile=%s %s" % \
+          (self.options["agent_binary"], self.options["pidfile"],
+            configuration_path)))
+    agent_crond.close()
+
+    return self.path_list + [configuration_path, key_filepath, cert_filepath, agent_crond_path]
+
+  def installCrond(self):
+    _, self.ws = self.egg.working_set()
+    timestamps = self.createDataDirectory('cronstamps')
+    cron_output = os.path.join(self.log_directory, 'cron-output')
+    self._createDirectory(cron_output)
+    catcher = zc.buildout.easy_install.scripts([('catchcron',
+      __name__ + '.catdatefile', 'catdatefile')], self.ws, sys.executable,
+      self.bin_directory, arguments=[cron_output])[0]
+    self.path_list.append(catcher)
+    cron_d = os.path.join(self.etc_directory, 'cron.d')
+    crontabs = os.path.join(self.etc_directory, 'crontabs')
+    self._createDirectory(cron_d)
+    self._createDirectory(crontabs)
+    wrapper = zc.buildout.easy_install.scripts([('crond',
+      'slapos.recipe.librecipe.execute', 'execute')], self.ws, sys.executable,
+      self.wrapper_directory, arguments=[
+        self.options['dcrond_binary'].strip(), '-s', cron_d, '-c', crontabs,
+        '-t', timestamps, '-f', '-l', '5', '-M', catcher]
+      )[0]
+    self.path_list.append(wrapper)
+    return cron_d
