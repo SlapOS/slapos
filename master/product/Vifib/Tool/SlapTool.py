87eadd6fbbff9aa8f1c24175376b69c1fa42087a
Romain Courteaud <romain@nexedi.com>
2012-05-15 16:21:15 +0200

    Drop start_computer_partition

--- master/product/Vifib/Tool/SlapTool.py
+++ master/product/Vifib/Tool/SlapTool.py
@@ -742,9 +742,18 @@ class SlapTool(BaseTool):
     """
     Reports that Computer Partition is started
     """
-    return self._getSoftwareInstanceForComputerPartition(
+    instance = self._getSoftwareInstanceForComputerPartition(
         computer_id,
-        computer_partition_id).startComputerPartition()
+        computer_partition_id)
+    delivery = instance.getCausalityValue(portal_type=["Sale Packing List"])
+    if delivery is not None:
+      portal = self.getPortalObject()
+      line = delivery.contentValues(portal_type="Sale Packing List Line")[0]
+      if line.getResource() in [
+          portal.portal_preferences.getPreferredInstanceHostingResource(),
+          portal.portal_preferences.getPreferredInstanceUpdateResource()]:
+        if portal.portal_workflow.isTransitionPossible(delivery, 'start'):
+          delivery.start()
 
   @convertToREST
   def _stoppedComputerPartition(self, computer_id, computer_partition_id):
