78899370a5545961fff37fc369211ea23effb737
Romain Courteaud <romain@nexedi.com>
2012-05-15 15:52:09 +0200

    Drop start_computer_partition_installation

--- master/product/Vifib/Tool/SlapTool.py
+++ master/product/Vifib/Tool/SlapTool.py
@@ -676,9 +676,17 @@ class SlapTool(BaseTool):
     """
     Reports that Computer Partition is being build
     """
-    return self._getSoftwareInstanceForComputerPartition(
+    instance = self._getSoftwareInstanceForComputerPartition(
         computer_id,
-        computer_partition_id).startComputerPartitionInstallation()
+        computer_partition_id)
+    delivery = instance.getCausalityValue(portal_type=["Sale Packing List"])
+    if delivery is not None:
+      portal = self.getPortalObject()
+      line = delivery.contentValues(portal_type="Sale Packing List Line")[0]
+      if line.getResource() == portal.portal_preferences.\
+                                 getPreferredInstanceSetupResource():
+        if portal.portal_workflow.isTransitionPossible(delivery, 'start'):
+          delivery.start()
 
   @convertToREST
   def _availableComputerPartition(self, computer_id, computer_partition_id):
