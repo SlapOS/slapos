a9a933f2291222ff67693d49f3715f46e8d0bf89
Cédric de Saint Martin <cedric.dsm@tiolive.com>
2012-08-17 11:47:51 +0200

    Release 0.28

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.27.1-dev'
+version = '0.28'
 name = 'slapos.core'
 long_description = open("README.txt").read() + "\n" + \
     open("CHANGES.txt").read() + "\n"
