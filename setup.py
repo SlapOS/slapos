e67eff579e18d52bfc25b44ae73e58a2f2e803e8
Cédric Le Ninivin <cedric.leninivin@tiolive.com>
2012-08-23 15:18:46 +0200

    Prepare release 0.28.5

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup, find_packages
 import glob
 import os
-
-version = '0.29-dev'
+version = '0.28.5'
 name = 'slapos.core'
 long_description = open("README.txt").read() + "\n" + \
     open("CHANGES.txt").read() + "\n"
