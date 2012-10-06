aa0f8ae2d0ad71432175342398f78e98fbcb9979
Cédric de Saint Martin <cedric.dsm@tiolive.com>
2012-10-06 16:11:19 +0200

    Conventions.
    Excuse to restart testnode.

--- slapos/grid/utils.py
+++ slapos/grid/utils.py
@@ -131,7 +131,7 @@ def getCleanEnvironment(home_path='/tmp'):
       removed_env.append(k)
   changed_env['HOME'] = env['HOME'] = home_path
   for k in sorted(changed_env.iterkeys()):
-    logger.debug('Overriden %s = %r' % (k,changed_env[k]))
+    logger.debug('Overriden %s = %r' % (k, changed_env[k]))
   logger.debug('Removed from environment: %s' % ', '.join(sorted(removed_env)))
   return env
 
