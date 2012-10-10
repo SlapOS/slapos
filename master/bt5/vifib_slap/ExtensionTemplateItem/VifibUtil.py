95e98710a80bbb8e2b56ad6f8538035ff398a8c9
Łukasz Nowak <luke@nexedi.com>
2012-10-10 16:45:43 +0200

    Improve interactivity.
    Whenever planned Sale Order is reindexed try to fire up ordering alarm.
    Whenever ordered Sale Order is reindexed try to fire up confirming alarm.
    Whenever Hosting Subscription is reindexed try to fire up update open order alarm.
    In all cases obey alarm configuration (if it is subscribed, if alarm is active,
    if is not running).
    
    Whenever item is reindexed (Software Instance, Slave Instance, Software
    Installation or Computer Partition) try to register recalculation of related
    computer cache.
    
    In neither case try to be "smart" (eg. waiting for reindexation to finish) as
    this code just tries to speed up the system, it cannot achieve synchronism of
    updates.

--- master/bt5/vifib_slap/ExtensionTemplateItem/VifibUtil.py
+++ master/bt5/vifib_slap/ExtensionTemplateItem/VifibUtil.py
@@ -1,3 +1,42 @@
 import hashlib
+from Products.ERP5Type.UnrestrictedMethod import UnrestrictedMethod
+from AccessControl.SecurityManagement import getSecurityManager, \
+    setSecurityManager, newSecurityManager
+
 def getSha512Hexdiest(s):
   return hashlib.sha512(s).hexdigest()
+
+@UnrestrictedMethod
+def getComputerReference(item):
+  portal_type = item.getPortalType()
+  computer = None
+
+  if portal_type == 'Software Installation':
+    computer = item.getAggregateValue(portal_type='Computer')
+  elif portal_type == 'Computer Partition':
+    computer = item.getParentValue()
+  elif portal_type in ['Software Instance', 'Slave Instance']:
+    partition = item.getAggregateValue(portal_type='Computer Partition')
+    if partition is not None:
+      computer = partition.getParentValue()
+
+  if computer is not None and computer.getValidationState() == 'validated':
+    return computer.getReference()
+  return None
+
+def Item_activateFillComputerInformationCache(state_change):
+  item = state_change['object']
+  portal = item.getPortalObject()
+  computer_reference = getComputerReference(item)
+  if computer_reference is None:
+    return None
+
+  sm = getSecurityManager()
+  try:
+    newSecurityManager(None,
+      portal.acl_users.getUserById(computer_reference))
+    for full in (True, False):
+      portal.portal_slap._activateFillComputerInformationCache(
+        computer_reference, computer_reference, full)
+  finally:
+    setSecurityManager(sm)
