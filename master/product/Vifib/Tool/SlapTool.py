25452f88c80cc7ef4a21b43ab26c111e2758ce3d
Łukasz Nowak <luke@nexedi.com>
2012-10-24 18:07:23 +0200

    Force charset.

--- master/product/Vifib/Tool/SlapTool.py
+++ master/product/Vifib/Tool/SlapTool.py
@@ -102,7 +102,7 @@ def convertToREST(function):
           error=True)
       raise Unauthorized
 
-    self.REQUEST.response.setHeader('Content-Type', 'text/xml')
+    self.REQUEST.response.setHeader('Content-Type', 'text/xml; charset=utf-8')
     return '%s' % retval
   wrapper.__doc__ = function.__doc__
   return wrapper
@@ -1011,7 +1011,7 @@ class SlapTool(BaseTool):
     self.REQUEST.response.setHeader('Vary',
                                     'REMOTE_USER')
     self.REQUEST.response.setHeader('Last-Modified', rfc1123_date(DateTime()))
-    self.REQUEST.response.setHeader('Content-Type', 'text/xml')
+    self.REQUEST.response.setHeader('Content-Type', 'text/xml; charset=utf-8')
     self.REQUEST.response.setBody(xml_marshaller.xml_marshaller.dumps(d))
     return self.REQUEST.response
 
