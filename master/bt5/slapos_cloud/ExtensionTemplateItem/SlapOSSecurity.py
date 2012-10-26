66c4f283f1692186e482bc3acd0671a534be4294
Łukasz Nowak <luke@nexedi.com>
2012-10-26 10:38:36 +0200

    Merge and invert dependency.
    Merge SlapOSCloudSecurity into SlapOSSecurity and put them in slapos_cloud,
    in order to have correct dependency resolution.

--- master/bt5/slapos_cloud/ExtensionTemplateItem/SlapOSSecurity.py
+++ master/bt5/slapos_cloud/ExtensionTemplateItem/SlapOSSecurity.py
@@ -1,12 +1,12 @@
 ###############################################################################
 #
-# Copyright (c) 2002-2011 Nexedi SA and Contributors. All Rights Reserved.
+# Copyright (c) 2012 Nexedi SA and Contributors. All Rights Reserved.
 #
 # WARNING: This program as such is intended to be used by professional
 # programmers who take the whole responsibility of assessing all potential
 # consequences resulting from its eventual inadequacies and bugs
 # End users who are looking for a ready-to-use solution with commercial
-# guarantees and support are strongly adviced to contract a Free Software
+# guarantees and support are strongly advised to contract a Free Software
 # Service Company
 #
 # This program is Free Software; you can redistribute it and/or
@@ -26,6 +26,9 @@
 ##############################################################################
 
 from Products.ERP5Security.ERP5GroupManager import ConsistencyError
+from AccessControl.SecurityManagement import getSecurityManager, \
+             setSecurityManager, newSecurityManager
+from AccessControl import Unauthorized
 
 def getComputerSecurityCategory(self, base_category_list, user_name, 
                                 object, portal_type):
@@ -85,3 +88,40 @@ def getSoftwareInstanceSecurityCategory(self, base_category_list, user_name,
 
   return category_list
 
+def restrictMethodAsShadowUser(self, shadow_document=None, callable_object=None,
+    argument_list=None, argument_dict=None):
+  """
+  Restrict the security access of a method to the unaccessible shadow user
+  associated to the current user.
+  """
+  if argument_list is None:
+    argument_list = []
+  if argument_dict is None:
+    argument_dict = {}
+  if shadow_document is None or callable_object is None:
+    raise TypeError('shadow_document and callable_object cannot be None')
+  relative_url = shadow_document.getRelativeUrl()
+  if shadow_document.getPortalType() not in ('Person', 'Software Instance',
+      'Computer'):
+    raise Unauthorized("%s portal type %r is not supported" % (relative_url,
+      shadow_document.getPortalType()))
+  else:
+    # Switch to the shadow user temporarily, so that the behavior would not
+    # change even if this method is invoked by random users.
+    acl_users = shadow_document.getPortalObject().acl_users
+    reference = shadow_document.getReference()
+    if reference is None:
+      raise Unauthorized('%r is not configured' % relative_url)
+    real_user = acl_users.getUserById(reference)
+    if real_user is None:
+      raise Unauthorized('%s is not loggable user' % relative_url)
+    sm = getSecurityManager()
+    shadow_user = acl_users.getUserById('SHADOW-' + reference)
+    if shadow_user is None:
+      raise Unauthorized('Shadow of %s is not loggable user' % relative_url)
+    newSecurityManager(None, shadow_user)
+    try:
+      return callable_object(*argument_list, **argument_dict)
+    finally:
+      # Restore the original user.
+      setSecurityManager(sm)
