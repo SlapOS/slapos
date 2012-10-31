07ad2b3566bce3163d592c07775ef4552f60d71a
Łukasz Nowak <luke@nexedi.com>
2012-10-31 17:50:45 +0100

    Skeleton of test to write.

---
+++ master/bt5/slapos_cloud/TestTemplateItem/testSlapOSCoreSlapOSCloudInteractionWorkflow.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2002-2012 Nexedi SA and Contributors. All Rights Reserved.
+from Products.SlapOS.tests.testSlapOSMixin import \
+  testSlapOSMixin
+
+class TestSlapOSCorePersonRequest(testSlapOSMixin):
+
+  def test(self):
+    raise NotImplementedError
