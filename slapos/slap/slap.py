3c4ebec650622b232ebf480cf7aafa53fb298873
Cédric de Saint Martin <cedric.dsm@tiolive.com>
2012-11-02 09:42:30 +0100

    slaplib: Avoid connecting to SlapOS Master if no _computer_id is defined

--- slapos/slap/slap.py
+++ slapos/slap/slap.py
@@ -319,6 +319,9 @@ def _syncComputerPartitionInformation(func):
   def decorated(self, *args, **kw):
     if getattr(self, '_synced', 0):
       return func(self, *args, **kw)
+    if not self._computer_id:
+      # XXX Is it only in case of requesting instance?
+      raise ResourceNotReady("Intance is not ready yet.")
     # XXX: This is a ugly way to keep backward compatibility,
     # We should stablise slap library soon.
     computer = self._connection_helper.getFullComputerInformation(self._computer_id)
