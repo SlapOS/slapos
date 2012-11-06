37c9b32226b4efc474d37de0aaa238cf04e303f9
Łukasz Nowak <luke@nexedi.com>
2012-11-06 11:23:32 +0100

    Factorise tree creation.

--- master/product/SlapOS/tests/testSlapOSMixin.py
+++ master/product/SlapOS/tests/testSlapOSMixin.py
@@ -122,6 +122,80 @@ class testSlapOSMixin(Products.Vifib.tests.VifibMixin.testVifibMixin):
     ]
     return result
 
+  def _makeTree(self):
+    new_id = self.generateNewId()
+
+    self.request_kw = dict(
+        software_release=self.generateNewSoftwareReleaseUrl(),
+        software_title=self.generateNewSoftwareTitle(),
+        software_type=self.generateNewSoftwareType(),
+        instance_xml=self.generateSafeXml(),
+        sla_xml=self.generateEmptyXml(),
+        shared=False,
+        state="started"
+    )
+
+    # Clone person document
+    self.person_user = self.portal.person_module.template_member.\
+                                 Base_createCloneDocument(batch_mode=1)
+    self.person_user.edit(
+      title="live_test_%s" % new_id,
+      reference="live_test_%s" % new_id,
+      default_email_text="live_test_%s@example.org" % new_id,
+    )
+
+    self.person_user.validate()
+    for assignment in self.person_user.contentValues(portal_type="Assignment"):
+      assignment.open()
+    transaction.commit()
+    # prepare part of tree
+    self.hosting_subscription = self.portal.hosting_subscription_module\
+        .template_hosting_subscription.Base_createCloneDocument(batch_mode=1)
+    self.software_instance = self.portal.software_instance_module\
+        .template_software_instance.Base_createCloneDocument(batch_mode=1)
+
+    self.hosting_subscription.edit(
+        title=self.request_kw['software_title'],
+        reference="TESTHS-%s" % new_id,
+        url_string=self.request_kw['software_release'],
+        source_reference=self.request_kw['software_type'],
+        text_content=self.request_kw['instance_xml'],
+        sla_xml=self.request_kw['sla_xml'],
+        root_slave=self.request_kw['shared'],
+        predecessor=self.software_instance.getRelativeUrl(),
+        destination_section=self.person_user.getRelativeUrl()
+    )
+    self.hosting_subscription.validate()
+    self.portal.portal_workflow._jumpToStateFor(self.hosting_subscription, 'start_requested')
+
+    self.requested_software_instance = self.portal.software_instance_module\
+        .template_software_instance.Base_createCloneDocument(batch_mode=1)
+    self.software_instance.edit(
+        title=self.request_kw['software_title'],
+        reference="TESTSI-%s" % new_id,
+        url_string=self.request_kw['software_release'],
+        source_reference=self.request_kw['software_type'],
+        text_content=self.request_kw['instance_xml'],
+        sla_xml=self.request_kw['sla_xml'],
+        specialise=self.hosting_subscription.getRelativeUrl(),
+        predecessor=self.requested_software_instance.getRelativeUrl()
+    )
+    self.portal.portal_workflow._jumpToStateFor(self.software_instance, 'start_requested')
+    self.software_instance.validate()
+
+    self.requested_software_instance.edit(
+        title=self.generateNewSoftwareTitle(),
+        reference="TESTSI-%s" % self.generateNewId(),
+        url_string=self.request_kw['software_release'],
+        source_reference=self.request_kw['software_type'],
+        text_content=self.request_kw['instance_xml'],
+        sla_xml=self.request_kw['sla_xml'],
+        specialise=self.hosting_subscription.getRelativeUrl(),
+    )
+    self.portal.portal_workflow._jumpToStateFor(self.requested_software_instance, 'start_requested')
+    self.requested_software_instance.validate()
+    self.tic()
+
   def _makeComputer(self):
     self.computer = self.portal.computer_module.template_computer\
         .Base_createCloneDocument(batch_mode=1)
