f24f32f8c17a2226e5aba7c5d0337bf143e5a2ad
Łukasz Nowak <luke@nexedi.com>
2012-11-12 18:01:48 +0100

    Merge test suites.
    Base is SlapOSCloud, run everything from there also on VIFIB.

--- master/tests/__init__.py
+++ master/tests/__init__.py
@@ -1,33 +1,30 @@
 from test_suite import SavedTestSuite, ProjectTestSuite
 
+slapos_bt_list = [
+    'erp5_web_shacache',
+    'erp5_web_shadir',
+    'slapos_accounting',
+    'slapos_cache',
+    'slapos_cloud',
+    'slapos_erp5',
+    'slapos_pdm',
+    'slapos_rest_api',
+    'slapos_slap_tool',
+    'slapos_web',
+  ]
+
 class VIFIB(SavedTestSuite, ProjectTestSuite):
   _product_list = ['Vifib']
   _saved_test_id = 'Products.Vifib.tests.VifibMixin.testVifibMixin'
-  _bt_list = [
-    'slapos_cloud',
-    'slapos_rest_api',
+  _bt_list = slapos_bt_list + [
     'vifib_base',
     'vifib_data',
-    'slapos_category',
-    'slapos_erp5',
     'vifib_erp5',
     'vifib_slap',
     'vifib_upgrader',
-    'slapos_web',
   ]
 
 class SlapOSCloud(SavedTestSuite, ProjectTestSuite):
   _product_list = ['SlapOS']
   _saved_test_id = 'Products.SlapOS.tests.testSlapOSMixin.testSlapOSMixin'
-  _bt_list = [
-    'slapos_cloud',
-    'slapos_cache',
-    'slapos_erp5',
-    'slapos_pdm',
-    'slapos_accounting',
-    'slapos_slap_tool',
-    'slapos_rest_api',
-    'slapos_web',
-    'erp5_web_shadir',
-    'erp5_web_shacache',
-  ]
+  _bt_list = slapos_bt_list
