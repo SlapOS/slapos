2a9621f1a0d409525b6783339d734688935fa5e8
Łukasz Nowak <luke@nexedi.com>
2012-11-15 14:39:03 +0100

    Rebrand expanding.
    Drop Sale Order expanding, as no Sale Orders are going to be present.
    
    Switch alarm to expand *and* solve causality state in same moment. See
    9d1b92cc0d92be0dd02647ca57e7edf859d16297 which is changing causality state
    on delivery state change.
    
    Update test to reflect the change.

--- master/bt5/slapos_accounting/TestTemplateItem/testSlapOSAccountingSlapOSManageBuildingCalculatingDelivery.py
+++ master/bt5/slapos_accounting/TestTemplateItem/testSlapOSAccountingSlapOSManageBuildingCalculatingDelivery.py
@@ -42,10 +42,10 @@ class Simulator:
     open(self.outfile, 'w').write(repr(l))
     return self.to_return
 
-def simulateDelivery_updateCausalityState(func):
+def simulateDelivery_manageBuildingCalculatingDelivery(func):
   @functools.wraps(func)
   def wrapped(self, *args, **kwargs):
-    script_name = 'Delivery_updateCausalityState'
+    script_name = 'Delivery_manageBuildingCalculatingDelivery'
     if script_name in self.portal.portal_skins.custom.objectIds():
       raise ValueError('Precondition failed: %s exists in custom' % script_name)
     createZODBPythonScript(self.portal.portal_skins.custom,
@@ -53,8 +53,8 @@ def simulateDelivery_updateCausalityState(func):
                         '*args, **kwargs',
                         '# Script body\n'
 """portal_workflow = context.portal_workflow
-if context.getTitle() == 'Not visited by Delivery_updateCausalityState':
-  context.setTitle('Visited by Delivery_updateCausalityState')
+if context.getTitle() == 'Not visited by Delivery_manageBuildingCalculatingDelivery':
+  context.setTitle('Visited by Delivery_manageBuildingCalculatingDelivery')
 """ )
     transaction.commit()
     try:
@@ -66,74 +66,87 @@ if context.getTitle() == 'Not visited by Delivery_updateCausalityState':
   return wrapped
 
 class TestAlarm(testSlapOSMixin):
-  @simulateDelivery_updateCausalityState
+  @simulateDelivery_manageBuildingCalculatingDelivery
   def _test(self, state, message):
     delivery = self.portal.sale_packing_list_module.newContent(
-        title='Not visited by Delivery_updateCausalityState',
+        title='Not visited by Delivery_manageBuildingCalculatingDelivery',
         portal_type='Sale Packing List')
     self.portal.portal_workflow._jumpToStateFor(delivery, state)
     self.tic()
 
-    self.portal.portal_alarms.slapos_update_delivery_causality_state\
+    self.portal.portal_alarms.slapos_manage_building_calculating_delivery\
         .activeSense()
     self.tic()
 
     self.assertEqual(message, delivery.getTitle())
 
   def test_building(self):
-    self._test('building', 'Visited by Delivery_updateCausalityState')
+    self._test('building', 'Visited by Delivery_manageBuildingCalculatingDelivery')
 
   def test_calculating(self):
-    self._test('calculating', 'Visited by Delivery_updateCausalityState')
+    self._test('calculating', 'Visited by Delivery_manageBuildingCalculatingDelivery')
 
   def test_diverged(self):
-    self._test('diverged', 'Not visited by Delivery_updateCausalityState')
+    self._test('diverged', 'Not visited by Delivery_manageBuildingCalculatingDelivery')
 
   def test_solved(self):
-    self._test('solved', 'Not visited by Delivery_updateCausalityState')
+    self._test('solved', 'Not visited by Delivery_manageBuildingCalculatingDelivery')
 
   @withAbort
-  def _test_Delivery_updateCausalityState(self, state, empty=False):
+  def _test_Delivery_manageBuildingCalculatingDelivery(self, state, empty=False):
     delivery = self.portal.sale_packing_list_module.newContent(
-        title='Not visited by Delivery_updateCausalityState',
+        title='Not visited by Delivery_manageBuildingCalculatingDelivery',
         portal_type='Sale Packing List')
     self.portal.portal_workflow._jumpToStateFor(delivery, state)
 
     updateCausalityState_simulator = tempfile.mkstemp()[1]
+    updateSimulation_simulator = tempfile.mkstemp()[1]
     try:
       from Products.ERP5.Document.Delivery import Delivery
       Delivery.original_updateCausalityState = Delivery\
           .updateCausalityState
       Delivery.updateCausalityState = Simulator(
           updateCausalityState_simulator, 'updateCausalityState')
+      Delivery.updateSimulation = Simulator(
+          updateSimulation_simulator, 'updateSimulation')
 
-      delivery.Delivery_updateCausalityState()
+      delivery.Delivery_manageBuildingCalculatingDelivery()
 
-      value = eval(open(updateCausalityState_simulator).read())
+      updateCausalityState_value = eval(open(updateCausalityState_simulator).read())
+      updateSimulation_value = eval(open(updateSimulation_simulator).read())
 
       if empty:
-        self.assertEqual([], value)
+        self.assertEqual([], updateCausalityState_value)
+        self.assertEqual([], updateSimulation_value)
       else:
         self.assertEqual([{
           'recmethod': 'updateCausalityState',
           'recargs': (),
           'reckwargs': {'solve_automatically': False}}],
-          value
+          updateCausalityState_value
+        )
+        self.assertEqual([{
+          'recmethod': 'updateSimulation',
+          'recargs': (),
+          'reckwargs': {'expand_root': 1, 'expand_related': 1}}],
+          updateSimulation_value
         )
     finally:
       Delivery.updateCausalityState = Delivery.original_updateCausalityState
       delattr(Delivery, 'original_updateCausalityState')
       if os.path.exists(updateCausalityState_simulator):
         os.unlink(updateCausalityState_simulator)
+      if os.path.exists(updateSimulation_simulator):
+        os.unlink(updateSimulation_simulator)
 
-  def test_Delivery_updateCausalityState_calculating(self):
-    self._test_Delivery_updateCausalityState('calculating')
+  def test_Delivery_manageBuildingCalculatingDelivery_calculating(self):
+    self._test_Delivery_manageBuildingCalculatingDelivery('calculating')
 
-  def test_Delivery_updateCausalityState_building(self):
-    self._test_Delivery_updateCausalityState('building')
+  def test_Delivery_manageBuildingCalculatingDelivery_building(self):
+    self._test_Delivery_manageBuildingCalculatingDelivery('building')
 
-  def test_Delivery_updateCausalityState_solved(self):
-    self._test_Delivery_updateCausalityState('solved', True)
+  def test_Delivery_manageBuildingCalculatingDelivery_solved(self):
+    self._test_Delivery_manageBuildingCalculatingDelivery('solved', True)
 
-  def test_Delivery_updateCausalityState_diverged(self):
-    self._test_Delivery_updateCausalityState('diverged', True)
+  def test_Delivery_manageBuildingCalculatingDelivery_diverged(self):
+    self._test_Delivery_manageBuildingCalculatingDelivery('diverged', True)
