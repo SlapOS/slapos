6b4bb98124b5b4ab02791f4cb1245b22d8ef7805
Łukasz Nowak <luke@nexedi.com>
2012-11-20 11:10:37 +0100

    Declare another constraint tests.

--- master/bt5/slapos_accounting/TestTemplateItem/testSlapOSAccountingConstraint.py
+++ master/bt5/slapos_accounting/TestTemplateItem/testSlapOSAccountingConstraint.py
@@ -95,3 +95,22 @@ class TestHostingSubscription(TestSlapOSConstraintMixin):
       template % 'periodicity_minute', empty_string=False)
     self._test_property_existence(self.subscription, 'periodicity_month_day',
       template % 'periodicity_month_day', empty_string=False)
+
+class TestSaleInvoiceTransaction(TestSlapOSConstraintMixin):
+  def test_price_currency(self):
+    raise NotImplementedError
+
+  def test_resource(self):
+    raise NotImplementedError
+
+  def test_sale_invoice_specialise_sale_trade_condition_constraint(self):
+    raise NotImplementedError
+
+  def test_specialise_value(self):
+    raise NotImplementedError
+
+  def test_total_price_equal_accounting(self):
+    raise NotImplementedError
+
+  def test_trade_model_match_lines(self):
+    raise NotImplementedError
