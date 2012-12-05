0a817ea57582164f126695410042928095c88b3b
Łukasz Nowak <luke@nexedi.com>
2012-12-05 16:31:01 +0100

    Avoid template document migration.

--- master/bt5/slapos_upgrader/ExtensionTemplateItem/SlapOSUpgrader.py
+++ master/bt5/slapos_upgrader/ExtensionTemplateItem/SlapOSUpgrader.py
@@ -1,8 +1,15 @@
 from Products.ERP5Type.Base import WorkflowMethod
 
 def Instance_migrateUrlString(self):
+  BLACKLIST_RELATIVE_URL_LIST = (
+    'software_instance_module/template_slave_instance',
+    'software_instance_module/template_software_instance',
+    'hosting_subscription_module/template_hosting_subscription',
+  )
   @WorkflowMethod.disable
   def real(self):
+    if self.getRelativeUrl() in BLACKLIST_RELATIVE_URL_LIST:
+      return
     property_id = 'root_software_release_url'
     if self.getPortalType() not in ('Hosting Subscription', 'Software Instance', 'Slave Instance'):
       raise TypeError(self.getPortalType())
