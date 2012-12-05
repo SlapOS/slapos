edc5bc6923fcac83c142af754addb55db2dc6f9d
Łukasz Nowak <luke@nexedi.com>
2012-12-05 17:23:58 +0100

    Migrate computer categories.

--- master/bt5/slapos_upgrader/ExtensionTemplateItem/SlapOSUpgrader.py
+++ master/bt5/slapos_upgrader/ExtensionTemplateItem/SlapOSUpgrader.py
@@ -29,3 +29,39 @@ def Instance_migrateUrlString(self):
       real(o[0])
   else:
     real(self)
+
+def Computer_migrateCategory(self):
+  if self.getRelativeUrl() == 'computer_module/template_computer':
+    return
+  portal = self.getPortalObject()
+  def Computer_updateDestinationSection(computer):
+    # copy of portal_workflow/slapos_cloud_interaction_workflow/scripts/Computer_updateDestinationSection
+    
+    subject_list = computer.getSubjectList()
+    person_list = []
+    
+    for subject in subject_list:
+      if subject:
+        person_list.extend([x.getObject() for x in portal.portal_catalog(validation_state="validated", portal_type="Person", default_email_text=subject)])
+    
+    computer.edit(destination_section_value_list=person_list)
+
+  @WorkflowMethod.disable
+  def real(self):
+    sale_supply_line_list = portal.portal_catalog(portal_type='Sale Supply Line',
+      default_aggregate_uid=self.getUid())
+    assert(1 == len(sale_supply_line_list))
+    internal_packing_list_line = portal.portal_catalog.getResultValue(
+      portal_type='Internal Packing List Line',
+      default_aggregate_uid=self.getUid(),
+      sort_on=(('creation_date', 'DESC'),)
+    )
+    assert(internal_packing_list_line is not None)
+    sale_trade_condition = sale_supply_line_list[0].getParentValue()
+    self.edit(
+      source_administration=internal_packing_list_line.getParentValue().getSourceAdministration(),
+      subject_list=sale_trade_condition.getSubjectList(),
+    )
+    Computer_updateDestinationSection(self)
+    
+  real(self)
