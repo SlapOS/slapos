09e1ccf1e05f97b47c011ea3df61a72fe42f87e8
Cédric de Saint Martin <cedric.dsm@tiolive.com>
2013-01-17 15:57:55 +0100

    Advertise development of new version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import glob
 import os
-version = '0.33.2'
+version = '0.33.3-dev'
 name = 'slapos.core'
 long_description = open("README.txt").read() + "\n" + \
     open("CHANGES.txt").read() + "\n"
