29e65e5677449fb7b02b9d596f135aa297e71cac
Vincent Pelletier <vincent@nexedi.com>
2013-01-25 15:00:12 +0100

    Make "socket" parameter optional.

--- slapos/recipe/generic_mysql/mysql.py
+++ slapos/recipe/generic_mysql/mysql.py
@@ -42,7 +42,9 @@ def updateMysql(args):
   sleep = 30
   is_succeed = False
   while True:
-    mysql_upgrade_list = [conf['mysql_upgrade_binary'], '--no-defaults', '--user=root', '--socket=%s' % conf['socket']]
+    mysql_upgrade_list = [conf['mysql_upgrade_binary'], '--no-defaults', '--user=root']
+    if 'socket' in conf:
+      mysql_upgrade_list.append('--socket=' + conf['socket'])
     mysql_upgrade = subprocess.Popen(mysql_upgrade_list, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     result = mysql_upgrade.communicate()[0]
     if mysql_upgrade.returncode is None:
@@ -54,7 +56,9 @@ def updateMysql(args):
         print "MySQL database upgraded with result:\n%s" % result
       else:
         print "No need to upgrade MySQL database"
-      mysql_list = [conf['mysql_binary'].strip(), '--no-defaults', '-B', '--user=root', '--socket=%s' % conf['socket']]
+      mysql_list = [conf['mysql_binary'].strip(), '--no-defaults', '-B', '--user=root']
+      if 'socket' in conf:
+        mysql_list.append('--socket=' + conf['socket'])
       mysql = subprocess.Popen(mysql_list, stdin=subprocess.PIPE,
           stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
       result = mysql.communicate(conf['mysql_script'])[0]
