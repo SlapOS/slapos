93d061eeca34867ff1a964d7cc56096d8be1b50e
Vincent Pelletier <vincent@nexedi.com>
2013-01-25 14:16:01 +0100

    Add a comment near space-unsafe code.

--- slapos/recipe/symbolic_link.py
+++ slapos/recipe/symbolic_link.py
@@ -40,6 +40,7 @@ class Recipe:
     """
     path_list = []
     target_directory = self.options['target-directory']
+    # XXX: breaks if any path contains spaces.
     for linkline in self.options['link-binary'].split():
       path, linkname = os.path.split(linkline)
 
