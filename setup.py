38f5a409780980403cc6240dd20aee3505a6845c
Marco Mariani <marco.mariani@nexedi.com>
2013-01-29 14:37:19 +0100

    release number

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.71.2-dev'
+version = '0.71.2'
 name = 'slapos.cookbook'
 long_description = open("README.txt").read() + "\n" + \
     open("CHANGES.txt").read() + "\n"
