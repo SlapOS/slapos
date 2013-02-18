3245fb3ebec5061f4a838d70604e7c2789828bf5
Cédric de Saint Martin <cedric.dsm@tiolive.com>
2013-02-18 14:00:12 +0100

    Release slapos.cookbook-0.73.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.72.0'
+version = '0.73.0'
 name = 'slapos.cookbook'
 long_description = open("README.txt").read() + "\n" + \
     open("CHANGES.txt").read() + "\n"
