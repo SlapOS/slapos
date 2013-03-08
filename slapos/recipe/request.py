d9025faf126110ec900a95c396dfc11aa51cc5b4
Cédric de Saint Martin <cedric.dsm@tiolive.com>
2013-03-08 14:39:51 +0100

    Add backward compatibility about Partition.getInstanceGuid().
    Old versions of slapproxy/slapos master don't know it.
    Catching it here so that we have a chance to debug problems in SRs.

--- slapos/recipe/request.py
+++ slapos/recipe/request.py
@@ -127,9 +127,13 @@ class Recipe(object):
       return_parameter_dict = self._getReturnParameterDict(self.instance,
           return_parameters)
       if not slave:
-        options['instance-guid'] = self.instance.getInstanceGuid()
-        # XXX: deprecated, to be removed
-        options['instance_guid'] = self.instance.getInstanceGuid()
+        try:
+          options['instance-guid'] = self.instance.getInstanceGuid()
+          # XXX: deprecated, to be removed
+          options['instance_guid'] = self.instance.getInstanceGuid()
+        except slapmodule.ResourceNotReady:
+          # Backward compatibility. Old SlapOS masters don't know this.
+          self.logger.warning("Impossible to fetch instance GUID.")
     except (slapmodule.NotFoundError, slapmodule.ServerError, slapmodule.ResourceNotReady) as exc:
       self._raise_request_exception = exc
       self._raise_request_exception_formatted = traceback.format_exc()
