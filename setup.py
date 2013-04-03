ef633794e9abf3d9ebaa775599d9027ff4f996d4
Cédric de Saint Martin <cedric.dsm@tiolive.com>
2013-04-03 17:33:00 +0200

    Advertise development of new version.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.76.0'
+version = '0.76.1.dev'
 name = 'slapos.cookbook'
 long_description = open("README.txt").read() + "\n" + \
     open("CHANGES.txt").read() + "\n"
