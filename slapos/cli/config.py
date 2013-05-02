af66f9a6173472a49426c3e8cbe86dd5339905fc
Marco Mariani <marco.mariani@nexedi.com>
2013-05-02 10:57:09 +0200

    on help: print configuration file defaults

--- slapos/cli/config.py
+++ slapos/cli/config.py
@@ -22,7 +22,9 @@ class ConfigCommand(Command):
 
     def get_parser(self, prog_name):
         ap = super(ConfigCommand, self).get_parser(prog_name)
-        ap.add_argument('--cfg', help='SlapOS configuration file')
+        ap.add_argument('--cfg', help='SlapOS configuration file - ' +
+                                      'defaults to $%s ' % self.default_config_var +
+                                      'or %s' % self.default_config_path)
         return ap
 
     def _get_config(self, cfg_path, required=False):
