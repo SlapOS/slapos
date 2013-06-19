2846462cee13211ca4d58edf3f1ec4680ca86190
Benjamin Blanc <benjamin.blanc@tiolive.com>
2013-06-19 12:02:13 +0200

    UP

---
+++ slapos/recipe/generic_scalability_launcher/__init__.py
@@ -0,0 +1,48 @@
+##############################################################################
+#
+# Copyright (c) 2011 Vifib SARL and Contributors. All Rights Reserved.
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsibility of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# guarantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+import os
+from slapos.recipe.librecipe import GenericBaseRecipe
+
+class Recipe(GenericBaseRecipe):
+  def install(self):
+
+    # Var passed to template
+    config = dict(
+      url=self.options['url'],
+      scalability_launcher_log=self.options['scalability-launcher-log'],
+      scalability_launcher_binary=self.options['scalability-launcher-binary'],
+      test_suite_title=self.options['test-suite-title'],
+      shell_path=self.options['shell-path'],
+    )
+
+    # Executable creation and return the path
+    path_list = []
+    path_list.append(self.createExecutable(self.options['scalability-launcher-wrapper'],
+      self.substituteTemplate(self.getTemplateFilename('launcher.in'),
+        config)))
+
+    return path_list
