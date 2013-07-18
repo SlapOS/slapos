688ce000ff44fa6b95a2d257d528fdca983d3ff3
Cédric Le Ninivin <cedric.leninivin@tiolive.com>
2013-07-18 13:49:03 +0200

    Changelog for 0.78.3, release bump

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.78.2'
+version = '0.78.3'
 name = 'slapos.cookbook'
 long_description = open("README.txt").read() + "\n" + \
     open("CHANGES.txt").read() + "\n"
