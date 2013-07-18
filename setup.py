6cce7d0487557ad82678f414311d08f84e8bd920
Marco Mariani <marco.mariani@nexedi.com>
2013-07-18 14:05:10 +0200

    0.78.4 back to dev

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.78.3'
+version = '0.78.4.dev'
 name = 'slapos.cookbook'
 long_description = open("README.txt").read() + "\n" + \
     open("CHANGES.txt").read() + "\n"
