4f4a97939fa2d51c6f3a44e0bccf71eff8f5b3e0
Cédric de Saint Martin <cedric.dsm@tiolive.com>
2013-10-02 17:52:27 +0200

    CLI: must_be_root: extract logic to own function.

--- slapos/cli/command.py
+++ slapos/cli/command.py
@@ -19,12 +19,14 @@ class Command(cliff.command.Command):
 
         return parser
 
+def check_root_user(config_command_instance):
+  if sys.platform != 'cygwin' and os.getuid() != 0:
+      config_command_instance.app.log.error('This slapos command must be run as root.')
+      sys.exit(5)
 
 def must_be_root(func):
     @functools.wraps(func)
     def inner(self, *args, **kw):
-        if sys.platform != 'cygwin' and os.getuid() != 0:
-            self.app.log.error('This slapos command must be run as root.')
-            sys.exit(5)
+        check_root_user(self)
         return func(self, *args, **kw)
     return inner
