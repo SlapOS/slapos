fd6b8918913e0a5df5e65722dfc1d33cf44ec554
Alain Takoudjou <talino@tiolive.com>
2014-07-31 12:36:20 +0200

    Slapos_pdm: Alarm create upgrade decision for software release

--- master/bt5/slapos_pdm/TestTemplateItem/testSlapOSPDMAlarm.py
+++ master/bt5/slapos_pdm/TestTemplateItem/testSlapOSPDMAlarm.py
@@ -20,6 +20,21 @@ class TestSlapOSUpgradeDecisionProcess(testSlapOSMixin):
          title="TESTUPDE-%s" % self.new_id)
     upgrade_decision.confirm()
     return upgrade_decision
+  
+  def _makeComputer(self,new_id):
+    # Clone computer document
+    person = self.portal.person_module.template_member\
+         .Base_createCloneDocument(batch_mode=1)
+    computer = self.portal.computer_module\
+      .template_computer.Base_createCloneDocument(batch_mode=1)
+    computer.edit(
+      title="computer ticket %s" % (new_id, ),
+      reference="TESTCOMPT-%s" % (new_id, ),
+      source_administration_value=person
+    )
+    computer.validate()
+
+    return computer
     
   def _simulateUpgradeDecision_upgradeHostingSubscription(self, fake_return="True"):
     self._simulateScript('UpgradeDecision_upgradeHostingSubscription', fake_return)
@@ -82,13 +97,44 @@ return %s
     self.assertEqual(
         'Visited by UpgradeDecision_upgradeComputer', 
         upgrade_decision.workflow_history['edit_workflow'][-1]['comment'])
+  
+  
+  def _simulateComputer_checkAndCreateUpgradeDecision(self):
+    script_name = 'Computer_checkAndCreateUpgradeDecision'
+    if script_name in self.portal.portal_skins.custom.objectIds():
+      raise ValueError('Precondition failed: %s exists in custom' % script_name)
+    createZODBPythonScript(self.portal.portal_skins.custom,
+      script_name,
+      '*args, **kw',
+      '# Script body\n'
+"""portal_workflow = context.portal_workflow
+portal_workflow.doActionFor(context, action='edit_action', comment='Visited by Computer_checkAndCreateUpgradeDecision') """ )
+    transaction.commit()
 
+  def _dropComputer_checkAndCreateUpgradeDecision(self):
+    script_name = 'Computer_checkAndCreateUpgradeDecision'
+    if script_name in self.portal.portal_skins.custom.objectIds():
+      self.portal.portal_skins.custom.manage_delObjects(script_name)
+    transaction.commit()
+  
+  def test_Alarm_computerCheckUpgradeSoftwareRelease(self):
+    computer = self._makeComputer(self.new_id)
+    computer.edit(allocation_scope = 'open/public')
+    computer2 = self._makeComputer(self.generateNewId())
+    computer2.edit(allocation_scope = 'open/personal')
+    
+    self._simulateComputer_checkAndCreateUpgradeDecision()
 
+    try:
+      self.portal.portal_alarms.slapos_pdm_computer_create_upgrade_decision.\
+        activeSense()
+      self.tic()
+    finally:
+      self._dropComputer_checkAndCreateUpgradeDecision()
 
-
-
-
-
-
-
+    self.assertEqual('Visited by Computer_checkAndCreateUpgradeDecision',
+      computer.workflow_history['edit_workflow'][-1]['comment'])
+    
+    self.assertNotEqual('Visited by Computer_checkAndCreateUpgradeDecision',
+      computer2.workflow_history['edit_workflow'][-1]['comment'])
 
