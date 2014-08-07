7a4d45914c4e56e41e6d5a48d3dba69e9c875f7a
Cédric de Saint Martin <cedric.dsm@tiolive.com>
2014-08-07 10:22:39 +0000

    jsondump recipe: use GenericBaseRecipe.

--- slapos/recipe/jsondump.py
+++ slapos/recipe/jsondump.py
@@ -1,22 +1,22 @@
 
+from slapos.recipe.librecipe import GenericBaseRecipe
+
 import json
 import os
 
-class Recipe(object):
-    def __init__(self, buildout, name, options):
+class Recipe(GenericBaseRecipe):
+
+    def install(self):
         parameter_dict = {
             key: value
-            for key, value in options.items()
+            for key, value in self.options.items()
             if key not in ['json-output', 'recipe']
         }
 
-        self._json_output = options['json-output']
-        with os.fdopen(os.open(self._json_output, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, 0o600), 'w') as fout:
+        with os.fdopen(os.open(self.options['json-output'], os.O_WRONLY | os.O_CREAT | os.O_TRUNC, 0o600), 'w') as fout:
             fout.write(json.dumps(parameter_dict, indent=2, sort_keys=True))
-            fout.close()
 
-    def install(self):
-        return [self._json_output]
+        return [self.options['json-output']]
 
     update = install
 
