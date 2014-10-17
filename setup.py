5b183ffb4e0c3ccb999a1b3a0a39acc06e874334
Marco Mariani <marco.mariani@nexedi.com>
2014-10-17 10:24:07 +0200

    version 0.89

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.88'
+version = '0.89'
 name = 'slapos.cookbook'
 long_description = open("README.txt").read() + "\n" + \
     open("CHANGES.txt").read() + "\n"
