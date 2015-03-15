b0a6563a656e274b187773b0e49e880be1b8e63f
Rafael Monnerat <rafael@nexedi.com>
2015-03-15 22:34:44 +0000

    slapos_erp5: RJS skin was included
    After the instalation of erp5_web_renderjs_ui a new skin was introduced.

--- master/bt5/slapos_erp5/TestTemplateItem/portal_components/test.erp5.testSlapOSERP5SkinSelection.py
+++ master/bt5/slapos_erp5/TestTemplateItem/portal_components/test.erp5.testSlapOSERP5SkinSelection.py
@@ -64,6 +64,7 @@ class TestSlaposSkinSelection(testSlapOSMixin):
           'Multiflex',
           'ODS',
           'ODT',
+          'RJS',
           'RSS',
           'SHACACHE',
           'SHADIR',
@@ -220,6 +221,7 @@ zpt_generic
     selection_string_list = \
 """
 custom
+erp5_web_renderjs_ui
 slapos_upgrader
 slapos_contract
 slapos_crm_monitoring
@@ -1129,6 +1131,111 @@ zpt_control
 zpt_generic
 """
     self.assertSameSkinSelection(skin_name, selection_string_list)
+
+  def test_18_RJS_selection(self, quiet=0, run=run_all_test):
+    """
+    Check the skin folder order
+    """
+    if not run: return
+
+    skin_name = 'RJS'
+    selection_string_list = \
+"""
+custom
+erp5_web_renderjs_ui
+slapos_upgrader
+slapos_contract
+slapos_crm_monitoring
+slapos_accounting
+slapos_administration
+slapos_cache
+slapos_cloud
+slapos_cloud_promise
+slapos_consumption
+slapos_core
+slapos_crm
+slapos_hypermedia
+slapos_payzen
+slapos_pdm
+slapos_rest_api
+slapos_simulation
+slapos_slap_tool
+slapos_web
+slapos_configurator
+erp5_km
+erp5_knowledge_pad
+erp5_simulation
+erp5_dms_base
+erp5_dms_web
+erp5_accounting_l10n_fr
+erp5_upgrader
+erp5_access_tab
+erp5_access_token
+erp5_accounting
+erp5_administration
+erp5_base
+erp5_bearer_token
+erp5_commerce
+erp5_commerce_widget_library
+erp5_computer_immobilisation
+erp5_configurator
+erp5_configurator_wizard
+erp5_content_translation
+erp5_core
+erp5_core_proxy_field_legacy
+erp5_credential
+erp5_credential_oauth2
+erp5_crm
+erp5_data_set
+erp5_dhtml_style
+erp5_dms
+erp5_fckeditor
+erp5_forge
+erp5_forge_release
+erp5_gadget
+erp5_glossary
+erp5_html_compatibility
+erp5_ingestion
+erp5_ingestion_test
+erp5_integration
+erp5_invoicing
+erp5_item
+erp5_item_trade
+erp5_jquery
+erp5_ods_core
+erp5_odt_core
+erp5_ooo_import
+erp5_open_trade
+erp5_payzen_secure_payment
+erp5_pdm
+erp5_project
+erp5_project_trade
+erp5_promise
+erp5_rss_core
+erp5_secure_payment
+erp5_simplified_invoicing
+erp5_software_pdm
+erp5_stock_cache
+erp5_system_event
+erp5_toolbox
+erp5_trade
+erp5_vcs
+erp5_web
+erp5_web_crm
+erp5_web_minimal_theme
+erp5_web_service
+erp5_web_widget_library
+erp5_workflow
+erp5_xhtml_style
+external_method
+Images
+activity
+zpt_content
+zpt_control
+zpt_generic
+"""
+    self.assertSameSkinSelection(skin_name, selection_string_list)
+
 
 def test_suite():
   suite = unittest.TestSuite()
