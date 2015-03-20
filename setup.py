180fa8f1b30f06db3844e439848632a70bafa33b
Alain Takoudjou <talino@tiolive.com>
2015-03-20 16:35:15 +0100

    Advertise development of new

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.96'
+version = '0.97.dev0'
 name = 'slapos.cookbook'
 long_description = open("README.txt").read() + "\n" + \
     open("CHANGES.txt").read() + "\n"
