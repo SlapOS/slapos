52d2c342ccfc58c3cf12512998370ad76f3071d6
Jérome Perrin <jerome@nexedi.com>
2015-04-09 02:06:58 +0000

    Advertise development of new version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.98'
+version = '0.99.dev'
 name = 'slapos.cookbook'
 long_description = open("README.txt").read() + "\n" + \
     open("CHANGES.txt").read() + "\n"
