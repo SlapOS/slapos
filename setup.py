322415c1181d57563045ba5500fe8bb56b792e4a
Rafael Monnerat <rafael@nexedi.com>
2015-08-17 19:20:19 +0200

    Release 1.0.9

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '1.0.9.dev0'
+version = '1.0.9'
 name = 'slapos.cookbook'
 long_description = open("README.txt").read() + "\n" + \
     open("CHANGES.txt").read() + "\n"
