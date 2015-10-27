dcc7153526d94b99a8016ee20f6fd82b3c233e81
Alain Takoudjou <alain.takoudjou@nexedi.com>
2015-10-27 17:52:38 +0100

    Release 1.0.16

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '1.0.15.dev0'
+version = '1.0.15'
 name = 'slapos.cookbook'
 long_description = open("README.txt").read() + "\n" + \
     open("CHANGES.txt").read() + "\n"
