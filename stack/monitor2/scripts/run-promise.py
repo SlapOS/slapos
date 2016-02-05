a3763e6785aad8b79d8bd7af11039ccdbddb1afc
Alain Takoudjou <alain.takoudjou@nexedi.com>
2016-02-05 14:12:55 +0100

    monitor2 stack: first version for renderjs web interface

--- stack/monitor2/scripts/run-promise.py
+++ stack/monitor2/scripts/run-promise.py
@@ -1,17 +1,28 @@
-#!/usr/bin/env python
+#!{{ python }}
 # -*- coding: utf-8 -*-
 
 import sys
 import os
 import subprocess
 import json
+import psutil
+import time
+from shutil import copyfile
+import glob
 
 def main():
   if len(sys.argv) < 4:
-    print("Usage: %s <pid_path> <output_path> <command...>" % sys.argv[0])
+    print("Usage: %s <pid_path> <output_path> <command> [<name>] [...]" % sys.argv[0])
     return 2
   pid_path=sys.argv[1]
   output_path=sys.argv[2]
+  promise_name = history_folder = related_url = ""
+  if len(sys.argv) >= 5:
+    promise_name = sys.argv[4]
+  if len(sys.argv) >= 6:
+    related_url = sys.argv[5]
+  if len(sys.argv) >= 7:
+    history_folder = sys.argv[6]
   if os.path.exists(pid_path):
     with open(pid_path, "r") as pidfile:
       try:
@@ -21,27 +32,82 @@ def main():
       if pid and os.path.exists("/proc/" + str(pid)):
         print("A process is already running with pid " + str(pid))
         return 1
+  start_date = ""
   with open(pid_path, "w") as pidfile:
     process = executeCommand(sys.argv[3:])
+    ps_process = psutil.Process(process.pid)
+    start_date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(ps_process.create_time()))
     pidfile.write(str(process.pid))
-  status_json = generateStatusJsonFromProcess(process)
+  
+  status_json = generateStatusJsonFromProcess(process, start_date=start_date, title=promise_name)
+
+  # Save the lastest status change date (needed for rss)
+  if related_url:
+    status_json['_links'] = {"monitor": {"href": related_url}}
+  status_json['change-time'] = ps_process.create_time()
+  if os.path.exists(output_path):
+    with open(output_path) as f:
+      last_result = json.loads(f.read())
+      if status_json['status'] == last_result['status'] and last_result.has_key('change-time'):
+        status_json['change-time'] = last_result['change-time']
+
+  if history_folder:
+    updateStatusHistoryFolder(promise_name, output_path, history_folder)
   with open(output_path, "w") as outputfile:
     json.dump(status_json, outputfile)
   os.remove(pid_path)
 
+def updateStatusHistoryFolder(name, status_file, history_folder):
+  old_history_list = []
+  history_path = os.path.join(history_folder, name, '.jio_documents')
+  if not os.path.exists(status_file):
+    return
+  if not os.path.exists(history_folder):
+    return
+  if not os.path.exists(history_path):
+    try:
+      os.makedirs(history_path)
+    except OSError, e:
+      if e.errno == os.errno.EEXIST and os.path.isdir(history_path):
+        pass
+      else: raise
+  with open(status_file, 'r') as sf:
+    status_dict = json.loads(sf.read())
+    filename = '%s.status.json' % (
+      status_dict['start-date'].replace(' ', '_').replace(':', ''))
 
-def generateStatusJsonFromProcess(process):
+  copyfile(status_file, os.path.join(history_path, filename))
+  # Don't let history foler grow too much, keep 30 files
+  file_list = filter(os.path.isfile,
+      glob.glob("%s/*.status.json" % history_path)
+    )
+  file_count = len(file_list)
+  if file_count > 30:
+    file_list.sort(key=lambda x: os.path.getmtime(x))
+    while file_count > 30:
+      to_delete = file_list.pop(0)
+      try:
+        os.unlink(to_delete)
+        file_count -= 1
+      except OSError:
+        raise
+
+def generateStatusJsonFromProcess(process, start_date=None, title=None):
   stdout, stderr = process.communicate()
   try:
     status_json = json.loads(stdout)
   except ValueError:
     status_json = {}
   if process.returncode != 0:
-    status_json["status"] = "error"
+    status_json["status"] = "ERROR"
   elif not status_json.get("status"):
     status_json["status"] = "OK"
   if stderr:
-    status_json["error"] = stderr
+    status_json["message"] = stderr
+  if start_date:
+    status_json["start-date"] = start_date
+  if title:
+    status_json["title"] = title
   return status_json
 
 
--- stack/monitor2/scripts/status2rss.py
+++ stack/monitor2/scripts/status2rss.py
@@ -4,104 +4,47 @@ import json
 import datetime
 import base64
 import hashlib
+import PyRSS2Gen
+
+def getKey(item):
+  return item.pubDate
 
 def main():
-  _, title, link, public_folder, previous_status_path, output_path = sys.argv
-  final_status = "OK";
-  # getting status
-  for filename in os.listdir(public_folder):
+  _, title, link, base_url, status_folder, output_path = sys.argv
+
+  rss_item_list = []
+  for filename in os.listdir(status_folder):
     if filename.endswith(".status.json"):
-      filepath = os.path.join(public_folder, filename)
-      status = None
+      filepath = os.path.join(status_folder, filename)
+      result_dict = None
       try:
-        status = json.load(open(filepath, "r"))
+        result_dict = json.load(open(filepath, "r"))
       except ValueError:
+        print "Failed to load json file: %s" % filepath
         continue
-      try:
-        if status["status"] != "OK":
-          final_status = "BAD"
-          break
-      except KeyError:
-        final_status = "BAD"
-        break
-  # checking previous status
-  try:
-    status = open(previous_status_path, "r").readline(4)
-    if status == final_status:
-      return 0
-  except IOError:
-    pass
-  # update status
-  open(previous_status_path, "w").write(final_status)
-  # generating RSS
-  utcnow = datetime.datetime.utcnow()
-  open(output_path, "w").write(
-    newRssString(
-      title,
-      title,
-      link,
-      utcnow,
-      utcnow,
-      "60",
-      [
-        newRssItemString(
-          "Status is %s" % final_status,
-          "Status is %s" % final_status,
-          link,
-          newGuid("%s, %s" % (utcnow, final_status)),
-          utcnow,
-        )
-      ],
+      description = result_dict.get('message', '')
+      event_time = datetime.datetime.fromtimestamp(result_dict['change-time'])
+      rss_item = PyRSS2Gen.RSSItem(
+        title = '[%s] %s' % (result_dict['status'], result_dict['title']),
+        description = "%s: %s\n%s" % (event_time, result_dict['status'], description),
+        link = '%s/%s' % (base_url, filename),
+        pubDate = event_time,
+        guid = PyRSS2Gen.Guid(base64.b64encode("%s, %s" % (event_time, result_dict['status'])))
+      )
+      rss_item_list.append(rss_item)
+
+
+  ### Build the rss feed
+  sorted(rss_item_list, key=getKey)
+  rss_feed = PyRSS2Gen.RSS2 (
+    title = title,
+    link = link,
+    description = '',
+    lastBuildDate = datetime.datetime.utcnow(),
+    items = rss_item_list
     )
-  )
-
-
-def escapeHtml(string):
-  return string.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("\"", "&quot;")
-
-def newGuid(string):
-  sha256 = hashlib.sha256()
-  sha256.update(string)
-  return sha256.hexdigest()
-
-def newRssItemString(title, description, link, guid, pub_date, guid_is_perma_link=True):
-  return """<item>
- <title>%(title)s</title>
- <description>%(description)s</description>
- <link>%(link)s</link>
- <guid isPermaLink="%(guid_is_perma_link)s">%(guid)s</guid>
- <pubDate>%(pub_date)s</pubDate>
-</item>""" % {
-    "title": escapeHtml(title),
-    "description": escapeHtml(description),
-    "link": escapeHtml(link),
-    "guid": escapeHtml(guid),
-    "pub_date": escapeHtml(pub_date.strftime("%a, %d %b %Y %H:%M:%S +0000")),
-    "guid_is_perma_link": escapeHtml(repr(guid_is_perma_link).lower()),
-  }
-
-def newRssString(title, description, link, last_build_date, pub_date, ttl, rss_item_string_list):
-  return """<?xml version="1.0" encoding="UTF-8" ?>
-<rss version="2.0">
-<channel>
- <title>%(title)s</title>
- <description>%(description)s</description>
- <link>%(link)s</link>
- <lastBuildDate>%(last_build_date)s</lastBuildDate>
- <pubDate>%(pub_date)s</pubDate>
- <ttl>%(ttl)s</ttl>
-%(items)s
-</channel>
-</rss>
-""" % {
-    "title": escapeHtml(title),
-    "description": escapeHtml(description),
-    "link": escapeHtml(link),
-    "last_build_date": escapeHtml(last_build_date.strftime("%a, %d %b %Y %H:%M:%S +0000")),
-    "pub_date": escapeHtml(pub_date.strftime("%a, %d %b %Y %H:%M:%S +0000")),
-    "ttl": escapeHtml(str(ttl)),
-    "items": "\n\n".join([" " + item.replace("\n", "\n ") for item in rss_item_string_list]),
-  }
+  with open(output_path, 'w') as frss:
+    frss.write(rss_feed.to_xml())
 
 if __name__ == "__main__":
   exit(main())
