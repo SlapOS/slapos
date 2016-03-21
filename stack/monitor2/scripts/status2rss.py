15a22114471f8bb9258ba807eb53049427074f86
Alain Takoudjou <alain.takoudjou@nexedi.com>
2016-03-21 16:36:44 +0100

    monitor2: new stack working with erp5 renderjs web interface

--- stack/monitor2/scripts/status2rss.py
+++ stack/monitor2/scripts/status2rss.py
@@ -4,104 +4,73 @@ import json
 import datetime
 import base64
 import hashlib
+import PyRSS2Gen
+import argparse
+
+def parseArguments():
+  """
+  Parse arguments for monitor Rss Generator.
+  """
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--items_folder',
+                      help='Path where to get *.status.json files which contain result of promises.')
+  parser.add_argument('--output',
+                      help='The Path of file where feed file will be saved.')
+  parser.add_argument('--public_url',
+                      help='Monitor Instance public URL.')
+  parser.add_argument('--private_url',
+                      help='Monitor Instance private URL.')
+  parser.add_argument('--instance_name',
+                      default='UNKNOW Software Instance',
+                      help='Software Instance name.')
+  parser.add_argument('--hosting_name',
+                      default='',
+                      help='Hosting Subscription name.')
+
+  return parser.parse_args()
+
+def getKey(item):
+  return item.pubDate
 
 def main():
-  _, title, link, public_folder, previous_status_path, output_path = sys.argv
-  final_status = "OK";
-  # getting status
-  for filename in os.listdir(public_folder):
+  parser = parseArguments()
+
+  rss_item_list = []
+  for filename in os.listdir(parser.items_folder):
     if filename.endswith(".status.json"):
-      filepath = os.path.join(public_folder, filename)
-      status = None
+      filepath = os.path.join(parser.items_folder, filename)
+      result_dict = None
       try:
-        status = json.load(open(filepath, "r"))
+        result_dict = json.load(open(filepath, "r"))
       except ValueError:
+        print "Failed to load json file: %s" % filepath
         continue
-      try:
-        if status["status"] != "OK":
-          final_status = "BAD"
-          break
-      except KeyError:
-        final_status = "BAD"
-        break
-  # checking previous status
-  try:
-    status = open(previous_status_path, "r").readline(4)
-    if status == final_status:
-      return 0
-  except IOError:
-    pass
-  # update status
-  open(previous_status_path, "w").write(final_status)
-  # generating RSS
-  utcnow = datetime.datetime.utcnow()
-  open(output_path, "w").write(
-    newRssString(
-      title,
-      title,
-      link,
-      utcnow,
-      utcnow,
-      "60",
-      [
-        newRssItemString(
-          "Status is %s" % final_status,
-          "Status is %s" % final_status,
-          link,
-          newGuid("%s, %s" % (utcnow, final_status)),
-          utcnow,
-        )
-      ],
+      description = result_dict.get('message', '')
+      event_time = datetime.datetime.fromtimestamp(result_dict['change-time'])
+      rss_item = PyRSS2Gen.RSSItem(
+        categories = [result_dict['status']],
+        source = PyRSS2Gen.Source(result_dict['title'], parser.public_url),
+        title = '[%s] %s' % (result_dict['status'], result_dict['title']),
+        comments = description,
+        description = "%s: %s\n%s" % (event_time, result_dict['status'], description),
+        link = parser.private_url,
+        pubDate = event_time,
+        guid = PyRSS2Gen.Guid(base64.b64encode("%s, %s" % (event_time, result_dict['status'])))
+      )
+      rss_item_list.append(rss_item)
+
+
+  ### Build the rss feed
+  sorted(rss_item_list, key=getKey)
+  rss_feed = PyRSS2Gen.RSS2 (
+    title = parser.instance_name,
+    link = parser.public_url,
+    description = parser.hosting_name,
+    lastBuildDate = datetime.datetime.utcnow(),
+    items = rss_item_list
     )
-  )
-
-
-def escapeHtml(string):
-  return string.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("\"", "&quot;")
-
-def newGuid(string):
-  sha256 = hashlib.sha256()
-  sha256.update(string)
-  return sha256.hexdigest()
-
-def newRssItemString(title, description, link, guid, pub_date, guid_is_perma_link=True):
-  return """<item>
- <title>%(title)s</title>
- <description>%(description)s</description>
- <link>%(link)s</link>
- <guid isPermaLink="%(guid_is_perma_link)s">%(guid)s</guid>
- <pubDate>%(pub_date)s</pubDate>
-</item>""" % {
-    "title": escapeHtml(title),
-    "description": escapeHtml(description),
-    "link": escapeHtml(link),
-    "guid": escapeHtml(guid),
-    "pub_date": escapeHtml(pub_date.strftime("%a, %d %b %Y %H:%M:%S +0000")),
-    "guid_is_perma_link": escapeHtml(repr(guid_is_perma_link).lower()),
-  }
-
-def newRssString(title, description, link, last_build_date, pub_date, ttl, rss_item_string_list):
-  return """<?xml version="1.0" encoding="UTF-8" ?>
-<rss version="2.0">
-<channel>
- <title>%(title)s</title>
- <description>%(description)s</description>
- <link>%(link)s</link>
- <lastBuildDate>%(last_build_date)s</lastBuildDate>
- <pubDate>%(pub_date)s</pubDate>
- <ttl>%(ttl)s</ttl>
-%(items)s
-</channel>
-</rss>
-""" % {
-    "title": escapeHtml(title),
-    "description": escapeHtml(description),
-    "link": escapeHtml(link),
-    "last_build_date": escapeHtml(last_build_date.strftime("%a, %d %b %Y %H:%M:%S +0000")),
-    "pub_date": escapeHtml(pub_date.strftime("%a, %d %b %Y %H:%M:%S +0000")),
-    "ttl": escapeHtml(str(ttl)),
-    "items": "\n\n".join([" " + item.replace("\n", "\n ") for item in rss_item_string_list]),
-  }
+  with open(parser.output, 'w') as frss:
+    frss.write(rss_feed.to_xml())
 
 if __name__ == "__main__":
   exit(main())
