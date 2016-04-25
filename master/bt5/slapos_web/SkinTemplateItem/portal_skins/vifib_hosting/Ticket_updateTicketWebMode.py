bf2d0b49c7684b8f3723d080e76a48aaf2e122b7
Alain Takoudjou <alain.takoudjou@nexedi.com>
2016-04-25 12:53:59 +0000

    slapos_web: fix user ticked creation, update ticket method use proxy rule "Manager" to let user re-open closed tickets.

--- master/bt5/slapos_web/SkinTemplateItem/portal_skins/vifib_hosting/Ticket_updateTicketWebMode.py
+++ master/bt5/slapos_web/SkinTemplateItem/portal_skins/vifib_hosting/Ticket_updateTicketWebMode.py
@@ -1,6 +1,15 @@
+from zExceptions import Unauthorized
 portal = context.getPortalObject()
 web_site = context.getWebSiteValue()
 person = portal.ERP5Site_getAuthenticatedMemberPersonValue()
+
+# XXX Proxy role is Manager so user to open ticket which is closed.
+# We check permissions
+if not person:
+  raise Unauthorized("You need to be logged in before modify this object!")
+
+if context.getDestinationDecision() != person.getRelativeUrl():
+  raise Unauthorized
 
 request = context.REQUEST
 if 'field_your_description' in request:
@@ -9,10 +18,14 @@ else:
   return context.Base_redirect('view', keep_items={'portal_status_message':context.Base_translateString('No changes.')})
 
 web_message_template = portal.restrictedTraverse(
-  portal.portal_preferences.getPreferredWebMessageTemplate())
+    portal.portal_preferences.getPreferredWebMessageTemplate())
 web_message = web_message_template.Base_createCloneDocument(batch_mode=1)
 # XXX init script is not called when cloning?
-web_message.Event_init()
+# web_message.Event_init()
+# XXX cannot call init script here with proxy role as Manager.
+web_message.setContentType(portal.portal_preferences.getPreferredTextFormat())
+web_message.Event_generateReference()
+
 web_message.edit(
   title="Re: %s" % context.getTitle(),
   content_type="text/plain",
@@ -22,6 +35,7 @@ web_message.edit(
   start_date=DateTime(),
   follow_up_value=context,
 )
+
 web_message.stop(comment="Submitted from the web site contact form")
 if portal.portal_workflow.isTransitionPossible(context, "validate"):
   context.validate(comment="See %s" % web_message.getRelativeUrl())
