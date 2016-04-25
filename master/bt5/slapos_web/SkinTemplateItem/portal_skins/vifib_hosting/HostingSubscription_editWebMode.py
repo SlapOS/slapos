f65752c5c41aa00d8b745ac667bac2ea724002b1
Alain Takoudjou <alain.takoudjou@nexedi.com>
2016-04-25 12:57:18 +0000

    slapos_web: prevent user to try to change software_type of requested hosting subscription

--- master/bt5/slapos_web/SkinTemplateItem/portal_skins/vifib_hosting/HostingSubscription_editWebMode.py
+++ master/bt5/slapos_web/SkinTemplateItem/portal_skins/vifib_hosting/HostingSubscription_editWebMode.py
@@ -1,4 +1,16 @@
 request = context.REQUEST
+
+def isSoftwareTypeChanged(software_type):
+  base_type = ['RootSoftwareInstance', 'default']
+  current_software_type = context.getSourceReference()
+  if software_type in base_type and current_software_type in base_type:
+    return False
+  else:
+    return current_software_type != software_type
+
+if 'software_type' in request and isSoftwareTypeChanged(request['software_type']):
+  message = "Sorry, you cannot change 'Software Type' value."
+  return context.Base_redirect('view', keep_items={'portal_status_message':context.Base_translateString(message)})
 if 'field_your_instance_xml' in request:
   if context.getTextContent() != request['field_your_instance_xml']:
     context.HostingSubscription_requestPerson(instance_xml=request['field_your_instance_xml'])
