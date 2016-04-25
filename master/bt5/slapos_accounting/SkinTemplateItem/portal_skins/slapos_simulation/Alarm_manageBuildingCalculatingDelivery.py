68722dd821effe9f687191f5f8e2ee374db93eb5
Alain Takoudjou <alain.takoudjou@nexedi.com>
2016-04-25 12:41:53 +0000

    slapos_accounting: fix alarm building and calculating delivery.
    Add proxy rules Manager to script Delivery_manageBuildingCalculatingDelivery.
    Fix condition which check if activity doesn't exists with this path, check if there is no more than one activity running where method is Delivery_manageBuildingCalculatingDelivery and path is current path.

--- master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_simulation/Alarm_manageBuildingCalculatingDelivery.py
+++ master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_simulation/Alarm_manageBuildingCalculatingDelivery.py
@@ -6,6 +6,7 @@ portal.portal_catalog.searchAndActivate(
   portal_type=portal.getPortalDeliveryTypeList(),
   causality_state=('building', 'calculating'),
   activate_kw=activate_kw,
+  packet_size=30,
   method_id='Delivery_manageBuildingCalculatingDelivery'
 )
 context.activate(after_tag=tag).getId()
--- master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_simulation/Delivery_manageBuildingCalculatingDelivery.py
+++ master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_simulation/Delivery_manageBuildingCalculatingDelivery.py
@@ -1,9 +1,14 @@
+if REQUEST is not None:
+  raise Unauthorized
+
 delivery = context
 if delivery.getCausalityState() not in ('building', 'calculating'):
   return
 path = delivery.getPath()
 portal_activities = context.getPortalObject().portal_activities
-if portal_activities.countMessage(path=path) == 0 \
+
+
+if portal_activities.countMessage(method_id='Delivery_manageBuildingCalculatingDelivery', path=path) == 1 \
   and portal_activities.countMessageWithTag('%s_solve' % path) == 0:
   delivery.serialize()
   delivery.updateCausalityState(solve_automatically=True)
