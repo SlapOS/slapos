e677e97b460d046f9ed81710bd52950b401ef1a6
Rafael Monnerat <rafael@nexedi.com>
2016-05-02 12:00:28 +0000

    slapos_crm: Add a helper script  to check Person/Users rules.

---
+++ master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/Person_hasRequiredRole.py
@@ -0,0 +1,9 @@
+if role_id in context.getRoleList():
+  return True
+
+for assignment in context.contentValues(portal_type="Assignment"):
+  if assignment.getValidationState() == "open" and \
+    assignment.getRole() == role_id:
+    return True
+
+return False
