871fb03463ed36f3d78243ff363182797eaa7f84
Alain Takoudjou <alain.takoudjou@nexedi.com>
2016-05-09 16:24:21 +0000

    slapos PAS constraint check also external plugins

--- master/bt5/slapos_erp5/SkinTemplateItem/portal_skins/slapos_erp5/CertificateAuthorityTool_checkSlapOSPASConsistency.py
+++ master/bt5/slapos_erp5/SkinTemplateItem/portal_skins/slapos_erp5/CertificateAuthorityTool_checkSlapOSPASConsistency.py
@@ -3,6 +3,13 @@ slapos_plugin_dict = {
   'IExtractionPlugin': [
     'SlapOS Machine Authentication Plugin',
     'ERP5 Access Token Extraction Plugin',
+#   external PAS
+    'Vifib Browser ID Extraction Plugin',
+    'Vifib Facebook Server Extraction Plugin',
+    'Vifib Google Server Extraction Plugin',
+#   'ERP5 Bearer Extraction Plugin',
+    'ERP5 Facebook Extraction Plugin',
+    'ERP5 Google Extraction Plugin',
   ],
   'IAuthenticationPlugin': [
     'SlapOS Machine Authentication Plugin',
