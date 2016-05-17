f6e1b27fd30ab65686ab87b94216e3a2ddf07896
Rafael Monnerat <rafael@nexedi.com>
2016-05-17 04:01:00 +0000

    slapos_crm: Reuse Person_hasRequiredRole instead duplicate code.

--- master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/Person_isServiceProvider.py
+++ master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/Person_isServiceProvider.py
@@ -1,9 +1,1 @@
-if 'service_provider' in context.getRoleList():
-  return True
-
-for assignment in context.contentValues(portal_type="Assignment"):
-  if assignment.getValidationState() == "open" and \
-    assignment.getRole() == 'service_provider':
-    return True
-
-return False
+return context.Person_hasRequiredRole("service_provider")
