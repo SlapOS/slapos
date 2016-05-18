40d141094924820d55f1669f80b746daf1aadb92
Alain Takoudjou <alain.takoudjou@nexedi.com>
2016-05-18 13:33:32 +0000

    slapos_erp5: Check all module IdGenerator in one constraint to speed up post upgrade

--- master/bt5/slapos_erp5/SkinTemplateItem/portal_skins/slapos_erp5/Module_checkSlapOSModuleIdGeneratorConsistency.py
+++ master/bt5/slapos_erp5/SkinTemplateItem/portal_skins/slapos_erp5/Module_checkSlapOSModuleIdGeneratorConsistency.py
@@ -1,10 +1,15 @@
+portal = context.getPortalObject()
 id_generator = '_generatePerDayId'
 error_list = []
 
-if context.getIdGenerator() != id_generator:
-  error_list.append("%s module has incorrect ID generator" % context.getRelativeUrl())
+for module_id in portal.objectIds(spec=('ERP5 Folder',)) + ["portal_simulation", "portal_activities"]:
+  module = portal.restrictedTraverse(module_id)
+  if module.getIdGenerator() != id_generator:
 
-  if fixit:
-    context.setIdGenerator(id_generator)
+    if fixit:
+      error_list.append("%s module has incorrect ID generator (fixed)." % context.getRelativeUrl())
+      module.setIdGenerator(id_generator)
+    else:
+      error_list.append("%s module has incorrect ID generator." % context.getRelativeUrl())
 
 return error_list
