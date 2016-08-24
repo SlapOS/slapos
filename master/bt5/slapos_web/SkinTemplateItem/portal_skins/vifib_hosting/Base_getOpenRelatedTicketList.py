371adef0801fd60fba6610caedcecf927d15a45c
Rafael Monnerat <rafael@nexedi.com>
2016-08-24 18:04:07 +0000

    slapos_web: Sort the tickets and include limit
    On the UI we don't want to see infinity tickets, only the most relevant for the computers and hosting subscriptions.

--- master/bt5/slapos_web/SkinTemplateItem/portal_skins/vifib_hosting/Base_getOpenRelatedTicketList.py
+++ master/bt5/slapos_web/SkinTemplateItem/portal_skins/vifib_hosting/Base_getOpenRelatedTicketList.py
@@ -17,8 +17,12 @@ query = ComplexQuery(
   logical_operator='or')
 
 # Use event modification date instead. 
-kw['sort_on'] = [('modification_date', 'DESC'),]
+kw['sort_on'] = (("catalog.simulation_state='suspended'", 'DESC'),
+                   ("catalog.simulation_state='validated'", 'DESC'),
+                   ("catalog.simulation_state='confirmed'", 'DESC'),
+                   ('modification_date', 'DESC'),)
 kw['simulation_state'] = "NOT cancelled"
+kw['limit'] = 30
 result_list = []
 for document in portal.portal_catalog(query=query, **kw):
   if document.getPortalType() == "Upgrade Decision Line":
