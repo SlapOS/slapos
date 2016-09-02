e3376cbfcb06e89d623c8bddff52ee1e724a73c1
Alain Takoudjou <alain.takoudjou@nexedi.com>
2016-09-02 15:49:20 +0000

    slapos_pdm: add personal computer in list to create upgrade decision

--- master/bt5/slapos_pdm/SkinTemplateItem/portal_skins/slapos_pdm/Alarm_computerCheckUpgradeSoftwareRelease.py
+++ master/bt5/slapos_pdm/SkinTemplateItem/portal_skins/slapos_pdm/Alarm_computerCheckUpgradeSoftwareRelease.py
@@ -4,12 +4,14 @@ public_scope_uid = portal.restrictedTraverse(
   "portal_categories/allocation_scope/open/public", None).getUid()
 friend_scope_uid =  portal.restrictedTraverse(
   "portal_categories/allocation_scope/open/friend", None).getUid()
+personal_scope_uid =  portal.restrictedTraverse(
+  "portal_categories/allocation_scope/open/personal", None).getUid()
 
 if public_scope_uid and friend_scope_uid:
   portal.portal_catalog.searchAndActivate(
     portal_type='Computer',
     validation_state = 'validated',
-    default_allocation_scope_uid=[public_scope_uid, friend_scope_uid],
+    default_allocation_scope_uid=[public_scope_uid, friend_scope_uid, personal_scope_uid],
     method_id = 'Computer_checkAndCreateUpgradeDecision',
     activate_kw = {'tag':tag}
   )
