73d73b529a050c4b55df16984fc1c0eded059ed0
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2016-09-16 10:38:54 +0200

    Revert "slapconfiguration: returns also slapuser"
    The recipe userinfo already provides this information
    
    This reverts commit fd5f808b9eccb1ff5fdd502bac9b57d8743ecd67.

--- slapos/recipe/slapconfiguration.py
+++ slapos/recipe/slapconfiguration.py
@@ -27,7 +27,6 @@
 
 import json
 import os
-import pwd
 
 import slapos.slap
 from slapos.recipe.librecipe import unwrap
@@ -106,8 +105,6 @@ class Recipe(object):
       anyway, and are available through "configuration" output key.
     instance-state
       The instance state.
-    slapuser
-      The username associated to the slappart.
   """
 
   # XXX: used to detect if a configuration key is a valid section key. This
@@ -125,8 +122,6 @@ class Recipe(object):
           options['configuration.' + key] = value
 
   def fetch_parameter_dict(self, options, instance_root):
-      options['slapuser'] = str(pwd.getpwuid(os.getuid()).pw_name)
-
       slap = slapos.slap.slap()
       slap.initializeConnection(
           options['url'],
