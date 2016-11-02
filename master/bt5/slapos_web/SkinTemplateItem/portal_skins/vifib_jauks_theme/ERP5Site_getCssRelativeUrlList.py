bd29bc8ec17c63475f33a0d62c25d9f58523a13b
Rafael Monnerat <rafael@nexedi.com>
2016-11-02 21:53:44 +0000

    slapos_web: Remove unused code
    getJavascrip and getCss is overwritten under vifib_hosting_theme

--- master/bt5/slapos_web/SkinTemplateItem/portal_skins/vifib_jauks_theme/ERP5Site_getCssRelativeUrlList.py
+++
@@ -1,6 +0,0 @@
-# This script returns an iterable of the paths to standard CSS objects.
-
-return ('km_css/km_listbox.css', 
-        'vifib_style/listbox.css',
-        'vifib_style/vifib.css',
-        'vifib_style/zocial.css')
--- master/bt5/slapos_web/SkinTemplateItem/portal_skins/vifib_jauks_theme/ERP5Site_getJavaScriptRelativeUrlList.py
+++
@@ -1,3 +0,0 @@
-# This script returns an iterable of the paths to standard JavaScript objects.
-
-return ('jquery/core/jquery.js', )
