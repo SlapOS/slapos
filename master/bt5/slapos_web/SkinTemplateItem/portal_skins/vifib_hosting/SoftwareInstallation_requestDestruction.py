3d934f11f2e856865ed12d99874cfc6193d629e1
Rafael Monnerat <rafael@nexedi.com>
2017-02-07 18:05:38 +0000

    slapos_web: SlaposInstallation_requestDestruction was moved into slapos_cloud

--- master/bt5/slapos_web/SkinTemplateItem/portal_skins/vifib_hosting/SoftwareInstallation_requestDestruction.py
+++
@@ -1,2 +0,0 @@
-context.getAggregateValue(portal_type='Computer').requestSoftwareRelease(software_release_url=context.getUrlString(), state='destroyed')
-context.Base_redirect('view', keep_items={'portal_status_message': 'Requested Destruction'})
