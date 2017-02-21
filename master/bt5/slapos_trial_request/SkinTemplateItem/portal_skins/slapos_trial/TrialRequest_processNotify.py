a31fd79b990bf754e2237f50416cbff40947b016
Rafael Monnerat <rafael@nexedi.com>
2017-02-21 10:01:11 +0000

    slapos_trial: Notify using Mail Message
    This records what was sent to the user.

--- master/bt5/slapos_trial_request/SkinTemplateItem/portal_skins/slapos_trial/TrialRequest_processNotify.py
+++ master/bt5/slapos_trial_request/SkinTemplateItem/portal_skins/slapos_trial/TrialRequest_processNotify.py
@@ -9,9 +9,7 @@ person = portal.portal_catalog.getResultValue(
 if person is None:
   return "Free Trial Person not Found"
 
-# connection_key_list = context.getSubjectList() ?
-
-connection_key_list = [ "url", "token" ]
+connection_key_list = context.getSubjectList()
 
 instance = context.getAggregateValue()
 
@@ -47,9 +45,36 @@ if state != "destroy_requested":
     message = message = notification_message.asText(
               substitution_method_parameter_dict={'mapping_dict': mapping_dict})
 
-    mfrom = '%s <%s>' % (person.getTitle(), person.getDefaultEmailText())
+    person_title = "%s FREE TRIAL" % email
+
+    free_trial_destination = portal.portal_catalog.getResultValue(
+       portal_type="Person",
+       title=person_title,
+       reference=None)
+
     
-    portal.MailHost.send(message, mto=email, mfrom=mfrom,
-                                subject=subject, charset="UTF-8")
+    if free_trial_destination is None:
+      free_trial_destination = portal.person_module.newContent(
+         portal_type="Person",
+         title=person_title)
+
+      free_trial_destination.setDefaultEmailText(email)
+
+    event = portal.event_module.newContent(
+       portal_type="Mail Message",
+       start_date=DateTime(),
+       destination=free_trial_destination.getRelativeUrl(),
+       follow_up=context.getRelativeUrl(),
+       source=person.getRelativeUrl(),
+       title=subject,
+       resource="service_module/slapos_crm_information",
+       text_content=message,
+    )
+
+
+    portal.portal_workflow.doActionFor(event, 'start_action', send_mail=True)
+    event.stop()
+    event.deliver()
+    event.reindexObject()
 
     context.validate()
