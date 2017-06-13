dfd4d114cf8002c25210649d64a8a46964b7564a
Tomas Peterka <tomas.peterka@nexedi.com>
2017-06-13 11:23:59 -0400

    [slapconfiguration.test] Replace tabs with spaces

--- slapos/test/recipe/test_slaposconfiguration.py
+++ slapos/test/recipe/test_slaposconfiguration.py
@@ -10,44 +10,44 @@ from slapos import format as slapformat
 
 class SlapConfigurationTest(unittest.TestCase):
 
-	def setUp(self):
-		"""Prepare files on filesystem."""
-		self.instance_root = "/tmp/instance_test_resourcefile"
-		os.mkdir(self.instance_root)
-		# create testing resource file
-		self.resource_file = os.path.join(self.instance_root, slapformat.Partition.resource_file)
-		self.resource = {
-			"tun": {
-				"ipv4": "192.168.0.1"
-			},
-			"address_list": [
-				10, 20
-			]
-		}
-		with open(self.resource_file, "wt") as fo:
-			json.dump(self.resource, fo)
-		# do your tests inside try block and clean up in finally
-		self.buildout = {
-			"buildout": {
-				"directory": self.instance_root
-			}
-		}
+  def setUp(self):
+    """Prepare files on filesystem."""
+    self.instance_root = "/tmp/instance_test_resourcefile"
+    os.mkdir(self.instance_root)
+    # create testing resource file
+    self.resource_file = os.path.join(self.instance_root, slapformat.Partition.resource_file)
+    self.resource = {
+      "tun": {
+        "ipv4": "192.168.0.1"
+      },
+      "address_list": [
+        10, 20
+      ]
+    }
+    with open(self.resource_file, "wt") as fo:
+      json.dump(self.resource, fo)
+    # do your tests inside try block and clean up in finally
+    self.buildout = {
+      "buildout": {
+        "directory": self.instance_root
+      }
+    }
 
-	def tearDown(self):
-		os.unlink(self.resource_file)
-		os.rmdir(self.instance_root)
+  def tearDown(self):
+    os.unlink(self.resource_file)
+    os.rmdir(self.instance_root)
 
-	@mock.patch("slapos.slap.slap")
-	def test_correct_naming(self, MockClient):
-		"""Test correct naming of variables from resource file."""
-		MockClient.initializeConnection.return_value = None
-		MockClient.getInstanceParameterDict.return_value = dict()
-	
-		options = defaultdict(str)
-		recipe = slapconfiguration.Recipe(self.buildout, "slapconfiguration", options)
+  @mock.patch("slapos.slap.slap")
+  def test_correct_naming(self, MockClient):
+    """Test correct naming of variables from resource file."""
+    MockClient.initializeConnection.return_value = None
+    MockClient.getInstanceParameterDict.return_value = dict()
+  
+    options = defaultdict(str)
+    recipe = slapconfiguration.Recipe(self.buildout, "slapconfiguration", options)
 
-		self.assertEqual(options['tun-ipv4'], "192.168.0.1",
+    self.assertEqual(options['tun-ipv4'], "192.168.0.1",
       "Folded attrs should be separated by -")
 
-		self.assertEqual(options['address-list'], [10, 20],
+    self.assertEqual(options['address-list'], [10, 20],
       "All underscores should be replaced with -")
