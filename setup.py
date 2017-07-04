722f28fe1cb52b645b1e5e1cf9bc111b04f94030
Rafael Monnerat <rafael@nexedi.com>
2017-07-04 17:22:49 +0200

    Release slapos.cookbook 1.0.52

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '1.0.51.dev0'
+version = '1.0.52'
 name = 'slapos.cookbook'
 long_description = open("README.rst").read() + "\n" + \
     open("CHANGES.rst").read() + "\n"
