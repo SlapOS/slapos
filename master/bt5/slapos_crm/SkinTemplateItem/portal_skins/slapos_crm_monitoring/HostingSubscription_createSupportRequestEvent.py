72a3ddb3323dd4d03d43315ce51610ff72272f5b
Rafael Monnerat <rafael@nexedi.com>
2017-07-11 16:51:26 +0000

    slapos_crm: Remove whitespaces.

--- master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/HostingSubscription_createSupportRequestEvent.py
+++ master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/HostingSubscription_createSupportRequestEvent.py
@@ -4,7 +4,7 @@ ticket_title = "Hosting Subscription %s is failing." % context.getTitle()
 
 description = "%s contains software instances which are unallocated or reporting errors." % (
         context.getTitle())
-       
+
 support_request = context.Base_generateSupportRequestForSlapOS(
   ticket_title,
   description,
@@ -12,11 +12,11 @@ support_request = context.Base_generateSupportRequestForSlapOS(
 
 if support_request is None:
   return
-  
+
 person = context.getDestinationSectionValue(portal_type="Person")
 if not person:
   return
-  
+
 if support_request.getSimulationState() not in ["validated", "suspended"]:
   support_request.validate()
 
@@ -32,6 +32,6 @@ if notification_message is not None:
 
   message = notification_message.asText(
               substitution_method_parameter_dict={'mapping_dict':mapping_dict})
-  
+
 return support_request.SupportRequest_trySendNotificationMessage(
               ticket_title, message, person.getRelativeUrl())
