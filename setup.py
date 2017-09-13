7989fb5cd0ee1c452c72943cb7cb4d9df3706491
Rafael Monnerat <rafael@nexedi.com>
2017-09-13 09:03:29 +0200

    slapos.cookbook: Start new development cycle

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '1.0.53'
+version = '1.0.54.dev0'
 name = 'slapos.cookbook'
 long_description = open("README.rst").read() + "\n" + \
     open("CHANGES.rst").read() + "\n"
