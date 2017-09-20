21c3fbb65a452ec028cd9f64d11f5c555c34534a
Rafael Monnerat <rafael@nexedi.com>
2017-09-20 13:47:32 +0200

    slapos/test: Bootstrap test for register
      Include a test for fetch configuration

---
+++ slapos/tests/register.py
@@ -0,0 +1,57 @@
+##############################################################################
+#
+# Copyright (c) 2010 Vifib SARL and Contributors. All Rights Reserved.
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsibility of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# guarantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+import unittest
+import slapos.cli.register
+
+class TestRegister(unittest.TestCase):
+  """ Tests for slapos.cli.register
+
+      XXX There is a lack of tests for register, so include more. 
+  """
+
+  def test_fetch_configuration(self):
+    """ Simple test to Fetch the configuration template 
+    """
+    template = slapos.cli.register.fetch_configuration_template()
+    self.assertNotEquals("", template)
+
+    for entry in  ['computer_id', 
+                   'master_url',
+                   'key_file', 
+                   'cert_file',
+                   'certificate_repository_path',
+                   'interface_name', 
+                   'ipv4_local_network',
+                   'partition_amount', 
+                   'create_tap']:
+      self.assertTrue(entry in template, "%s is not in template (%s)" % (entry, template))
+
+
+
+
+
