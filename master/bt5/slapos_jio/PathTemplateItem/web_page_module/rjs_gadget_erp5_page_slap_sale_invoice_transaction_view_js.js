218b8eac36764d614b9e627de26b73d842ae8935
Rafael Monnerat <rafael@nexedi.com>
2018-01-22 18:20:27 +0000

    slapos_jio: Include View gadget forms for Sale Invoice Transaction.

---
+++ master/bt5/slapos_jio/PathTemplateItem/web_page_module/rjs_gadget_erp5_page_slap_sale_invoice_transaction_view_js.js
@@ -0,0 +1,144 @@
+/*global window, rJS, RSVP, jIO, Blob */
+/*jslint nomen: true, indent: 2, maxerr: 3 */
+(function (window, rJS, RSVP, jIO, Blob) {
+  "use strict";
+
+  rJS(window)
+    /////////////////////////////////////////////////////////////////
+    // Acquired methods
+    /////////////////////////////////////////////////////////////////
+    .declareAcquiredMethod("updateHeader", "updateHeader")
+    .declareAcquiredMethod("getUrlParameter", "getUrlParameter")
+    .declareAcquiredMethod("getUrlFor", "getUrlFor")
+    .declareAcquiredMethod("updateDocument", "updateDocument")
+    .declareAcquiredMethod("jio_getAttachment", "jio_getAttachment")
+    .declareAcquiredMethod("jio_putAttachment", "jio_putAttachment")
+    .declareAcquiredMethod("notifySubmitting", "notifySubmitting")
+    .declareAcquiredMethod("notifySubmitted", 'notifySubmitted')
+    .declareAcquiredMethod("jio_allDocs", "jio_allDocs")
+
+    /////////////////////////////////////////////////////////////////
+    // declared methods
+    /////////////////////////////////////////////////////////////////
+
+    .declareMethod("render", function (options) {
+      return this.changeState({
+        jio_key: options.jio_key,
+        doc: options.doc,
+        editable: 1
+      });
+    })
+
+    .onEvent('submit', function () {
+      var gadget = this;
+      return gadget.notifySubmitting()
+        .push(function () {
+          return gadget.getDeclaredGadget('form_view');
+        })
+        .push(function (form_gadget) {
+          return form_gadget.getContent();
+        })
+        .push(function (content) {
+          return gadget.updateDocument(content);
+        })
+        .push(function () {
+          return gadget.notifySubmitted({message: 'Data Updated', status: 'success'});
+        });
+    })
+
+    .declareMethod("triggerSubmit", function () {
+      return this.element.querySelector('button[type="submit"]').click();
+    })
+
+    .onStateChange(function () {
+      var gadget = this, data;
+      return new RSVP.Queue()
+        .push(function () {
+          return gadget.getDeclaredGadget('form_view');
+        })
+        .push(function (form_gadget) {
+          var editable = gadget.state.editable;
+          return form_gadget.render({
+            erp5_document: {
+              "_embedded": {"_view": {
+                "my_title": {
+                  "description": "",
+                  "title": "Title",
+                  "default": gadget.state.doc.title,
+                  "css_class": "",
+                  "required": 1,
+                  "editable": 0,
+                  "key": "title",
+                  "hidden": 0,
+                  "type": "StringField"
+                },
+                "my_reference": {
+                  "description": "",
+                  "title": "Reference",
+                  "default": gadget.state.doc.reference,
+                  "css_class": "",
+                  "required": 1,
+                  "editable": 0,
+                  "key": "reference",
+                  "hidden": 0,
+                  "type": "StringField"
+                },
+                "my_payment_state": {
+                  "description": "",
+                  "title": "Payment State",
+                  "default": {jio_key: gadget.state.jio_key},
+                  "css_class": "",
+                  "required": 1,
+                  "editable": 0,
+                  "url": "gadget_slapos_invoice_state.html",
+                  "sandbox": "",
+                  "key": "monitoring_status",
+                  "hidden": 0,
+                  "type": "GadgetField"
+                },
+                "my_download": {
+                  "description": "",
+                  "title": "Download",
+                  "default": {jio_key: gadget.state.jio_key},
+                  "css_class": "",
+                  "required": 1,
+                  "editable": 0,
+                  "url": "gadget_slapos_invoice_printout.html",
+                  "sandbox": "",
+                  "key": "monitoring_status",
+                  "hidden": 0,
+                  "type": "GadgetField"
+                }
+              }},
+              "_links": {
+                "type": {
+                  // form_list display portal_type in header
+                  name: ""
+                }
+              }
+            },
+            form_definition: {
+              group_list: [[
+                "left",
+                [["my_title"], ["my_reference"], ['my_payment_state'], ["my_download"]]
+              ]]
+            }
+          });
+        })
+        .push(function () {
+          return RSVP.all([
+            gadget.getUrlFor({command: "change", options: {editable: true}})
+          ]);
+        })
+        .push(function (url_list) {
+          var header_dict = {
+            page_title: gadget.state.doc.title,
+            save_action: true
+          };
+          if (!gadget.state.editable) {
+            header_dict.edit_content = url_list[0];
+          }
+          return gadget.updateHeader(header_dict);
+        });
+    });
+}(window, rJS, RSVP, jIO, Blob));
