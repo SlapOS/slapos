1730fc994f458d7876bba7dadddb42d5855e52a1
Rafael Monnerat <rafael@nexedi.com>
2018-02-08 18:15:34 +0000

    slapos_jio: Return empty if it was sofware release is empty

--- master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/SoftwareInstallation_getSoftwareReleaseInformation.py
+++ master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/SoftwareInstallation_getSoftwareReleaseInformation.py
@@ -3,4 +3,7 @@ software_release = context.portal_catalog.getResultValue(
   url_string={'query': context.getUrlString(), 'key': 'ExactMatch'},
   portal_type='Software Release')
 
+if software_release is None:
+  return json.dumps("")
+
 return json.dumps("%s (%s)" % (software_release.getTitle(), software_release.getVersion()))
