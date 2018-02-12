db923b71bad59d8d45118758c5bde28af595de93
Rafael Monnerat <rafael@nexedi.com>
2018-02-12 16:34:19 +0000

    slapos_erp5: Include constraint to migrate/configure OAuth for facebook and google login

---
+++ master/bt5/slapos_erp5/SkinTemplateItem/portal_skins/slapos_erp5/OAuthTool_checkGoogleConnectorExistenceConsistency.py
@@ -0,0 +1,20 @@
+portal_oauth = context.portal_oauth
+error_list = []
+
+
+for connector in portal_oauth.searchFolder(portal_type="Google Connector"):
+  if connector.gerReference() == "default":
+    return []
+
+if fixit:
+  system_preference = context.portal_preferences.getActiveSystemPreference()
+  connector = portal_oauth.newContent(
+    portal_type="Google Connector",
+    reference="default",
+    client_id=getattr(system_preference, "preferred_vifib_google_application_id", None),
+    secret_key=getattr(system_preference, "preferred_vifib_google_application_secret", None)
+  )
+  connector.validate()
+  return []
+
+return ["Default Google Connector is missing."]
