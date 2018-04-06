eb11c99c2b5dc9a8d916e35286688612c73aa1d8
Eteri <eteri.sokhoyan@nexedi.com>
2018-04-06 16:36:12 +0200

    fix path

--- software/fluentTest/testIngestion.py
+++ software/fluentTest/testIngestion.py
@@ -1,11 +1,13 @@
+#!${buildout:directory}/bin/${eggs:interpreter}
+# BEWARE: This file is operated by slapgrid
+# BEWARE: It will be overwritten automatically
+
 import unittest
 import requests
 from StringIO import StringIO
 
-
 class TestPost(unittest.TestCase):
   
-    
     def test_get(self):
         resp = requests.get('http://10.0.46.242:4443')
         self.assertEqual(resp.status_code, 200)
@@ -23,16 +25,14 @@ class TestPost(unittest.TestCase):
         
 
 if __name__ == "__main__":
-   # print("in test")
-    #unittest.main()
-    #suite = unittest.TestLoader().loadTestsFromTestCase(TestPost)
-    #result = unittest.TextTestRunner(verbosity=2).run(suite)
-    
+  
     stream = StringIO()
     runner = unittest.TextTestRunner(stream=stream)
     result = runner.run(unittest.makeSuite(TestPost))
+ 
+    
     print 'Tests run ', result.testsRun
     print 'Errors ', result.errors
-    print(result.failures)
+    print "Failures ", result.failures
     stream.seek(0)
     print 'Test output\n', stream.read() 
