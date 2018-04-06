23206ec97c7d139a626ef2fdef7f7b24edbbeccc
Eteri <eteri.sokhoyan@nexedi.com>
2018-04-06 09:57:33 +0200

    fix test

--- software/fluentTest/testIngestion.py
+++ software/fluentTest/testIngestion.py
@@ -23,5 +23,5 @@ class TestPost(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestPost)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    #suite = unittest.TestLoader().loadTestsFromTestCase(TestPost)
+    #unittest.TextTestRunner(verbosity=2).run(suite)
