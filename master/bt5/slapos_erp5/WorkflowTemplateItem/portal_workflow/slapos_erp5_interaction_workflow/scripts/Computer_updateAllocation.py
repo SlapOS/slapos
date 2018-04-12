20c2d16cd6bb9da735eac0b5efb997ff1bfd6ace
Rafael Monnerat <rafael@nexedi.com>
2018-04-12 16:46:36 +0000

    slapos_erp5: Reimplement Computer_updateAllocationScope into an interaction workflow
    This is for adjust subject_list and capacity_scope of the computer after the computer been edited.

---
+++ master/bt5/slapos_erp5/WorkflowTemplateItem/portal_workflow/slapos_erp5_interaction_workflow/scripts/Computer_updateAllocation.py
@@ -0,0 +1,38 @@
+computer = state_object["object"]
+allocation_scope = computer.getAllocationScope()
+
+if allocation_scope == 'open/public':
+  # Public computer capacity is handle by an alarm
+  capacity_scope = 'close'
+elif allocation_scope.startswith('open'):
+  # Capacity is not handled for 'private' computers
+  capacity_scope = 'open'
+else:
+  capacity_scope = 'close'
+
+edit_kw = {
+  'capacity_scope': capacity_scope,
+}
+
+self_person = computer.getSourceAdministrationValue(portal_type="Person")
+if self_person is None:
+  computer.edit(**edit_kw)
+  return
+
+self_email = self_person.getDefaultEmailCoordinateText()
+if allocation_scope == 'open/public':
+  # reset friends and update in place
+  edit_kw['subject_list'] = ['']
+  edit_kw['destination_section'] = None
+elif allocation_scope == 'open/personal':
+  # reset friends to self and update in place
+  edit_kw['subject_list'] = [self_email]
+  edit_kw['destination_section_value'] = self_person
+else:
+  subject_list = computer.getSubjectList()
+  if self_email not in subject_list:
+    # add self as friend
+    subject_list.append(self_email)
+    edit_kw['subject_list'] = subject_list
+
+computer.edit(**edit_kw)
