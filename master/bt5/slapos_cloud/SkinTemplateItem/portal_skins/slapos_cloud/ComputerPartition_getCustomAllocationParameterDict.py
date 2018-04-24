cfceef599c4727966090d3a12fd3be03fa76b22d
Rafael Monnerat <rafael@nexedi.com>
2018-04-24 16:06:37 +0000

    slapos_cloud: Include frontend default compatibility
    Old software releases may have one old frontend url which leads to no software, this change allows others masters keep their own frontend configuration with existing software releases.

--- master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/ComputerPartition_getCustomAllocationParameterDict.py
+++ master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/ComputerPartition_getCustomAllocationParameterDict.py
@@ -1,3 +1,17 @@
 query_kw = dict()
 
+if software_instance_portal_type == "Slave Instance" and\
+   software_release_url == "http://git.erp5.org/gitweb/slapos.git/blob_plain/HEAD:/software/apache-frontend/software.cfg":
+
+  software_release_list = context.SoftwareProduct_getSortedSoftwareReleaseList(software_product_reference="frontend")
+
+  if len(software_release_list):
+    software_release_document = software_release_list[0]
+
+  query_kw['software_release_url'] = software_release_document.getUrlString()
+
+  # This should be adjusted
+  query_kw['software_type'] = "custom-personal"
+  return query_kw
+
 return query_kw
