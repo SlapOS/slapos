8e4169d608980c4a480e3cd79e527e98c1c4b7f3
Rafael Monnerat <rafael@nexedi.com>
2018-06-11 20:28:36 +0000

    slapos_cloud: Only search for instances which aren't already destroyed

--- master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/Alarm_garbageCollectDestroyedRootTree.py
+++ master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/Alarm_garbageCollectDestroyedRootTree.py
@@ -5,7 +5,8 @@ portal.portal_catalog.searchAndActivate(
   validation_state="validated",
   specialise_validation_state="archived",
   method_id='Instance_tryToGarbageCollect',
-  activate_kw={'tag': tag}
+  activate_kw={'tag': tag},
+  **{"slapos_item.slap_state": ("start_requested", "stop_requested")}
 )
 
 context.activate(after_tag=tag).getId()
