c5d1d27e866028bb83348e339e4aee4cdb841ae6
Jérome Perrin <jerome@nexedi.com>
2018-06-11 11:29:30 +0200

    component/perl: Infrastructure for CPAN packages in inpendent parts
    Installing everything in site_perl is not the "buildout way" and is
    incompatible with the upcoming shared slapos.recipe.cmmi

---
+++ component/perl/perl-CPAN-package-create-wrapper.py
@@ -0,0 +1,33 @@
+import os
+import textwrap
+import glob
+
+def post_make_hook(options, buildout, environmet):
+  prefix = options['prefix']
+  site_perl = options['site_perl']
+  perl_location = options['perl_location']
+  bin_folder = os.path.join(prefix, 'bin')
+  if not os.path.isdir(bin_folder):
+      os.mkdir(bin_folder)
+
+  # install a ./bin/perl wrapper with @INC set
+  perl_wrapper_path = os.path.join(bin_folder, 'perl')
+  with open(perl_wrapper_path, 'w') as wrapper:
+      wrapper.write(textwrap.dedent('''\
+          #!/bin/sh
+          export PERL5LIB="{site_perl}:$PERL5LIB"
+          exec {perl_location}/bin/perl "$@"
+          '''.format(**locals())))
+  os.chmod(perl_wrapper_path, 0755)
+
+  # create a wrapper for each scripts installed in perl-bin
+  for script_path in glob.glob(os.path.join(prefix, 'perl-bin', '*')):
+      script_name = os.path.basename(script_path)
+      wrapper_path = os.path.join(prefix, 'bin', script_name)
+      with open(wrapper_path, 'w') as wrapper:
+          wrapper.write(textwrap.dedent('''\
+              #!/bin/sh
+              export PERL5LIB="{site_perl}:$PERL5LIB"
+              exec {perl_location}/bin/perl {script_path} "$@"
+              '''.format(**locals())))
+      os.chmod(wrapper_path, 0755)
