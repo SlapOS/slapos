41a7a6d80a3366d54469893a3a8b8ac9c63b485b
Lukasz Nowak <luke@nexedi.com>
2018-08-08 11:21:56 +0200

    caddy-frontend: Dependencies on instances
    Create caddyprofiledeps egg with dummy noop recipe.
    
    Thanks to setting dependencies of this egg and enabling it on the instance
    profile, buildout will install eggs during software run and activate them
    during instance run.
    
    No existing egg (like slapos.cookbook) is used, as this technique is to allow
    profile/software release developer to choose required eggs used during
    instantiation.
    
    Another apporach would be to add dependency for validators in
    slapos.recipe.template (in install_requires).

---
+++ software/caddy-frontend/setup.py
@@ -0,0 +1,15 @@
+# The caddyprofiledeps egg allows to set dependecies of the Caddy profiles
+# which are enabled during the instance run, thanks to using caddyprofiledeps
+# recipe
+
+from setuptools import setup
+setup(
+  name='caddyprofiledeps',
+  install_requires=[
+  ],
+  entry_points={
+    'zc.buildout': [
+      'default = caddyprofiledummy:Recipe',
+    ]
+  }
+)
