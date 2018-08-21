235c3bcac68e1bc83654b8fdef2b5455a55c3fb0
Jérome Perrin <jerome@nexedi.com>
2018-08-21 12:10:14 +0200

    fixup! component/perl: Infrastructure for CPAN packages in inpendent parts
    This usage of textwrap.dedent *sometimes* does not work and sometimes
    create files with indentation, so we don't have "#!/bin/sh" as first
    line but "    #!/bin/sh"
    
    Use a "safer" version that should work in all cases.

--- component/perl/perl-CPAN-package-create-wrapper.py
+++ component/perl/perl-CPAN-package-create-wrapper.py
@@ -13,11 +13,10 @@ def post_make_hook(options, buildout, environmet):
   # install a ./bin/perl wrapper with @INC set
   perl_wrapper_path = os.path.join(bin_folder, 'perl')
   with open(perl_wrapper_path, 'w') as wrapper:
-      wrapper.write(textwrap.dedent('''\
-          #!/bin/sh
-          export PERL5LIB="{site_perl}:$PERL5LIB"
-          exec {perl_location}/bin/perl "$@"
-          '''.format(**locals())))
+      wrapper.write('''#!/bin/sh
+export PERL5LIB="{site_perl}:$PERL5LIB"
+exec {perl_location}/bin/perl "$@"
+'''.format(**locals()))
   os.chmod(perl_wrapper_path, 0755)
 
   # create a wrapper for each scripts installed in perl-bin
@@ -25,9 +24,8 @@ def post_make_hook(options, buildout, environmet):
       script_name = os.path.basename(script_path)
       wrapper_path = os.path.join(prefix, 'bin', script_name)
       with open(wrapper_path, 'w') as wrapper:
-          wrapper.write(textwrap.dedent('''\
-              #!/bin/sh
-              export PERL5LIB="{site_perl}:$PERL5LIB"
-              exec {perl_location}/bin/perl {script_path} "$@"
-              '''.format(**locals())))
+          wrapper.write('''#!/bin/sh
+export PERL5LIB="{site_perl}:$PERL5LIB"
+exec {perl_location}/bin/perl {script_path} "$@"
+'''.format(**locals()))
       os.chmod(wrapper_path, 0755)
