b26ab1129e85457774453ba3d69b50c661e95b72
Jérome Perrin <jerome@nexedi.com>
2018-10-01 10:16:30 +0200

    software/erp5testnode/testsuite/seleniumrunner: test suite for selenium runner
    Software just to in install a test runner

--- software/seleniumrunner/test/test.py
+++ software/seleniumrunner/test/test.py
@@ -44,7 +44,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-from .utils import SlapOSInstanceTestCase, findFreeTCPPort
+from utils import SlapOSInstanceTestCase, findFreeTCPPort
 
 debug_mode = os.environ.get('DEBUG')
 # for development: debugging logs and install Ctrl+C handler
