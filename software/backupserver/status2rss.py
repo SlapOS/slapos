63ff94b68a038105ffdaa2f2006d91abb15b5f19
Thomas Gambier <thomas.gambier@nexedi.com>
2018-10-16 14:58:14 +0200

    [backupserver] cleanup the status file
     * keep the start date
     * don't remove the file before writing in it (use > directly) so that the file always exist
     * format the date with ISO 8601
     * format all the lines the same way
    
    This will let the monitor promise read this file easily

--- software/backupserver/status2rss.py
+++ software/backupserver/status2rss.py
@@ -2,7 +2,7 @@ import datetime
 import uuid
 import PyRSS2Gen
 import sys
-from email.utils import parsedate_tz, mktime_tz
+from dateutil.parser import parse
 import base64
 
 # Based on http://thehelpfulhacker.net/2011/03/27/a-rss-feed-for-your-crontabs/
@@ -23,13 +23,13 @@ while 1:
   if not line:
     break
 
-  time, statistic, desc = line.split(',', 2)
+  time, statistic, desc = line.split(', ', 2)
 
   rss_item = PyRSS2Gen.RSSItem(
     title = desc,
     description = "<p>%s</p>" % "<br/>".join(("%s, %s\n<a href='http://www.nongnu.org/rdiff-backup/FAQ.html#statistics'>Lastest statistic</a>\n%s" % (time, desc,
       open(statistic).read())).split("\n")),
-    pubDate = datetime.datetime.fromtimestamp(mktime_tz(parsedate_tz(time))),
+    pubDate = parse(time),
     guid = PyRSS2Gen.Guid(base64.b64encode("%s, %s" % (time, desc)), isPermaLink=0)
     )
   items.append(rss_item)
