aac6c5212d327c0a9d5b069bfff0c98efed8f19f
Lukasz Nowak <luke@nexedi.com>
2019-04-19 15:31:53 +0200

    caddy-frontend/test: Add missing runKedifaUpdater

--- software/caddy-frontend/test/test.py
+++ software/caddy-frontend/test/test.py
@@ -5353,6 +5353,7 @@ class TestSlaveSlapOSMasterCertificateCompatibilityUpdate(
 
     })
     self.runComputerPartition(max_quantity=1)
+    self.runKedifaUpdater()
 
     result = self.fakeHTTPSResult(
       parameter_dict['domain'], parameter_dict['public-ipv4'], 'test-path')
