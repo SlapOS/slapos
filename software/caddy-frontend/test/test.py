a9f168d25bca71f13aeb982a03f2e8f95d6cd663
Lukasz Nowak <luke@nexedi.com>
2019-08-28 15:56:59 +0200

    caddy-frontend: Switch to monitor-promise-base
    As consequence assert that etc/promise is not created anymore and drop
    asserting monitor-bootstrap.pid, as its appearance is totally uncontrollable
    and not important for the tests.

--- software/caddy-frontend/test/test.py
+++ software/caddy-frontend/test/test.py
@@ -360,6 +360,8 @@ class TestDataMixin(object):
       'validate_configuration_state_signature',
       # run by cron from time to time
       'monitor/monitor-collect.pid',
+      # totally non-controllable
+      'monitor-bootstrap.pid',
     ])
 
   def test_file_list_etc_cron_d(self):
@@ -447,20 +449,9 @@ class TestDataMixin(object):
     self.assertTestData('\n'.join(promise_status_list), msg=(msg or None))
 
   def test_promise_run_promise(self):
-    partition_path_list = glob.glob(os.path.join(self.instance_path, '*'))
-    promise_status_list = []
-    for partition_path in sorted(partition_path_list):
-      promise_path_list = sorted(glob.glob(
-          os.path.join(partition_path, 'etc', 'promise', '*')
-      ))
-      for promise_path in promise_path_list:
-        promise_result = subprocess.call([promise_path])
-        promise_status_list.append(
-          '%s: %s' % (
-            promise_path[len(self.instance_path) + 1:],
-            promise_result == 0 and 'OK' or 'ERROR'))
-
-    self.assertTestData('\n'.join(promise_status_list))
+    promise_path_list = glob.glob(
+      os.path.join(self.instance_path, '*', 'etc', 'promise'))
+    self.assertEqual([], promise_path_list)
 
 
 class HttpFrontendTestCase(SlapOSInstanceTestCase):
