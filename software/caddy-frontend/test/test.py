e7a2c5105001f9ef5aaecdd415107fa6394107cc
Lukasz Nowak <luke@nexedi.com>
2019-09-10 09:23:59 +0200

    caddy-frontend: Drop needless request keys
    They comes from list of Slave Instance of some SlapOS Master implementations.

--- software/caddy-frontend/test/test.py
+++ software/caddy-frontend/test/test.py
@@ -1071,6 +1071,8 @@ http://apachecustomhttpsaccepted.example.com:%%(http_port)s {
       },
       'Url': {
         'url': cls.backend_url,
+        'timestamp': '57239487234',
+        'connection-parameter-hash': 'THIS_HASH_SHALL_NOT_APPEAR',
       },
       'url_https-url': {
         'url': cls.backend_url + 'http',
@@ -1494,6 +1496,26 @@ http://apachecustomhttpsaccepted.example.com:%%(http_port)s {
 """,
       result_missing.text
     )
+
+  def test_timestamp_connection_parameter_hash_removed(self):
+    # Here it is checked that keys:
+    #  * timestamp
+    #  * connection-parameter-hash
+    # are not present in the master partition in a place where it requests
+    # other nodes.
+    master_partition_path = '/' + os.path.join(
+      *glob.glob(
+        os.path.join(
+          self.instance_path, '*', 'etc', 'Caddyfile-rejected-slave'
+        )
+      )[0].split('/')[:-2])
+    buildout_file = os.path.join(
+      master_partition_path, 'buildout-switch-softwaretype.cfg')
+    slave_list_line = [
+      q for q in open(buildout_file).readlines()
+      if q.startswith('config-slave-list')][0]
+    self.assertFalse('timestamp' in slave_list_line)
+    self.assertFalse('connection-parameter-hash' in slave_list_line)
 
   def test_url(self):
     parameter_dict = self.assertSlaveBase('Url')
