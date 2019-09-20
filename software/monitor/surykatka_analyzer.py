8cad9cdaf9e4fb47769ac4615d2edf206f66f1e9
Rafael Monnerat <rafael@nexedi.com>
2019-09-20 00:27:04 -0300

    monitor: Edge testing of urls

---
+++ software/monitor/surykatka_analyzer.py
@@ -0,0 +1,54 @@
+import datetime
+import email.utils
+import json
+import subprocess
+import sys
+import time
+
+key = sys.argv[2]
+status = True
+jsoned = json.load(open(sys.argv[1]))
+if key == 'http_query':
+  url = sys.argv[3]
+  http_code = sys.argv[4]
+  ip_list = sys.argv[5:]
+
+  entry_list = [q for q in jsoned[key] if q['url'] == url]
+  if len(entry_list) == 0:
+    print 'no entries for %s' % (url,)
+    status = False
+  entry_list_dump = json.dumps(entry_list, indent=2)
+  http_code_list = [q['status_code'] for q in entry_list]
+  db_ip_list = [q['ip'] for q in entry_list]
+  if not all([http_code == str(q) for q in http_code_list]):
+    print 'http_code %s does not match at least one entry in:\n%s' % (
+      http_code, entry_list_dump)
+    status = False
+  if len(ip_list):
+    if set(ip_list) != set(db_ip_list):
+      print 'ip_list %s differes from:\n%s' % (ip_list, entry_list_dump)
+      status = False
+elif key == 'bot_status':
+  value = jsoned[key][0]
+  value_dump = json.dumps(value, indent=2)
+  if value['text'] != 'loop':
+    print 'Not loop type detected in:\n%s' % (value_dump,)
+    status = False
+  timetuple = email.utils.parsedate(value['date'])
+  last_bot_datetime = datetime.datetime.fromtimestamp(time.mktime(timetuple))
+  utcnow = datetime.datetime.utcnow()
+  delta = utcnow - last_bot_datetime
+  # sanity check
+  if delta < datetime.timedelta(minutes=0):
+    print 'Last bot datetime %s seems in future, UTC now %s' % (
+      last_bot_datetime, utcnow)
+    status = False
+  if delta > datetime.timedelta(minutes=15):
+    print 'Last bot datetime %s is more than 15 minutes old, UTC now %s' % (
+      last_bot_datetime, utcnow)
+    status = False
+else:
+  print 'Unknown key %s' % (key,)
+  status = False
+if not status:
+  sys.exit(1)
--- software/monitor/test/test.py
+++ software/monitor/test/test.py
@@ -26,9 +26,10 @@
 ##############################################################################
 
 import os
-
+import re
+import requests
+import xml.etree.ElementTree as ET
 from slapos.recipe.librecipe import generateHashFromFiles
-
 from slapos.testing.testcase import makeModuleSetUpAndTestCaseClass
 
 setUpModule, SlapOSInstanceTestCase = makeModuleSetUpAndTestCaseClass(
@@ -59,3 +60,193 @@ class ServicesTestCase(SlapOSInstanceTestCase):
       expected_process_name = name.format(hash=h)
 
       self.assertIn(expected_process_name, process_names)
+
+
+class MonitorTestMixin(object):
+  monitor_setup_url_key = 'monitor-setup-url'
+
+  def test_monitor_setup(self):
+    connection_parameter_dict = self\
+      .computer_partition.getConnectionParameterDict()
+    self.assertTrue(
+      self.monitor_setup_url_key in connection_parameter_dict,
+      '%s not in %s' % (self.monitor_setup_url_key, connection_parameter_dict))
+    monitor_setup_url_value = connection_parameter_dict[
+      self.monitor_setup_url_key]
+    monitor_url_match = re.match(r'.*url=(.*)', monitor_setup_url_value)
+    self.assertNotEqual(
+      None, monitor_url_match, '%s not parsable' % (monitor_setup_url_value,))
+    self.assertEqual(1, len(monitor_url_match.groups()))
+    monitor_url = monitor_url_match.groups()[0]
+    monitor_url_split = monitor_url.split('&')
+    self.assertEqual(
+      3, len(monitor_url_split), '%s not splitabble' % (monitor_url,))
+    self.monitor_url = monitor_url_split[0]
+    monitor_username = monitor_url_split[1].split('=')
+    self.assertEqual(
+      2, len(monitor_username), '%s not splittable' % (monitor_username))
+    monitor_password = monitor_url_split[2].split('=')
+    self.assertEqual(
+      2, len(monitor_password), '%s not splittable' % (monitor_password))
+    self.monitor_username = monitor_username[1]
+    self.monitor_password = monitor_password[1]
+
+    opml_text = requests.get(self.monitor_url, verify=False).text
+    opml = ET.fromstring(opml_text)
+
+    body = opml[1]
+    self.assertEqual('body', body.tag)
+
+    outline_list = body[0].findall('outline')
+
+    self.assertEqual(
+      self.monitor_configuration_list,
+      [q.attrib for q in outline_list]
+    )
+
+    expected_status_code_list = []
+    got_status_code_list = []
+    for monitor_configuration in self.monitor_configuration_list:
+      status_code = requests.get(
+          monitor_configuration['url'],
+          verify=False,
+          auth=(self.monitor_username, self.monitor_password)
+        ).status_code
+      expected_status_code_list.append(
+        {
+          'url': monitor_configuration['url'],
+          'status_code': 200
+        }
+      )
+      got_status_code_list.append(
+        {
+          'url': monitor_configuration['url'],
+          'status_code': status_code
+        }
+      )
+    self.assertEqual(
+      expected_status_code_list,
+      got_status_code_list
+    )
+
+
+class EdgeSlaveMixin(MonitorTestMixin):
+  __partition_reference__ = 'edge'
+  instance_max_retry = 20
+
+  @classmethod
+  def getInstanceSoftwareType(cls):
+    return 'edgetest'
+
+  def requestEdgetestSlave(self, partition_reference, partition_parameter_kw):
+    software_url = self.getSoftwareURL()
+    self.slap.request(
+      software_release=software_url,
+      software_type='edgetest',
+      partition_reference=partition_reference,
+      partition_parameter_kw=partition_parameter_kw,
+      shared=True
+    )
+
+  def setUp(self):
+    self.bot_partition_path = os.path.join(
+      self.slap.instance_directory,
+      self.__partition_reference__ + '1')
+    self.monitor_configuration_list = [
+      {
+        'xmlUrl': 'https://[%s]:9700/public/feed' % (self._ipv6_address,),
+        'version': 'RSS',
+        'title': 'testing partition 0',
+        'url': 'https://[%s]:9700/share/private/' % (self._ipv6_address,),
+        'text': 'testing partition 0',
+        'type': 'rss',
+        'htmlUrl': 'https://[%s]:9700/public/feed' % (self._ipv6_address,)
+      },
+      {
+        'xmlUrl': 'https://[%s]:9701/public/feed' % (self._ipv6_address,),
+        'version': 'RSS',
+        'title': 'edgebot-1',
+        'url': 'https://[%s]:9701/share/private/' % (self._ipv6_address,),
+        'text': 'edgebot-1',
+        'type': 'rss',
+        'htmlUrl': 'https://[%s]:9701/public/feed' % (self._ipv6_address,)
+      }
+    ]
+
+  def assertSurykatkaIni(self):
+    surykatka_ini = open(
+      os.path.join(
+        self.bot_partition_path, 'etc', 'surykatka.ini')).read().strip()
+
+    expected = self.surykatka_ini % dict(
+        partition_path=self.bot_partition_path)
+    self.assertEqual(
+      expected.strip(),
+      surykatka_ini)
+
+  def initiateSurykatkaRun(self):
+    """
+    Runs instance and force-runs surykatka-report-json to not wait for cron"""
+    try:
+      self.slap.waitForInstance(max_retry=2)
+    except Exception:
+      pass
+
+    self.assertEqual(
+      0,
+      os.system(
+        os.path.join(self.bot_partition_path, 'bin', 'surykatka-status-json'))
+    )
+    self.slap.waitForInstance()
+
+
+class TestEdge(EdgeSlaveMixin, SlapOSInstanceTestCase):
+  surykatka_ini = """[SURYKATKA]
+INTERVAL = 1
+SQLITE = %(partition_path)s/srv/surykatka.db
+URL =
+  https://www.erp5.com/"""
+
+  @classmethod
+  def getInstanceParameterDict(cls):
+    return {
+      'internal_surykatka_interval': '1',
+    }
+
+  def test(self):
+    self.requestEdgetestSlave(
+      'backend',
+      {'url': 'https://www.erp5.com/'},
+    )
+
+    self.initiateSurykatkaRun()
+
+    self.assertSurykatkaIni()
+
+
+class TestEdgeDnsCheckFrontendIp(EdgeSlaveMixin, SlapOSInstanceTestCase):
+  surykatka_ini = """[SURYKATKA]
+INTERVAL = 1
+SQLITE = %(partition_path)s/srv/surykatka.db
+DNS =
+  8.8.8.8
+
+URL =
+  https://www.erp5.com/"""
+
+  @classmethod
+  def getInstanceParameterDict(cls):
+    return {
+      'dns': '8.8.8.8',
+      'check_frontend_ip': cls._ipv4_address,
+      'internal_surykatka_interval': '1',
+    }
+
+  def test(self):
+    self.requestEdgetestSlave(
+      'backend',
+      {'url': 'https://www.erp5.com/'},
+    )
+
+    self.initiateSurykatkaRun()
+    self.assertSurykatkaIni()
