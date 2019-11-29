036e518b51f0f20b0ce4abc79d6edfdc36a5241b
Julien Muchembled <jm@nexedi.com>
2019-11-29 16:31:56 +0100

    ERP5: new parameters to override ZODB settings for any zope

--- software/erp5/test/test/__init__.py
+++ software/erp5/test/test/__init__.py
@@ -46,3 +46,13 @@ class ERP5InstanceTestCase(SlapOSInstanceTestCase):
     """Return the output paramters from the root partition"""
     return json.loads(
         self.computer_partition.getConnectionParameterDict()['_'])
+
+  def getComputerPartition(self, partition_reference):
+    for computer_partition in self.slap.computer.getComputerPartitionList():
+      if partition_reference == computer_partition.getInstanceParameter(
+          'instance_title'):
+        return computer_partition
+
+  def getComputerPartitionPath(self, partition_reference):
+    partition_id = self.getComputerPartition(partition_reference).getId()
+    return os.path.join(self.slap._instance_root, partition_id)
