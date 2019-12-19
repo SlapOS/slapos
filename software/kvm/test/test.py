c0794c5faf12f27de68beacd03fd220fc770b093
Thomas Gambier <thomas.gambier@nexedi.com>
2019-12-19 17:08:57 +0100

    kvm: NDB server instance is OK even if no image is uploaded
    NBD server is not running until an image is uploaded. Make sure that the
    promise doesn't fail until the image is there. Also display a WARNING in
    the connection parameter to say that the NBD server is not running.

--- software/kvm/test/test.py
+++ software/kvm/test/test.py
@@ -308,3 +308,22 @@ class TestInstanceResilient(InstanceTestCase):
         'takeover-kvm-1-password',
         'takeover-kvm-1-url',
         'url']))
+
+class TestInstanceNbdServer(InstanceTestCase):
+  __partition_reference__ = 'ins'
+  instance_max_retry = 5
+
+  @classmethod
+  def getInstanceSoftwareType(cls):
+    return 'nbd'
+
+  def test(self):
+    connection_parameter_dict = self.computer_partition\
+      .getConnectionParameterDict()
+    result = requests.get(connection_parameter_dict['upload_url'].strip(), verify=False)
+    self.assertEqual(
+      httplib.OK,
+      result.status_code
+    )
+    self.assertIn('<title>Upload new File</title>', result.text)
+    self.assertIn("WARNING", connection_parameter_dict['status_message'])
