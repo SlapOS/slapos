SlapOS/slapos

commit 8725ab1ae535c2e0d4bab152a8a9607af4b5e512

slaprunner: use switch-softwaretype instead of softwaretype

stack/resilient/buildout.hash.cfg
@@ -26,7 +26,7 @@ md5sum = 2b0c71b085cfe8017f28098c160b1f49
 
 [template-pull-backup]
 filename = instance-pull-backup.cfg.in
-md5sum = 0bbe16f3d805afd880a251a4f40ecaf1
+md5sum = 555d528b198564f0ce1e94db1160ebf3
 
 [template-replicated]
 filename = template-replicated.cfg.in
@@ -38,7 +38,7 @@ md5sum = 071b1034ee8f5cc14f79b16fdeba2813
 
 [template-resilient-templates]
 filename = template-resilient-templates.cfg.in
-md5sum = 41e82859dc6b65e94a300a006d51536e
+md5sum = 097a14371efde11465ab4bd08ef3131b
 
 [instance-frozen]
 filename = instance-frozen.cfg.in

stack/resilient/instance-pull-backup.cfg.in
@@ -19,10 +19,6 @@ parts =
 extends =
   ${monitor2-template:rendered}
   ${template-logrotate-base:rendered}
-eggs-directory = ${buildout:eggs-directory}
-develop-eggs-directory = ${buildout:develop-eggs-directory}
-offline = true
-
 
 #----------------
 #--
@@ -90,7 +86,7 @@ rendered = $${:wrapper}
 wrapper = $${basedirectory:services}/notifier
 mode = 0700
 command = ${buildout:bin-directory}/pubsubserver --callbacks $${directory:notifier-callbacks} --feeds $${directory:notifier-feeds} --equeue-socket $${equeue:socket} --logfile $${basedirectory:log}/notifier.log $${:host} $${:port}
-host = $${slap-network-information:global-ipv6}
+host = {{ ipv6 }}
 port = $${notifier-port:port}
 context =
   key content notifier:command
@@ -128,8 +124,8 @@ run-directory = $${basedirectory:run}
 pull-push-maximum-run = 5
 # XXX: this should be named "notifier-host"
 notifier-url = http://[$${notifier:host}]:$${notifier:port}
-slave-instance-list = $${slap-parameter:slave_instance_list}
-ignore-known-hosts-file = $${slap-parameter:ignore-known-hosts-file}
+slave-instance-list = {{ slapparameter_dict.get('slave_instance_list', '[]') }}
+ignore-known-hosts-file = {{ slapparameter_dict.get('ignore-known-hosts-file', 'false') }}
 # To get a verbose feed about PBS state
 instance-root-name = $${monitor-instance-parameter:root-instance-title}
 log-url = $${monitor-publish-parameters:monitor-base-url}/private/notifier/
@@ -194,20 +190,6 @@ identity-file = $${:home}/id_rsa
 command-line = ${openssh:location}/bin/ssh -T -o "UserKnownHostsFile $${pbs:known-hosts}" -i $${:identity-file}
 wrapper-path = $${rootdirectory:bin}/ssh
 
-
-#----------------
-#--
-#-- Slave instance list (empty default).
-
-[slap-parameter]
-slave_instance_list = []
-ignore-known-hosts-file = false
-monitor-cors-domains = 
-monitor-httpd-port = 8070
-monitor-title = PBS Instance
-monitor-password = $${monitor-htpasswd:passwd}
-monitor-username = admin
-
 #----------------
 #--
 #-- Resiliency promises.
@@ -269,11 +251,11 @@ monitor-password = $${monitor-publish-parameters:monitor-password}
 #-- Monitor
 
 [monitor-instance-parameter]
-monitor-httpd-port = $${slap-parameter:monitor-httpd-port}
-monitor-title = $${slap-parameter:monitor-title}
-cors-domains = $${slap-parameter:monitor-cors-domains}
-username = $${slap-parameter:monitor-username}
-password = $${slap-parameter:monitor-password}
+monitor-httpd-port = {{ slapparameter_dict.get('monitor-httpd-port', 8070) }}
+monitor-title = {{ slapparameter_dict.get('monitor-title', 'PBS Instance') }}
+cors-domains = {{ slapparameter_dict.get('monitor-cors-domains', '') }}
+username = {{ slapparameter_dict.get('monitor-username', 'admin') }}
+password = {{ slapparameter_dict.get('monitor-password', '$${monitor-htpasswd:passwd}') }}
 
 [monitor-conf-parameters]
 private-path-list += 

stack/resilient/template-resilient-templates.cfg.in
@@ -8,3 +8,20 @@ context =
   raw pbsready_template_path ${pbsready:output}
   raw bash_executable_location ${bash:location}/bin/bash
   raw logrotate_executable_location ${logrotate:location}/usr/sbin/logrotate
+
+[instance-frozen]
+recipe = slapos.recipe.template:jinja2
+template = ${instance-frozen:output}
+rendered = $${buildout:directory}/template-frozen.cfg
+extensions = jinja2.ext.do
+context =
+  key slapparameter_dict slap-configuration:configuration
+
+[template-pull-backup]
+recipe = slapos.recipe.template:jinja2
+template = ${template-pull-backup:output}
+rendered = $${buildout:directory}/template-pull-backup.cfg
+extensions = jinja2.ext.do
+context =
+  key slapparameter_dict slap-configuration:configuration
+  key ipv6 slap-configuration:ipv6-random

software/slaprunner/buildout.hash.cfg
@@ -14,11 +14,11 @@
 # not need these here).
 [template]
 filename = instance.cfg
-md5sum = 8b78e32b877d591400746ec7fd68ed4c
+md5sum = ec70348dd71b319590a5c5837f3d2e45
 
 [template-runner]
 filename = instance-runner.cfg
-md5sum = 99f2c1e0ff1668e57a242a77a33b4d23
+md5sum = b20894f378530e79c847ddfb61782cc5
 
 [template-runner-import-script]
 filename = template/runner-import.sh.jinja2

software/slaprunner/instance-runner.cfg
@@ -51,9 +51,9 @@ extends =
   ${monitor2-template:rendered}
   ${template-logrotate-base:rendered}
 
-eggs-directory = ${buildout:eggs-directory}
-develop-eggs-directory = ${buildout:develop-eggs-directory}
-offline = true
+[slap-network-information]
+local-ipv4 = $${slap-configuration:ipv4-random}
+global-ipv6 = $${slap-configuration:ipv6-random}
 
 {% if slapparameter_dict.get('custom-frontend-backend-url') -%}
 [request-custom-frontend]
@@ -659,6 +659,9 @@ monitor-cors-domains =
 monitor-interface-url =
 monitor-httpd-port = 8386
 buildout-shared-folder = $${runnerdirectory:home}/shared
+{% for k, v in slapparameter_dict.items() -%}
+{{ k }} = {{ v }}
+{% endfor -%}
 
 [slapos-cfg]
 recipe = slapos.recipe.template:jinja2
@@ -738,15 +741,6 @@ command = if [ ! -f $${:software-type-path} -a "$${slap-parameter:slapos-softwar
 recipe = plone.recipe.command
 stop-on-error = true
 command = SR=$${slap-parameter:slapos-software} && if [ -n "$SR" ] && [ ! -f "$${directory:etc}/.project" ]; then echo workspace/slapos/$${slap-parameter:slapos-software}/ > $${directory:etc}/.project; fi
-
-[slap-configuration]
-recipe = slapos.cookbook:slapconfiguration.serialised
-computer = $${slap-connection:computer-id}
-partition = $${slap-connection:partition-id}
-url = $${slap-connection:server-url}
-key = $${slap-connection:key-file}
-cert = $${slap-connection:cert-file}
-
 
 [minishell-cwd]
 recipe = plone.recipe.command

software/slaprunner/instance.cfg
@@ -9,15 +9,18 @@ extends =
   ${template-resilient-templates:output}
 
 [switch_softwaretype]
-recipe = slapos.cookbook:softwaretype
-default = $${instance-base-runner:rendered}
-resilient = $${instance-resilient:rendered}
-runner = $${instance-base-runner:rendered}
-runner-import = $${template-runner-import:rendered}
-runner-export = $${template-runner-export:rendered}
+recipe = slapos.cookbook:switch-softwaretype
+default = $${:runner}
+resilient = instance-resilient:rendered
+runner = instance-base-runner:rendered
+runner-import = template-runner-import:rendered
+runner-export = template-runner-export:rendered
 
-frozen = ${instance-frozen:output}
-pull-backup = ${template-pull-backup:output}
+frozen = instance-frozen:rendered
+pull-backup = template-pull-backup:rendered
+
+# BBB
+RootSoftwareInstance = $${:default}
 
 [instance-base-runner]
 recipe = slapos.recipe.template:jinja2

commit a78cda7fa489bc7846009742b597998227f946c4

resilience: fix after switching to switch-softwaretype recipe
This was broken in 8725ab1ae535c2e0d4bab152a8a9607af4b5e512.

 * recipe/pbs: slave-instance-list is already a list of JSON
 * slave-instance-list is not a configuration but directly a parameter
from slapos

slapos/recipe/pbs.py
@@ -57,7 +57,7 @@ class Recipe(GenericSlapRecipe, Notify, Callback):
   def _options(self, options):
     options['rdiff-backup-data-folder'] = ""
     if 'slave-instance-list' in options:
-      for slave in json.loads(options['slave-instance-list']):
+      for slave in options['slave-instance-list']:
         if slave['type'] == 'pull':
           options['rdiff-backup-data-folder'] = str(os.path.join(options['directory'], slave['name'], 'rdiff-backup-data'))
 
@@ -299,8 +299,7 @@ class Recipe(GenericSlapRecipe, Notify, Callback):
 
     if self.optionIsTrue('client', True):
       self.logger.info("Client mode")
-
-      slaves = json.loads(self.options['slave-instance-list'])
+      slaves = self.options['slave-instance-list']
       known_hosts = KnownHostsFile(self.options['known-hosts'])
       with known_hosts:
         for slave in slaves:

stack/resilient/buildout.hash.cfg
@@ -26,7 +26,7 @@ md5sum = 2b0c71b085cfe8017f28098c160b1f49
 
 [template-pull-backup]
 filename = instance-pull-backup.cfg.in
-md5sum = 555d528b198564f0ce1e94db1160ebf3
+md5sum = 942cfb82b9cd129faf2a31e598fa3362
 
 [template-replicated]
 filename = template-replicated.cfg.in
@@ -38,7 +38,7 @@ md5sum = 071b1034ee8f5cc14f79b16fdeba2813
 
 [template-resilient-templates]
 filename = template-resilient-templates.cfg.in
-md5sum = 097a14371efde11465ab4bd08ef3131b
+md5sum = 99e1444f2978717ce0427767fb078945
 
 [instance-frozen]
 filename = instance-frozen.cfg.in

stack/resilient/instance-pull-backup.cfg.in
@@ -124,7 +124,7 @@ run-directory = $${basedirectory:run}
 pull-push-maximum-run = 5
 # XXX: this should be named "notifier-host"
 notifier-url = http://[$${notifier:host}]:$${notifier:port}
-slave-instance-list = {{ slapparameter_dict.get('slave_instance_list', '[]') }}
+slave-instance-list = {{ dumps(slave_instance_list) }}
 ignore-known-hosts-file = {{ slapparameter_dict.get('ignore-known-hosts-file', 'false') }}
 # To get a verbose feed about PBS state
 instance-root-name = $${monitor-instance-parameter:root-instance-title}

stack/resilient/template-resilient-templates.cfg.in
@@ -23,5 +23,6 @@ template = ${template-pull-backup:output}
 rendered = $${buildout:directory}/template-pull-backup.cfg
 extensions = jinja2.ext.do
 context =
+  key slave_instance_list slap-configuration:slave-instance-list
   key slapparameter_dict slap-configuration:configuration
   key ipv6 slap-configuration:ipv6-random