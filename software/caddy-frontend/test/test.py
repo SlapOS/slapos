3c29b879233312a864aa26c9bb888e5fd96a49be
Łukasz Nowak <luke@nexedi.com>
2020-07-21 14:50:46 +0200

    caddy-frontend: Ensure logs are correctly linked
    The logrotate folder also serves as an publication folder for logs, so be sure
    that logs are linked there.

--- software/caddy-frontend/test/test.py
+++ software/caddy-frontend/test/test.py
@@ -711,6 +711,14 @@ class HttpFrontendTestCase(SlapOSInstanceTestCase):
       result.status_code,
       'While accessing %r of %r the status code was %r' % (
         url, frontend, result.status_code))
+    self.assertEqual(
+      httplib.OK,
+      requests.get(url + 'access.log', verify=False).status_code
+    )
+    self.assertEqual(
+      httplib.OK,
+      requests.get(url + 'error.log', verify=False).status_code
+    )
 
   def assertKedifaKeysWithPop(self, parameter_dict, prefix=''):
     generate_auth_url = parameter_dict.pop('%skey-generate-auth-url' % (
