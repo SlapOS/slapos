8c60bc15a98e6ef2d45d89e1173a1910f9010cb1
Łukasz Nowak <luke@nexedi.com>
2020-07-23 17:39:45 +0200

    fixup! XXX caddy-frontend: Expose backend log files to slaves

--- software/caddy-frontend/test/test.py
+++ software/caddy-frontend/test/test.py
@@ -1739,9 +1739,9 @@ class TestSlave(SlaveHttpFrontendTestCase, TestDataMixin):
     log_regexp = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+ ' \
                  r'\[\d{2}\/.{3}\/\d{4}\:\d{2}\:\d{2}\:\d{2}.\d{3}\] ' \
                  r'http-backend _Url-http\/backend ' \
-                 r'\d+/\\d+\/\d+\/\d+\/\d+ ' \
+                 r'\d+/\d+\/\d+\/\d+\/\d+ ' \
                  r'200 \d+ - - ---- ' \
-                 r'\d\/\d\/\d\/\d\/\d \d\/\d' \
+                 r'\d\/\d\/\d\/\d\/\d \d\/\d ' \
                  r'"GET /test-path/deeper HTTP/1.1"'
 
     self.assertRegexpMatches(
