9f356b9b33cd37e7e7203865404db00c20567e9e
Łukasz Nowak <luke@nexedi.com>
2020-09-29 12:47:37 +0200

    software/cdn: Welcome CDN software
    It's currently based on caddy-frontend, but will evolve separately.

---
+++ software/cdn/setup.py
@@ -0,0 +1,18 @@
+# The caddyprofiledeps egg allows to set dependecies of the Caddy profiles
+# which are enabled during the instance run, thanks to using caddyprofiledeps
+# recipe
+
+from setuptools import setup
+setup(
+  name='caddyprofiledeps',
+  install_requires=[
+    'validators',
+    'furl',
+    'orderedmultidict',
+  ],
+  entry_points={
+    'zc.buildout': [
+      'default = caddyprofiledummy:Recipe',
+    ]
+  }
+)
