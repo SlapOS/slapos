3659dff7842da0899dde54c5ecb6af0cb3e4e91f
Leo-Paul Geneau <leo-paul.geneau@nexedi.com>
2020-10-16 15:43:30 +0200

    writes string file rather than binary file

--- software/proftpd/test/test.py
+++ software/proftpd/test/test.py
@@ -96,7 +96,7 @@ class TestSFTPOperations(ProFTPdTestCase):
   def test_simple_sftp_session(self):
     with self._getConnection() as sftp:
       # put a file
-      with tempfile.NamedTemporaryFile() as f:
+      with tempfile.NamedTemporaryFile(mode='w') as f:
         f.write("Hello FTP !")
         f.flush()
         sftp.put(f.name, remotepath='testfile')
