4bace83e97f726775e5d5c7c5cf3c57d8210048d
Łukasz Nowak <luke@nexedi.com>
2020-10-27 17:03:22 +0100

    XXX experiment

--- software/caddy-frontend/caddyprofiledummy.py
+++ software/caddy-frontend/caddyprofiledummy.py
@@ -1,3 +1,5 @@
+import json
+
 class Recipe(object):
   def __init__(self, *args, **kwargs):
     pass
@@ -7,3 +9,17 @@ class Recipe(object):
 
   def update(self):
     return self.install()
+
+class CdnSlaveParse(object):
+  def __init__(self, buildout, name, options):
+    self.options = options
+
+  def install(self):
+    output = self.options['output']
+    with open(output, 'w') as fh:
+      json.dump(self.options['slave-information'], fh, indent=2)
+    return [output]
+
+  def update():
+    # nothing to do during update
+    return []
