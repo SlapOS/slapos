587e451f7721dc22ae7089c0bd0d9cad1e19fb48
Xavier Thompson <xavier.thompson@nexedi.com>
2021-01-08 23:47:50 +0100

    software/jupyter: Add frontend

--- software/jupyter/test/test.py
+++ software/jupyter/test/test.py
@@ -48,10 +48,14 @@ class TestJupyter(InstanceTestCase):
     except Exception as e:
       self.fail("Can't parse json in %s, error %s" % (parameter_dict['_'], e))
 
+    self.assertTrue('password' in connection_dict)
+    password = connection_dict['password']
+
     self.assertEqual(
       {
         'jupyter-classic-url': 'https://[%s]:8888/tree' % (self._ipv6_address, ),
         'jupyterlab-url': 'https://[%s]:8888/lab' % (self._ipv6_address, ),
+        'password': '%s' % (password, ),
         'url': 'https://[%s]:8888/tree' % (self._ipv6_address, )
       },
       connection_dict
@@ -74,6 +78,69 @@ class TestJupyter(InstanceTestCase):
 
     result = requests.get(
       connection_dict['jupyterlab-url'],
+      verify=False, allow_redirects=False)
+    self.assertEqual(
+      [http.client.FOUND, True, '/login?next=%2Flab'],
+      [result.status_code, result.is_redirect, result.headers['Location']]
+    )
+
+
+class TestJupyterAdditional(InstanceTestCase):
+
+  @classmethod
+  def getInstanceParameterDict(cls):
+    return {
+      'frontend-additional-instance-guid': 'SOMETHING'
+    }
+
+  def test(self):
+    parameter_dict = self.computer_partition.getConnectionParameterDict()
+    self.assertTrue('_' in parameter_dict)
+    try:
+      connection_dict = json.loads(parameter_dict['_'])
+    except Exception as e:
+      self.fail("Can't parse json in %s, error %s" % (parameter_dict['_'], e))
+
+    result = requests.get(
+      connection_dict['url'], verify=False, allow_redirects=False)
+    self.assertEqual(
+      [http.client.FOUND, True, '/login?next=%2Ftree'],
+      [result.status_code, result.is_redirect, result.headers['Location']]
+    )
+
+    result = requests.get(
+      connection_dict['jupyter-classic-url'],
+      verify=False, allow_redirects=False)
+    self.assertEqual(
+      [http.client.FOUND, True, '/login?next=%2Ftree'],
+      [result.status_code, result.is_redirect, result.headers['Location']]
+    )
+
+    result = requests.get(
+      connection_dict['jupyterlab-url'],
+      verify=False, allow_redirects=False)
+    self.assertEqual(
+      [http.client.FOUND, True, '/login?next=%2Flab'],
+      [result.status_code, result.is_redirect, result.headers['Location']]
+    )
+
+    result = requests.get(
+      connection_dict['url-additional'], verify=False, allow_redirects=False)
+    self.assertEqual(
+      [http.client.FOUND, True, '/login?next=%2Ftree'],
+      [result.status_code, result.is_redirect, result.headers['Location']]
+    )
+
+    result = requests.get(
+      connection_dict['jupyter-classic-url-additional'],
+      verify=False, allow_redirects=False)
+    self.assertEqual(
+      [http.client.FOUND, True, '/login?next=%2Ftree'],
+      [result.status_code, result.is_redirect, result.headers['Location']]
+    )
+
+    result = requests.get(
+      connection_dict['jupyterlab-url-additional'],
       verify=False, allow_redirects=False)
     self.assertEqual(
       [http.client.FOUND, True, '/login?next=%2Flab'],
