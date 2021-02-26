505e9238416dc8c6cf2cb688dc81c10f549f06ec
Łukasz Nowak <luke@nexedi.com>
2021-02-26 15:54:59 +0100

    monitor: Update schema for new edgetest features
    json-in-xml became default serialization.
    
    Feature creep of not important defaults has been dropped.
    
    squash! monitor: Update schema for new edgetest features
    
    A lot have to be changed.
    Hacks needed to suport json-in-xml buildout requests.
    
    squash! monitor: Update schema for new edgetest features
    
    Even more stabilisation.

--- software/monitor/test/test.py
+++ software/monitor/test/test.py
@@ -147,7 +147,7 @@ class EdgeSlaveMixin(MonitorTestMixin):
       software_release=software_url,
       software_type='edgetest',
       partition_reference=partition_reference,
-      partition_parameter_kw=partition_parameter_kw,
+      partition_parameter_kw={'_': json.dumps(partition_parameter_kw)},
       shared=True
     )
 
@@ -356,10 +356,14 @@ URL =
 
   @classmethod
   def getInstanceParameterDict(cls):
-    return {
-      'nameserver': '127.0.1.1 127.0.1.2',
-      'check-frontend-ip': '127.0.0.1 127.0.0.2',
-    }
+    return {'_': json.dumps({
+      "region-dict": {
+        "Default": {
+          'nameserver-list': ['127.0.1.1', '127.0.1.2'],
+          'check-frontend-ip-list': ['127.0.0.1', '127.0.0.2'],
+        }
+      }
+    })}
 
   def assertSurykatkaPromises(self):
     self.assertPromiseContent(
@@ -406,9 +410,8 @@ URL =
 
   @classmethod
   def getInstanceParameterDict(cls):
-    return {
-      'check-status-code': '500',
-    }
+    return {'_': json.dumps({
+    })}
 
   def assertSurykatkaPromises(self):
     self.assertPromiseContent(
@@ -483,10 +486,8 @@ URL =
 
   @classmethod
   def getInstanceParameterDict(cls):
-    return {
-      'check-http-header-dict':
-        '{"B": "BBB"}',
-    }
+    return {'_': json.dumps({
+    })}
 
   def assertSurykatkaPromises(self):
     self.assertPromiseContent(
@@ -568,9 +569,8 @@ URL =
 
   @classmethod
   def getInstanceParameterDict(cls):
-    return {
-      'check-certificate-expiration-days': '10',
-    }
+    return {'_': json.dumps({
+    })}
 
   def assertSurykatkaPromises(self):
     self.assertPromiseContent(
@@ -658,9 +658,8 @@ URL =
 
   @classmethod
   def getInstanceParameterDict(cls):
-    return {
-      'check-maximum-elapsed-time': '5',
-    }
+    return {'_': json.dumps({
+    })}
 
   def assertSurykatkaPromises(self):
     self.assertPromiseContent(
@@ -766,9 +765,8 @@ URL =
 
   @classmethod
   def getInstanceParameterDict(cls):
-    return {
-      'failure-amount': '5'
-    }
+    return {'_': json.dumps({
+    })}
 
   def assertSurykatkaPromises(self):
     self.assertPromiseContent(
