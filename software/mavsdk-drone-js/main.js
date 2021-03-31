99c8d230697eab5af810a1245468a177470b08aa
Johan Hugé <johan.huge@nexedi.com>
2021-03-31 14:10:57 +0200

    Update main.js in mavsdk-drone-js

--- software/mavsdk-drone-js/main.js
+++ software/mavsdk-drone-js/main.js
@@ -1,21 +1,41 @@
 import * as mavsdk from "mavsdk";
 import * as os from "os";
 
+
 function fly_drone() {
-	if(mavsdk.start("udp://192.168.99.23:7909") != 0) {
+	if(mavsdk.start("udp://169.0.1.2:7909") != 0) {
 		return;
 	}
+	os.sleep(1000);
 
 	if(mavsdk.arm() != 0) {
 		return;
 	}
+	os.sleep(1000);
 
 	if(mavsdk.takeOff() != 0) {
 		return;
 	}
+	os.sleep(80000);
 
-	// Fly in circles for 10 seconds
-	os.sleep(10000);
+        console.log("setTargetAltitude(100)");
+	if(mavsdk.setTargetAltitude(100) != 0) {
+		return;
+	}
+
+	os.sleep(80000);
+        console.log("setTargetAltitude(30)");
+	if(mavsdk.setTargetAltitude(30) != 0) {
+		return;
+	}
+	os.sleep(80000);
+        console.log("setTargetAltitude(80)");
+	if(mavsdk.setTargetAltitude(80) != 0) {
+		return;
+	}
+
+	// Fly in circles for 5 minutes
+	os.sleep(300000);
 
 	if(mavsdk.land() != 0) {
 		return;
