9b5d112f62bdaf578a5cb5fb14ebd6014175b31a
Jérome Perrin <jerome@nexedi.com>
2021-05-20 06:52:05 +0200

    component/tesseract: version up 4.1.1
    Also enable shared parts and provide new languages:
    Simplied Chinese, Japanese and French

---
+++ component/tesseract/download-tessdata.py
@@ -0,0 +1,26 @@
+# This is a post-make hook script to download tesseract training data.
+#
+# This script uses the following buildout options:
+#   - tessdata-urls: list of URLs and their expected md5sum as URL fragments
+#   - tessdata-location: path where to install the data.
+
+import zc.buildout
+import os
+
+
+def post_make_hook(options, buildout, env):
+  os.makedirs(options['tessdata-location'])
+
+  download = zc.buildout.download.Download(
+      buildout['buildout'],
+      hash_name=True,
+  )
+  for url in options['tessdata-urls'].splitlines():
+    url, _, md5sum = url.partition('#')
+    if url:
+      download(
+          url,
+          md5sum=md5sum,
+          path=os.path.join(options['tessdata-location'],
+                            os.path.basename(url)),
+      )
