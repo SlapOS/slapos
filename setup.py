3c211d70c9cf1d1a14020a432c963afcd70bd0d7
Thomas Gambier <thomas.gambier@nexedi.com>
2021-06-04 14:21:19 +0200

    Release slapos.cookbook (1.0.197)

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '1.0.184'
+version = '1.0.197'
 name = 'slapos.cookbook'
 long_description = open("README.rst").read()
 
