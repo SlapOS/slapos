687999a76facc20c37c818a331254a91a2bd0434
lu.xu <lu.xu@nexedi.com>
2021-06-09 17:29:41 +0200

    software/hugo: Add Hugo software in slapos

---
+++ software/hugo/test/test.py
@@ -0,0 +1,59 @@
+  ##############################################################################
+#
+# Copyright (c) 2021 Nexedi SA and Contributors. All Rights Reserved.
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsibility of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# guarantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+import os
+import requests
+from urlparse import urlparse
+
+from slapos.testing.testcase import makeModuleSetUpAndTestCaseClass
+
+setUpModule, SlapOSInstanceTestCase = makeModuleSetUpAndTestCaseClass(
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), '..', 'software.cfg')))
+
+
+class HugoTestCase(SlapOSInstanceTestCase):
+  def checkUrlAndGetResponse(self, url):
+    response = requests.get(url)
+    self.assertEqual(requests.codes['OK'], response.status_code)
+    return response
+
+
+class TestEmptyDeploy(HugoTestCase):
+  def test_default_deploy(self):
+    hugo_url = self.requestDefaultInstance().getConnectionParameterDict()['hugo-server-cdn-url']
+    nginx_url = self.requestDefaultInstance().getConnectionParameterDict()['nginx-server-cdn-url']
+    hugo_response = self.checkUrlAndGetResponse(hugo_url)
+    nginx_response = self.checkUrlAndGetResponse(nginx_url)
+    hugo_result = hugo_response.text
+    nginx_result = nginx_response.text
+    self.assertFalse("<h1>" in hugo_result)
+    self.assertTrue("<h1>" in nginx_result)
+
+  @classmethod
+  def getInstanceParameterDict(cls):
+    return cls.instance_parameter_dict
