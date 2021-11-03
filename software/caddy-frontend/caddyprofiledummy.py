5e392d35e92f1cfe396457e925dba3194d389d82
Łukasz Nowak <luke@nexedi.com>
2021-11-03 11:09:13 +0100

    caddy-frontend: Implement netloc-list

--- software/caddy-frontend/caddyprofiledummy.py
+++ software/caddy-frontend/caddyprofiledummy.py
@@ -1,3 +1,5 @@
+import urlparse
+
 class Recipe(object):
   def __init__(self, *args, **kwargs):
     pass
@@ -7,3 +9,13 @@ class Recipe(object):
 
   def update(self):
     return self.install()
+
+def validate_netloc(netloc):
+  # a bit crazy way to validate that the passed parameter is haproxy
+  # compatible server netloc
+  parsed = urlparse.urlparse('scheme://'+netloc)
+  if ':' in parsed.hostname:
+    hostname = '[%s]' % parsed.hostname
+  else:
+    hostname = parsed.hostname
+  return netloc == '%s:%s' % (hostname, parsed.port)
