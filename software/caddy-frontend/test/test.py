964fe337b584067c4b4fb56fc914536e102873e2
Łukasz Nowak <luke@nexedi.com>
2021-11-08 11:18:32 +0100

    caddy-frontend: Fix test setup
    Provide any backend, so that requests can go somewhere, as otherwise in some
    circumstances there are unstable replies from haproxy.

--- software/caddy-frontend/test/test.py
+++ software/caddy-frontend/test/test.py
@@ -4629,12 +4629,15 @@ class TestEnableHttp2ByDefaultFalseSlave(SlaveHttpFrontendTestCase,
   def getSlaveParameterDictDict(cls):
     return {
       'enable-http2-default': {
+        'url': cls.backend_url,
       },
       'enable-http2-false': {
         'enable-http2': 'false',
+        'url': cls.backend_url,
       },
       'enable-http2-true': {
         'enable-http2': 'true',
+        'url': cls.backend_url,
       },
       'dummy-cached': {
         'url': cls.backend_url,
@@ -4719,12 +4722,15 @@ class TestEnableHttp2ByDefaultDefaultSlave(SlaveHttpFrontendTestCase,
   def getSlaveParameterDictDict(cls):
     return {
       'enable-http2-default': {
+        'url': cls.backend_url,
       },
       'enable-http2-false': {
         'enable-http2': 'false',
+        'url': cls.backend_url,
       },
       'enable-http2-true': {
         'enable-http2': 'true',
+        'url': cls.backend_url,
       },
       'dummy-cached': {
         'url': cls.backend_url,
