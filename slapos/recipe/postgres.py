SlapOS/slapos

commit 32fbf0d4366e7c80a3c09fb14cbdb07c2eb28fa4

recipe/postgres: support non standard port
even though the recipe has a required option for port, it was not used
and server was always listening on default (5432) port

slapos/recipe/postgres.py
@@ -147,6 +147,7 @@ class Recipe(GenericBaseRecipe):
         with open(postgres_conf, 'w') as cfg:
             cfg.write(textwrap.dedent("""\
                     listen_addresses = '%s'
+                    port = %s
                     logging_collector = on
                     log_rotation_size = 50MB
                     max_connections = 100
@@ -162,6 +163,7 @@ class Recipe(GenericBaseRecipe):
                     unix_socket_permissions = 0700
                     """ % (
                         ','.join(set(ipv4).union(ipv6)),
+                        self.options['port'],
                         pgdata,
                         )))
 
@@ -215,6 +217,7 @@ class Recipe(GenericBaseRecipe):
             p = subprocess.Popen([
                     psql_binary,
                     '-h', pgdata,
+                    '-p', self.options['port'],
                     '-U', user,
                     '-d', self.options['dbname'],
                 ],

slapos/test/recipe/test_postgres.py
@@ -110,3 +110,7 @@ class PostgresTest(unittest.TestCase):
         'password authentication failed'
     ):
       psycopg2.connect(dsn)
+
+
+class PostgresTestNonStandardPort(PostgresTest):
+  port = 5433

commit f54c31b683d04dad5c3d6ca7097e2719477cad9a

recipe/postgres: fix regression with empty port
32fbf0d43 (recipe/postgres: support non standard port, 2021-11-12)
broke support for empty port, it was generating config file with
`port = ` , which is an invalid config file syntax for postgresql.

slapos/recipe/postgres.py
@@ -147,7 +147,7 @@ class Recipe(GenericBaseRecipe):
         with open(postgres_conf, 'w') as cfg:
             cfg.write(textwrap.dedent("""\
                     listen_addresses = '%s'
-                    port = %s
+                    %s
                     logging_collector = on
                     log_rotation_size = 50MB
                     max_connections = 100
@@ -163,7 +163,7 @@ class Recipe(GenericBaseRecipe):
                     unix_socket_permissions = 0700
                     """ % (
                         ','.join(set(ipv4).union(ipv6)),
-                        self.options['port'],
+                        'port = %s' % self.options['port'] if self.options['port'] else '',
                         pgdata,
                         )))
 

slapos/test/recipe/test_postgres.py
@@ -1,7 +1,6 @@
 import os
 import shutil
 import tempfile
-import textwrap
 import time
 import unittest
 
@@ -54,7 +53,7 @@ class PostgresTest(unittest.TestCase):
     self.addCleanup(server_process.terminate)
 
     # wait for server to accept connections
-    for i in range(60):
+    for i in range(10):
       time.sleep(i)
       try:
         psycopg2.connect(self.buildout['postgres']['url']).close()
@@ -114,3 +113,7 @@ class PostgresTest(unittest.TestCase):
 
 class PostgresTestNonStandardPort(PostgresTest):
   port = 5433
+
+
+class PostgresTestEmptyPort(PostgresTest):
+  port = ''