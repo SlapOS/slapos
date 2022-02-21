50e623d5bf1e2501e655f12ab0f445b0c9b5c2b2
Łukasz Nowak <luke@nexedi.com>
2022-02-21 18:01:52 +0100

    XXX caddy-frontend: Switch to full CSR analysis
    Instead of trusting CSR id published by the node which tries to join the
    cluster add a tool which is able to compare exposed CSR with one in caucase
    and then decide to accept node in the cluster. This tool does what usual user
    would do, and it's logic implemented as a script leads to much simpler profiles.
    
    For sake of clean profiles csr_id has been removed, except when it's used for
    self joining of the user to the cluster.
    
    XXX:
     * simplify the smart-caucase-signer by using python modules instead of subprocess
     * recheck names of returned and requested values
     * (in another commit) add promise that there are no CSRs to be signed

--- software/caddy-frontend/caddyprofiledummy.py
+++ software/caddy-frontend/caddyprofiledummy.py
@@ -1,3 +1,10 @@
+from __future__ import print_function
+
+import json
+import os
+import ssl
+import subprocess
+import sys
 import urlparse
 
 class Recipe(object):
@@ -19,3 +26,69 @@ def validate_netloc(netloc):
   else:
     hostname = parsed.hostname
   return netloc == '%s:%s' % (hostname, parsed.port)
+
+
+def _check_certificate(url, certificate):
+  parsed = urlparse.urlparse(url)
+  got_certificate = ssl.get_server_certificate((parsed.hostname, parsed.port))
+  if certificate.strip() != got_certificate.strip():
+    raise ValueError('Certificate for %s does not match expected one' % (url,))
+
+
+def _get_exposed_csr(curl, url):
+  return subprocess.check_output([curl, '-s', '-k', '-g', url]).strip()
+
+
+def _get_caucase_csr_list(caucase, user_key):
+  return json.loads(subprocess.check_output(
+    [caucase, '--list-csr', '--list-csr-style', 'json', '--user-key', user_key]
+  ))
+
+
+def _get_modulus(openssl, csr):
+  popen = subprocess.Popen([
+    openssl, 'req', '-noout', '-modulus'], stdin=subprocess.PIPE,
+    stdout=subprocess.PIPE)
+  modulus = popen.communicate(csr)[0]
+  if popen.returncode != 0:
+    raise ValueError('Failed to get modulus')
+  return modulus
+
+
+def _sign_csr(caucase, openssl, user_key, csr, csr_list):
+  signed = False
+  for csr_entry in csr_list:
+    if _get_modulus(openssl, csr) == _get_modulus(openssl, csr_entry['csr']):
+      subprocess.check_call([
+        caucase, '--user-key', user_key, '--sign-csr', csr_entry['csr_id']])
+      print('Signed csr with id %s' % (csr_entry['csr_id'],))
+      signed = True
+      break
+  return signed
+
+
+def _mark_done(filename):
+  with open(filename, 'w') as fh:
+    fh.write('done')
+  print('Marked file %s' % (filename,))
+
+
+def _is_done(filename):
+  if os.path.exists(filename):
+    return True
+  return False
+
+
+def smart_sign():
+  curl, openssl, caucase_wrapper, done_file, user_key, csr_url, \
+    csr_url_certificate = sys.argv[1:]
+  if _is_done(done_file):
+    return
+  _check_certificate(csr_url, csr_url_certificate)
+  exposed_csr = _get_exposed_csr(curl, csr_url)
+  caucase_csr_list = _get_caucase_csr_list(caucase_wrapper, user_key)
+  if _sign_csr(
+    caucase_wrapper, openssl, user_key, exposed_csr, caucase_csr_list):
+    _mark_done(done_file)
+  else:
+    print('Failed to sign %s' % (csr_url,))
