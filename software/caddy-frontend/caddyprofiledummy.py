26f94b6d842e8a555316ea79470c180abe52146f
Łukasz Nowak <luke@nexedi.com>
2022-02-23 11:15:04 +0100

    squash! XXX caddy-frontend: Switch to full CSR analysis
    Phase one: use caucase as a library instead of subprocess

--- software/caddy-frontend/caddyprofiledummy.py
+++ software/caddy-frontend/caddyprofiledummy.py
@@ -1,11 +1,14 @@
 from __future__ import print_function
 
-import json
+import caucase.client
+import caucase.utils
 import os
 import ssl
 import subprocess
 import sys
 import urlparse
+
+from cryptography.hazmat.primitives import serialization
 
 class Recipe(object):
   def __init__(self, *args, **kwargs):
@@ -39,10 +42,25 @@ def _get_exposed_csr(curl, url):
   return subprocess.check_output([curl, '-s', '-k', '-g', url]).strip()
 
 
-def _get_caucase_csr_list(caucase, user_key):
-  return json.loads(subprocess.check_output(
-    [caucase, '--list-csr', '--list-csr-style', 'json', '--user-key', user_key]
-  ))
+def _get_caucase_client(ca_url, ca_crt, user_key):
+  return caucase.client.CaucaseClient(
+    ca_url=ca_url + '/cas',
+    ca_crt_pem_list=caucase.utils.getCertList(ca_crt),
+    user_key=user_key,
+  )
+
+
+def _get_caucase_csr_list(ca_url, ca_crt, user_key):
+  csr_list = []
+  for entry in _get_caucase_client(
+    ca_url, ca_crt, user_key).getPendingCertificateRequestList():
+    csr = caucase.utils.load_certificate_request(
+      caucase.utils.toBytes(entry['csr']))
+    csr_list.append({
+      'csr_id': entry['id'],
+      'csr': csr.public_bytes(serialization.Encoding.PEM).decode()
+    })
+  return csr_list
 
 
 def _get_modulus(openssl, csr):
@@ -55,12 +73,12 @@ def _get_modulus(openssl, csr):
   return modulus
 
 
-def _sign_csr(caucase, openssl, user_key, csr, csr_list):
+def _sign_csr(ca_url, ca_crt, user_key, openssl, csr, csr_list):
   signed = False
+  client = _get_caucase_client(ca_url, ca_crt, user_key)
   for csr_entry in csr_list:
     if _get_modulus(openssl, csr) == _get_modulus(openssl, csr_entry['csr']):
-      subprocess.check_call([
-        caucase, '--user-key', user_key, '--sign-csr', csr_entry['csr_id']])
+      client.createCertificate(int(csr_entry['csr_id']))
       print('Signed csr with id %s' % (csr_entry['csr_id'],))
       signed = True
       break
@@ -80,15 +98,15 @@ def _is_done(filename):
 
 
 def smart_sign():
-  curl, openssl, caucase_wrapper, done_file, user_key, csr_url, \
+  curl, openssl, ca_url, ca_crt, done_file, user_key, csr_url, \
     csr_url_certificate = sys.argv[1:]
   if _is_done(done_file):
     return
   _check_certificate(csr_url, csr_url_certificate)
   exposed_csr = _get_exposed_csr(curl, csr_url)
-  caucase_csr_list = _get_caucase_csr_list(caucase_wrapper, user_key)
+  caucase_csr_list = _get_caucase_csr_list(ca_url, ca_crt, user_key)
   if _sign_csr(
-    caucase_wrapper, openssl, user_key, exposed_csr, caucase_csr_list):
+    ca_url, ca_crt, user_key, openssl, exposed_csr, caucase_csr_list):
     _mark_done(done_file)
   else:
     print('Failed to sign %s' % (csr_url,))
