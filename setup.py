fd349f5299d829eb2d3bb3d99da74e01a97cecbe
Jérome Perrin <jerome@nexedi.com>
2022-07-07 14:36:44 +0900

    😱 try slapos.cookbook 😱

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '1.0.252'
+version = '1.0.253-dev'
 name = 'slapos.cookbook'
 long_description = open("README.rst").read()
 
