2c31482d225aafb078d9a918ab3783b1be47d0bf
Leo-Paul Geneau <leo-paul.geneau@nexedi.com>
2022-08-11 18:35:00 +0200

    component/qjs-wrapper: add message API
    Add support of the message API (send and receive custom messages between drones).

--- software/js-drone/worker.js
+++ software/js-drone/worker.js
@@ -1,6 +1,6 @@
+/* global console, std */
 {% set comma_separated_drone_id_list = ', '.join(drone_id_list.split()) -%}
 
-/*global console*/
 import {
   Drone,
   doParachute,
@@ -16,12 +16,12 @@ import {
   loiter,
   setAirspeed,
   setAltitude,
-  setCheckpoint,
   setManualControlInput,
+  setMessage,
   setTargetCoordinates
 } from "{{ qjs_wrapper }}";
-import { Worker } from "os"
 import * as std from "std";
+import { Worker } from "os";
 
 (function (console, Worker) {
   // Every script is evaluated per drone
@@ -30,13 +30,11 @@ import * as std from "std";
     drone_id_list = [{{ comma_separated_drone_id_list }}],
     IS_PUBLISHER = {{ is_publisher }};
 
-  var parent = Worker.parent,
+  let parent = Worker.parent,
     user_me = {
       //for debugging purpose
       fdopen: std.fdopen,
       in: std.in,
-      //to move into user script
-      setCheckpoint: setCheckpoint,
       //required to fly
       doParachute: doParachute,
       drone_dict: {},
@@ -57,51 +55,68 @@ import * as std from "std";
       id: {{ id }},
       landed: landed,
       loiter: loiter,
+      sendMsg: function(msg, id = -1) {
+        setMessage(JSON.stringify({ content: msg, dest_id: id }));
+      },
       setAirspeed: setAirspeed,
       setAltitude: setAltitude,
       setTargetCoordinates: setTargetCoordinates
     };
 
   function loadUserScript(path) {
-    var script_content = std.loadFile(path);
+    let script_content = std.loadFile(path);
     if (script_content === null) {
-      console.log('Failed to load user script ' + path);
+      console.log("Failed to load user script " + path);
       std.exit(1);
     }
     try {
       std.evalScript(
-        'function execUserScript(from, me) {' +
+        "function execUserScript(from, me) {" +
           script_content +
-        '};'
+        "};"
       );
     } catch (e) {
-      console.log('Failed to evaluate user script', e);
+      console.log("Failed to evaluate user script", e);
       std.exit(1);
     }
     execUserScript(null, user_me);
 
     // Call the drone onStart function
-    if (user_me.hasOwnProperty('onStart')) {
+    if (user_me.hasOwnProperty("onStart")) {
       user_me.onStart();
     }
   }
 
   function handleMainMessage(evt) {
-    var type = evt.data.type;
+    let type = evt.data.type,
+      message,
+      drone_id;
 
-    if (type === 'initPubsub') {
+    if (type === "initPubsub") {
       initPubsub(drone_id_list.length);
       for (let i = 0; i < drone_id_list.length; i++) {
-        let id = drone_id_list[i];
-        user_me.drone_dict[id] = new Drone(id);
-        user_me.drone_dict[id].init(i);
+        drone_id = drone_id_list[i];
+        user_me.drone_dict[drone_id] = new Drone(drone_id);
+        user_me.drone_dict[drone_id].init(i);
       }
       parent.postMessage({type: "initialized"});
-    }
-    else if (type === 'load') {
+    } else if (type === "load") {
       loadUserScript(evt.data.path);
       parent.postMessage({type: "loaded"});
-    } else if (type === 'update') {
+    } else if (type === "update") {
+      for (const [id, drone] of Object.entries(user_me.drone_dict)) {
+        message = drone.message
+        if (message.length > 0) {
+          message = JSON.parse(message);
+          if (user_me.id === id) {
+            continue;
+          }
+          if (user_me.hasOwnProperty("onGetMsg") &&
+              [-1, user_me.id].includes(message.dest_id)) {
+            user_me.onGetMsg(message.content);
+          }
+        }
+      }
       // Call the drone onStart function
       if (user_me.hasOwnProperty("onUpdate")) {
         if (IS_PUBLISHER && isInManualMode()) {
@@ -111,7 +126,7 @@ import * as std from "std";
       }
       parent.postMessage({type: "updated"});
     } else {
-      throw new Error('Unsupported message type', type);
+      throw new Error("Unsupported message type", type);
     }
   }
 
@@ -125,5 +140,4 @@ import * as std from "std";
       std.exit(1);
     }
   };
-
 }(console, Worker));
