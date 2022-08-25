f029984c48084a69323eb044535246106ecd4b8b
Łukasz Nowak <luke@nexedi.com>
2022-08-25 12:01:48 +0200

    caddy-frontend: c->h: Really avoid Content-Type modification
    Caddy was automat(g)ically adding Content-Type header to the backend
    response, but Haproxy does not do it, and change of the servers shall
    follow generic approach that not additional information is added to the
    response by the CDN.

--- software/caddy-frontend/test/test.py
+++ software/caddy-frontend/test/test.py
@@ -2554,10 +2554,7 @@ class TestSlave(SlaveHttpFrontendTestCase, TestDataMixin):
       }
     )
 
-    self.assertEqual(
-      'text/xml; charset=utf-8',
-      result.headers['Content-Type']
-    )
+    self.assertNotIn('Content-Type', result.headers)
 
   def test_type_zope_path(self):
     parameter_dict = self.assertSlaveBase('type-zope-path')
