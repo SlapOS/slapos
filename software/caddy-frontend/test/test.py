c732358a421950a81c9ec2d27909080e55ad5248
Łukasz Nowak <luke@nexedi.com>
2022-09-07 14:23:26 +0200

    caddy-frontend: c->h: Implement type:zope
    Adapt tests to cleaner responses from Haproxy.

--- software/caddy-frontend/test/test.py
+++ software/caddy-frontend/test/test.py
@@ -3022,7 +3022,7 @@ class TestSlave(SlaveHttpFrontendTestCase, TestDataMixin):
     )
 
     self.assertEqual(
-      'https://typezope.example.com:%s/test-path/deep/.././deeper' % (
+      'https://typezope.example.com:%s/test-path/deeper' % (
         HTTP_PORT,),
       result.headers['Location']
     )
@@ -3141,7 +3141,7 @@ class TestSlave(SlaveHttpFrontendTestCase, TestDataMixin):
     )
 
     self.assertEqual(
-      'https://%s:%s/test-path/deep/.././deeper' % (
+      'https://%s:%s/test-path/deeper' % (
         parameter_dict['domain'], HTTP_PORT),
       result.headers['Location']
     )
@@ -3182,7 +3182,7 @@ class TestSlave(SlaveHttpFrontendTestCase, TestDataMixin):
     )
 
     self.assertEqual(
-      'https://%s:%s/test-path/deep/.././deeper' % (
+      'https://%s:%s/test-path/deeper' % (
         parameter_dict['domain'], HTTP_PORT),
       result.headers['Location']
     )
