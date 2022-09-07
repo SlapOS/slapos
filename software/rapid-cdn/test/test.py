06180569ddf693e98d7abae4060cbc1a5d1a2ada
Łukasz Nowak <luke@nexedi.com>
2022-09-07 14:23:26 +0200

    rapid-cdn: c->h: Implement type:zope
    Adapt tests to cleaner responses from Haproxy.

--- software/rapid-cdn/test/test.py
+++ software/rapid-cdn/test/test.py
@@ -3015,7 +3015,7 @@ class TestSlave(SlaveHttpFrontendTestCase, TestDataMixin):
     )
 
     self.assertEqual(
-      'https://typezope.example.com:%s/test-path/deep/.././deeper' % (
+      'https://typezope.example.com:%s/test-path/deeper' % (
         HTTP_PORT,),
       result.headers['Location']
     )
@@ -3134,7 +3134,7 @@ class TestSlave(SlaveHttpFrontendTestCase, TestDataMixin):
     )
 
     self.assertEqual(
-      'https://%s:%s/test-path/deep/.././deeper' % (
+      'https://%s:%s/test-path/deeper' % (
         parameter_dict['domain'], HTTP_PORT),
       result.headers['Location']
     )
@@ -3175,7 +3175,7 @@ class TestSlave(SlaveHttpFrontendTestCase, TestDataMixin):
     )
 
     self.assertEqual(
-      'https://%s:%s/test-path/deep/.././deeper' % (
+      'https://%s:%s/test-path/deeper' % (
         parameter_dict['domain'], HTTP_PORT),
       result.headers['Location']
     )
