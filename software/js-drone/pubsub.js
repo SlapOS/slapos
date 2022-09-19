7cff78c7d21a2d767f6ab9795d21ee76de220257
Leo-Paul Geneau <leo-paul.geneau@nexedi.com>
2022-09-19 11:03:12 +0200

    software/js-drone: add software-type drone
    Now the default software-type request a list of drone software-type

--- software/js-drone/pubsub.js
+++ software/js-drone/pubsub.js
@@ -1,15 +1,16 @@
 import {runPubsub} from "{{ qjs_wrapper }}";
 import {Worker} from "os";
+import {readAsString} from "std";
 
-const PORT = "4840";
-const IPV6 = "{{ ipv6 }}";
+const PORT = "4840",
+  configuration = JSON.parse(readAsString({{ configuration }});
 
 let parent = Worker.parent;
 
 function handle_msg(e) {
   switch(e.data.action) {
     case "run":
-      runPubsub(IPV6, PORT, "{{ net_if }}", e.data.id, e.data.interval, e.data.publish);
+      runPubsub(IPV6, PORT, configuration.multicast-ip, e.data.id, e.data.interval, e.data.publish);
       parent.postMessage({running: false});
       parent.onmessage = null;
       break;
