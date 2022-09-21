05174a1fca20c6e6b2b900822d958cd4f4085d11
Łukasz Nowak <luke@nexedi.com>
2022-09-21 12:00:54 +0200

    caddy-frontend: Rename published values
    caddy-frontend-N is bad name, it's just frontend-node-N.

--- software/caddy-frontend/test/test.py
+++ software/caddy-frontend/test/test.py
@@ -1037,7 +1037,7 @@ class HttpFrontendTestCase(SlapOSInstanceTestCase):
     return generate_auth_url, upload_url
 
   def assertNodeInformationWithPop(self, parameter_dict):
-    key = 'caddy-frontend-1-node-information-json'
+    key = 'frontend-node-1-node-information-json'
     node_information_json_dict = {}
     for k in list(parameter_dict.keys()):
       if k.startswith('caddy-frontend') and k.endswith(
@@ -1054,7 +1054,7 @@ class HttpFrontendTestCase(SlapOSInstanceTestCase):
     self.node_information_dict = node_information_dict
 
   def assertBackendHaproxyStatisticUrl(self, parameter_dict):
-    url_key = 'caddy-frontend-1-backend-haproxy-statistic-url'
+    url_key = 'frontend-node-1-backend-haproxy-statistic-url'
     backend_haproxy_statistic_url_dict = {}
     for key in list(parameter_dict.keys()):
       if key.startswith('caddy-frontend') and key.endswith(
@@ -1562,11 +1562,11 @@ class TestMasterAIKCDisabledAIBCCDisabledRequest(
     self.assertRejectedSlavePromiseEmptyWithPop(parameter_dict)
     self.assertKeyWithPop('kedifa-csr-certificate', parameter_dict)
     self.assertKeyWithPop('kedifa-csr-url', parameter_dict)
-    self.assertKeyWithPop('caddy-frontend-1-kedifa-csr-url', parameter_dict)
+    self.assertKeyWithPop('frontend-node-1-kedifa-csr-url', parameter_dict)
     self.assertKeyWithPop(
-      'caddy-frontend-1-backend-client-csr-url', parameter_dict)
+      'frontend-node-1-backend-client-csr-url', parameter_dict)
     self.assertKeyWithPop(
-      'caddy-frontend-1-csr-certificate', parameter_dict)
+      'frontend-node-1-csr-certificate', parameter_dict)
     self.assertNodeInformationWithPop(parameter_dict)
     self.assertEqual(
       {
@@ -2100,7 +2100,7 @@ class TestSlave(SlaveHttpFrontendTestCase, TestDataMixin):
     }
     expected_node_information['version-hash-history']['testhash'] = 'testurl'
     self.assertEqual(
-      json.loads(parameter_dict['caddy-frontend-1-node-information-json']),
+      json.loads(parameter_dict['frontend-node-1-node-information-json']),
       expected_node_information
     )
 
@@ -4591,8 +4591,8 @@ class TestReplicateSlave(SlaveHttpFrontendTestCase, TestDataMixin):
     self.assertLogAccessUrlWithPop(parameter_dict)
     self.assertKedifaKeysWithPop(parameter_dict)
     key_list = [
-      'caddy-frontend-1-node-information-json',
-      'caddy-frontend-2-node-information-json'
+      'frontend-node-1-node-information-json',
+      'frontend-node-2-node-information-json'
     ]
     node_information_json_dict = {}
     for k in list(parameter_dict.keys()):
