15b0d07aab83fdb2c9e67f00473eafb6a60ae68e
mmanchev23 <mmanchev2003@gmail.com>
2022-12-30 15:08:48 +0200

    Initial commit ...

--- software/mosquitto/test/test.py
+++ software/mosquitto/test/test.py
@@ -28,6 +28,7 @@ class TestMQTT(SlapOSInstanceTestCase):
   message = str()
 
   def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
     client.subscribe("test")
 
   def on_message(client, userdata, msg):
