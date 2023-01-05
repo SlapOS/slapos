3de6dc617fc1a6e6f068bf2d7df383d56848b9b7
Boxiang Sun <boxiang.sun@nexedi.com>
2023-01-05 23:18:06 +0100

    theia: gitlab fixup

--- software/theia/test/project_tests.py
+++ software/theia/test/project_tests.py
@@ -458,9 +458,9 @@ class TestTheiaResilienceGitlab(test_resiliency.TestTheiaResilience):
 
   @classmethod
   def _deployEmbeddedSoftware(cls, software_url, instance_name, retries=0, instance_type='export'):
-    super(TestTheiaResilienceGitlab, self)._deployEmbeddedSoftware(cls, software_url, instance_name, retries=0, instance_type)
+    super(TestTheiaResilienceGitlab, self)._deployEmbeddedSoftware(cls, software_url, instance_name, retries=0, instance_type=instance_type)
     parameters = 'software_type="gitlab-test"'
-    cls.callSlapos('request', instance_name, software_url, instance_type=instance_type, "--parameters", parameters)
+    cls.callSlapos('request', instance_name, software_url, "--parameters", parameters, instance_type=instance_type)
     cls._processEmbeddedInstance(retries, instance_type)
 
   def _getGitlabConnexionParameters(self, instance_type='export'):
