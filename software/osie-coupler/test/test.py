219288971a7d5a3246f0566382a5a460caa95cd4
Thomas Gambier <thomas.gambier@nexedi.com>
2023-02-01 20:17:22 +0100

    software/osie-coupler: add basic test

---
+++ software/osie-coupler/test/test.py
@@ -0,0 +1,53 @@
+##############################################################################
+# coding: utf-8
+#
+# Copyright (c) 2022 Nexedi SA and Contributors. All Rights Reserved.
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsibility of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# guarantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+import io
+import os
+import urllib.parse
+import glob
+
+import lxml.etree
+import requests
+
+from slapos.testing.testcase import makeModuleSetUpAndTestCaseClass
+
+setUpModule, SlapOSInstanceTestCase = makeModuleSetUpAndTestCaseClass(
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), '..', 'software.cfg')))
+
+
+class OsieTestCase(SlapOSInstanceTestCase):
+
+  class TestnodeTest(SlapOSInstanceTestCase):
+  @classmethod
+  def getInstanceParameterDict(cls):
+    return {"mode": 1}
+
+  def test(self):
+    connexion_parameters = self.computer_partition.getConnectionParameterDict()
+    self.assertIn('opc_ua_port', connexion_parameters)
+    self.assertIn('interface', connexion_parameters)
