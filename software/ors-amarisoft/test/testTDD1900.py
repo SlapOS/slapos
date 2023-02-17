7c4adf65ceb7b42d70c7361cd19256590400d6e0
Boxiang Sun <boxiang.sun@nexedi.com>
2023-02-17 03:37:39 +0100

    test fixup

--- software/ors-amarisoft/test/testTDD1900.py
+++ software/ors-amarisoft/test/testTDD1900.py
@@ -31,6 +31,7 @@ import json
 import glob
 import requests
 from urllib.parse import urlsplit
+import subprocess
 
 from slapos.testing.testcase import makeModuleSetUpAndTestCaseClass
 
@@ -303,7 +304,12 @@ class TestMonitorGadgetUrl(ORSTestCase):
         print(public_url)
 
         self.assertIn('software.cfg.html', monitor_gadget_url)
-
+        output = subprocess.check_output(['curl', '-k', '--show-error', monitor_gadget_url],  universal_newlines=True)
+        print(output)
+        self.assertIn('<script src="rsvp.js"></script>', output)
+        self.assertIn('<script src="renderjs.js"></script>', output)
+        self.assertIn('<script src="g-chart.line.js"></script>', output)
+        self.assertIn('<script src="promise.gadget.js"></script>', output)
         response = requests.get(monitor_gadget_url, verify=False)
         self.assertEqual(requests.codes['OK'], response.status_code)
         self.assertIn('<script src="rsvp.js"></script>', response.text)
