83515b93d80562fd98ecf8285dfe174fdb81c0f6
Boxiang Sun <boxiang.sun@nexedi.com>
2023-02-20 16:06:12 +0100

    test fixup

--- software/ors-amarisoft/test/testTDD1900.py
+++ software/ors-amarisoft/test/testTDD1900.py
@@ -278,15 +278,20 @@ class TestMonitorGadgetUrl(ORSTestCase):
     @classmethod
     def waitForInstance(cls):
         super().waitForInstance()
-        exited_process = []
+        exited_services = []
         output = subprocess.getoutput('slapos node status')
-        process_lines = output.split('\n')
-        for process_line in process_lines:
-            process = process_line.split()
-            if process[1] == 'EXITED' and "on-watch" in process[0]:
-                exited_process.append(process[0])
-        if len(exited_process) >= 1:
-            raise Exception("Following process is in EXITED state: %s", str(exited_process))
+        service_lines = output.split('\n')
+        for service_line in service_lines:
+          service = service_line.split()
+          if service[1] == 'EXITED' and "on-watch" in service[0]:
+            exited_services.append(service[0])
+
+        if len(exited_services) >= 1:
+          raise Exception("Exited on watch service: %s", str(exited_services))
+        print("================")
+        print("================")
+        print("================")
+        print("All on watch services is running")
 
 
     def test_monitor_gadget_url(self):
