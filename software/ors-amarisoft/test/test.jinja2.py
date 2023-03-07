1748a2ec1d0cb2b24d39d7ce35530ff8975b8c6c
Boxiang Sun <boxiang.sun@nexedi.com>
2023-03-07 18:08:32 +0100

    test fixup

--- software/ors-amarisoft/test/test.jinja2.py
+++ software/ors-amarisoft/test/test.jinja2.py
@@ -29,6 +29,7 @@ import os
 import yaml
 import json
 import glob
+import requests
 
 from slapos.testing.testcase import makeModuleSetUpAndTestCaseClass
 
@@ -181,8 +182,8 @@ def test_sim_card(self):
 
     self.slap.waitForInstance() # Wait until publish is done
     p = self.requestSlaveInstance().getConnectionParameterDict()
-    p = p['_'] if '_' in p else p                  
-    self.assertIn('info', p)  
+    p = p['_'] if '_' in p else p
+    self.assertIn('info', p)
 
 class TestENBParameters(ORSTestCase):
     @classmethod
@@ -262,6 +263,34 @@ def requestSlaveInstance(cls, software_type):
         shared=True,
         software_type=software_type,
     )
+
+class TestMonitorGadgetUrl(ORSTestCase):
+    @classmethod
+    def getInstanceParameterDict(cls):
+        return {'_': json.dumps(enb_param_dict)}
+
+    @classmethod
+    def getInstanceSoftwareType(cls):
+        return "enb-epc"
+
+    def test_monitor_gadget_url(self):
+        parameters = json.loads(self.computer_partition.getConnectionParameterDict()['_'])
+        self.assertIn('monitor-gadget-url', parameters)
+
+        monitor_setup_url = parameters['monitor-setup-url']
+        monitor_gadget_url = parameters['monitor-gadget-url']
+        monitor_base_url = parameters['monitor-base-url']
+        public_url = monitor_base_url + '/public'
+        response = requests.get(public_url, verify=False)
+        self.assertEqual(requests.codes['OK'], response.status_code)
+
+        self.assertIn('software.cfg.html', monitor_gadget_url)
+        response = requests.get(monitor_gadget_url, verify=False)
+        self.assertEqual(requests.codes['OK'], response.status_code)
+        self.assertIn('<script src="rsvp.js"></script>', response.text)
+        self.assertIn('<script src="renderjs.js"></script>', response.text)
+        self.assertIn('<script src="g-chart.line.js"></script>', response.text)
+        self.assertIn('<script src="promise.gadget.js"></script>', response.text)
 
 class TestEPCSimCard(ORSTestCase):
     @classmethod
