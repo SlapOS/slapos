0938771400d84ee36c5b79f45b3dd1530f8aac5c
Łukasz Nowak <luke@nexedi.com>
2023-03-21 12:39:50 +0100

    software/rapid-cdn: Show full diffs in TestDataMixin
    It's very important to have readable and contextable information on failures,
    in order to track root cause of the problem.

--- software/rapid-cdn/test/test.py
+++ software/rapid-cdn/test/test.py
@@ -518,6 +518,9 @@ class AtsMixin(object):
 
 
 class TestDataMixin(object):
+  # show full diffs, as it is required for proper analysis of problems
+  maxDiff = None
+
   def getTrimmedProcessInfo(self):
     return '\n'.join(sorted([
       '%(group)s:%(name)s %(statename)s' % q for q
@@ -541,8 +544,6 @@ class TestDataMixin(object):
       value = data_replacement_dict[replacement]
       runtime_data = runtime_data.replace(value, replacement)
 
-    maxDiff = self.maxDiff
-    self.maxDiff = None
     longMessage = self.longMessage
     self.longMessage = True
     try:
@@ -557,7 +558,6 @@ class TestDataMixin(object):
           fh.write(runtime_data.strip() + '\n')
       raise
     finally:
-      self.maxDiff = maxDiff
       self.longMessage = longMessage
 
   def _test_file_list(self, slave_dir_list, IGNORE_PATH_LIST=None):
