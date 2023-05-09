b47ce37f45c28bc01dd447e9ab3f1a1b4c9b0d80
Leo-Paul Geneau <leo-paul.geneau@nexedi.com>
2023-05-09 14:00:50 +0200

    component/qjs-wrapper: v↑ qjs-wrapper (1.3 -> 2.0)

---
+++ component/qjs-wrapper/autopilot_wrapper.h
@@ -0,0 +1,61 @@
+#ifndef __AUTOPILOT_H__
+#define __AUTOPILOT_H__
+
+#ifndef DLL_PUBLIC
+#define DLL_PUBLIC __attribute__ ((visibility ("default")))
+#endif
+
+/*
+ * 0. latitude (double, degrees)
+ * 1. longitude (double, degrees)
+ * 2. absolute altitude (double, meters)
+ * 3. relative altitude (double, meters)
+*/
+#define POSITION_ARRAY_SIZE 4
+/*
+ * 0. yaw angle (float, degrees)
+ * 1. air speed (float, m/s)
+ * 2. climb rate (float, m/s)
+*/
+#define SPEED_ARRAY_SIZE 3
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include <stdint.h>
+// Connexion management functions
+DLL_PUBLIC int start(const char * ip, int port, const char * log_file, int timeout);
+DLL_PUBLIC int stop();
+DLL_PUBLIC int reboot(void);
+
+// Flight state management functions
+DLL_PUBLIC int arm(void);
+DLL_PUBLIC int takeOff(void);
+DLL_PUBLIC int takeOffAndWait(void);
+DLL_PUBLIC int triggerParachute(void);
+
+// Flight management functions
+DLL_PUBLIC int loiter(float radius);
+DLL_PUBLIC int setAirspeed(float airspeed);
+DLL_PUBLIC int setTargetCoordinates(double la, double lo, float a, bool going_through);
+
+// Information functions
+DLL_PUBLIC float getAltitude(void);
+DLL_PUBLIC float getAltitudeRel(void);
+DLL_PUBLIC float getInitialAltitude(void);
+DLL_PUBLIC double getInitialLatitude(void);
+DLL_PUBLIC double getInitialLongitude(void);
+DLL_PUBLIC double getLatitude(void);
+DLL_PUBLIC double getLongitude(void);
+DLL_PUBLIC double *getPositionArray(void);
+DLL_PUBLIC float *getSpeedArray(void);
+DLL_PUBLIC double getTakeOffAltitude(void);
+DLL_PUBLIC float getYaw(void);
+DLL_PUBLIC float getSpeed(void);
+DLL_PUBLIC float getClimbRate(void);
+DLL_PUBLIC int healthAllOk(void);
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __AUTOPILOT_H__ */
