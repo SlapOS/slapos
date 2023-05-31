6dec5b281d3a8a1001d8c3ec186f26cb728d8917
Boxiang Sun <boxiang.sun@nexedi.com>
2023-05-31 10:37:45 +0200

    pbs: fixup

--- slapos/recipe/pbs.py
+++ slapos/recipe/pbs.py
@@ -178,9 +178,6 @@ class Recipe(GenericSlapRecipe, Notify, Callback):
                     # instead do a push to the clone.
                 fi
             fi
-        else
-            # Everything's okay, cleaning up...
-            $RDIFF_BACKUP --api-version 201 --remove-older-than %(remove_backup_older_than)s --force $BACKUP_DIR
         fi
 
 
