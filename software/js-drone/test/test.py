e234780022d3dc82ffe994ed31ede8bed973d05d
Leo-Paul Geneau <leo-paul.geneau@nexedi.com>
2023-06-03 01:58:29 +0200

    software/js-drone: print pubsub logs in the GUI

--- software/js-drone/test/test.py
+++ software/js-drone/test/test.py
@@ -283,6 +283,46 @@ class SubscriberTestCase(SlapOSInstanceTestCase):
       ws.recv_frame().data,
       b'Unknown instruction %s\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' % ws.sock.getsockname()[0].encode()
     )
+    self.assertIn(
+      b'\\u001b[32minfo/userland\\u001b[0m\\tfieldsSize 3\\n"}',
+      ws.recv_frame().data,
+    )
+    self.assertEqual(
+      ws.recv_frame().data,
+      b''.join((
+        b'{"drone_dict":{"0":{"latitude":',
+        b'"%.6f","longitude":"%.6f","altitude":"%.2f",' % (0, 0, 0),
+        b'"yaw":"%.2f","speed":"%.2f","climbRate":"%.2f"}}}' % (0, 0, 0),
+      ))
+    )
+    self.assertIn(
+      b'\\u001b[32minfo/client\\u001b[0m\\tReceived position of drone 0: %.6f ? %.6f ? %.2f m %.2f m\\n"}' % (0, 0 , 0, 0),
+      ws.recv_frame().data,
+    )
+    self.assertEqual(
+      ws.recv_frame().data,
+      b''.join((
+        b'{"drone_dict":{"0":{"latitude":',
+        b'"%.6f","longitude":"%.6f","altitude":"%.2f",' % (0, 0, 0),
+        b'"yaw":"%.2f","speed":"%.2f","climbRate":"%.2f"}}}' % (0, 0, 0),
+      ))
+    )
+    self.assertIn(
+      b'\\u001b[32minfo/client\\u001b[0m\\tReceived speed of drone 0: %.2f ? %.2f m/s %.2f m/s\\n"}' % (0, 0 , 0),
+      ws.recv_frame().data,
+    )
+    self.assertEqual(
+      ws.recv_frame().data,
+      b''.join((
+        b'{"drone_dict":{"0":{"latitude":',
+        b'"%.6f","longitude":"%.6f","altitude":"%.2f",' % (0, 0, 0),
+        b'"yaw":"%.2f","speed":"%.2f","climbRate":"%.2f"}}}' % (0, 0, 0),
+      ))
+    )
+    self.assertIn(
+      b'\\u001b[32minfo/userland\\u001b[0m\\tfieldsSize 1\\n"}',
+      ws.recv_frame().data,
+    )
     self.assertEqual(
       ws.recv_frame().data,
       b''.join((
@@ -294,6 +334,10 @@ class SubscriberTestCase(SlapOSInstanceTestCase):
     self.send_ua_networkMessage()
     time.sleep(0.1)
     self.assertEqual(ws.recv_frame().data, MESSAGE_CONTENT.replace(b'\\', b''))
+    self.assertIn(
+      b'\\u001b[32minfo/client\\u001b[0m\\tReceived position of drone 0: %.6f ? %.6f ? %.2f m %.2f m\\n"}' % POSITION_ARRAY_OUTPUT_VALUES,
+      ws.recv_frame().data,
+    )
     self.assertEqual(
       ws.recv_frame().data,
       b''.join((
@@ -301,5 +345,9 @@ class SubscriberTestCase(SlapOSInstanceTestCase):
         b'"%.6f","longitude":"%.6f","altitude":"%.2f",' % POSITION_ARRAY_OUTPUT_VALUES[:-1],
         b'"yaw":"%.2f","speed":"%.2f","climbRate":"%.2f"}}}' % SPEED_ARRAY_VALUES,
       ))
+    )
+    self.assertIn(
+      b'\\u001b[32minfo/client\\u001b[0m\\tReceived speed of drone 0: %.2f ? %.2f m/s %.2f m/s\\n"}' % SPEED_ARRAY_VALUES,
+      ws.recv_frame().data,
     )
     ws.close()
