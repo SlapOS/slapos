ca82e8ccf3df34e4fdb5fd1095d50ad36ff5d201
lu.xu <lu.xu@nexedi.com>
2023-06-21 12:32:24 +0200

    fixup

--- software/ors-amarisoft/lopcomm-rrh-stats.jinja2.py
+++ software/ors-amarisoft/lopcomm-rrh-stats.jinja2.py
@@ -82,7 +82,7 @@ class LopcommNetconfClient:
 
         # Filter not compatible between ncclient and netconf server
         #result = self.conn.create_subscription(filter=('xpath', '/o-ran-fm:*'))
-        sub = self.conn.create_subscription(filter=('stream', 'o-ran-supervision'))
+        sub = self.conn.create_subscription()
         self.logger.info('Subscription to %s successful' % (self.address,))
 
     def get_notification(self):
