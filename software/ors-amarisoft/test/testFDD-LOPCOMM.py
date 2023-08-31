967e3de73cd777af782eeddb3495a2e3d2ec91ed
Joanne Hugé <joanne.huge@nexedi.com>
2023-08-31 12:03:12 +0200

    software/ors-amarisoft: add 4G inter eNB handover

--- software/ors-amarisoft/test/testFDD-LOPCOMM.py
+++ software/ors-amarisoft/test/testFDD-LOPCOMM.py
@@ -54,6 +54,7 @@ param_dict = {
     'enb_id': '0x17',
     'pci': 250,
     'tac': '0x1717',
+    'root_sequence_index': '1',
     'mme_list': {
         '10.0.0.1': {'mme_addr': '10.0.0.1'},
         '2001:db8::1': {'mme_addr': '2001:db8::1'},
@@ -78,19 +79,23 @@ param_dict = {
     'nr_handover_a3_offset': 10,
     'ncell_list': {
         'ORS1': {
+            'dl_earfcn': 100000,
             'dl_nr_arfcn': 100000,
             'ssb_nr_arfcn': 100000,
             'pci': 1,
             'nr_cell_id': '0x0000001',
+            'cell_id': '0x0000001',
             'gnb_id_bits': 28,
             'nr_band': 1,
             'tac': 1
         },
         'ORS2': {
+            'dl_earfcn': 200000,
             'dl_nr_arfcn': 200000,
             'ssb_nr_arfcn': 200000,
             'pci': 2,
             'nr_cell_id': '0x0000002',
+            'cell_id': '0x0000001',
             'gnb_id_bits': 30,
             'nr_band': 2,
             'tac': 2
@@ -145,11 +150,22 @@ def test_enb_conf(self):
     self.assertEqual(conf['enb_id'], int(enb_param_dict['enb_id'], 16))
     self.assertEqual(conf['cell_list'][0]['n_id_cell'], enb_param_dict['pci'])
     self.assertEqual(conf['cell_list'][0]['tac'], int(enb_param_dict['tac'], 16))
+    self.assertEqual(conf['cell_list'][0]['root_sequence_index'], int(enb_param_dict['root_sequence_index']))
     for p in conf['cell_default']['plmn_list']:
       for n in "plmn attach_without_pdn reserved".split():
           self.assertEqual(p[n], enb_param_dict['plmn_list'][p['plmn']][n])
     for p in conf['mme_list']:
       self.assertEqual(p['mme_addr'], enb_param_dict['mme_list'][p['mme_addr']]['mme_addr'])
+
+    for p in conf['cell_list'][0]['ncell_list']:
+      for k in enb_param_dict['ncell_list']:
+        if p['dl_earfcn'] == gnb_param_dict1['ncell_list'][k]['dl_earfcn']:
+          break
+      conf_ncell = enb_param_dict['ncell_list'][k]
+      self.assertEqual(p['dl_earfcn'],  conf_ncell['dl_earfcn'])
+      self.assertEqual(p['n_id_cell'],    conf_ncell['pci'])
+      self.assertEqual(p['cell_id'],   int(conf_ncell['cell_id'], 16))
+      self.assertEqual(p['tac'],          conf_ncell['tac'])
 
     with open(conf_file, 'r') as f:
         for l in f:
