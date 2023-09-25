00d0b51a1b96262611390f64e1f1534cab004739
Kirill Smelkov <kirr@nexedi.com>
2023-09-25 18:14:04 +0200

    .

--- software/ors-amarisoft/k/kslap.py
+++ software/ors-amarisoft/k/kslap.py
@@ -1,6 +1,6 @@
 # Module kslap provides utility routines for dealing with SlapOS.
 
-# ref_of_instance returns reference an instance was requested with
+# ref_of_instance returns reference an instance was requested with.
 def ref_of_instance(slap, inst):
     i_comp_id = inst.slap_computer_id
     i_part_id = inst.slap_computer_partition_id
@@ -9,3 +9,11 @@ def ref_of_instance(slap, inst):
            x._reference          == i_part_id:
             return x._partition_reference
     raise KeyError('not found reference of instance_guid=%s-%s' % (i_comp_id, i_part_id))
+
+
+# instance_by_ref returns instance corresponding to specified reference.
+def instance_by_ref(slap, ref):
+    for x in slap.getOpenOrderDict().values():      # XXX linear search
+        if x._partition_reference == ref:
+            return x
+    raise KeyError('not found instance coresponding to reference %s' % ref)
