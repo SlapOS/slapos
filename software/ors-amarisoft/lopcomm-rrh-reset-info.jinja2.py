748bbaddf9f77a21556f149e9d329411f62924e1
lu.xu <lu.xu@nexedi.com>
2023-10-13 18:53:50 +0200

    software/ors-amarisoft: add reset function for RRH

---
+++ software/ors-amarisoft/lopcomm-rrh-reset-info.jinja2.py
@@ -0,0 +1,41 @@
+#!{{ python_path }}
+import paramiko
+import logging
+from logging.handlers import RotatingFileHandler
+
+def get_uptime(hostname, username, password):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        client.connect(hostname, username=username, password=password)
+        stdin, stdout, stderr = client.exec_command('uptime')
+        uptime_output = stdout.read().decode()
+        return uptime_output
+
+    except Exception as e:
+        logger.info(f"Error: {e}")
+    finally:
+        client.close()
+
+# Usage
+hostname = "{{ netaddr.IPAddress(slap_configuration.get('tap-ipv6-gateway', '')) }}"
+username = "oranuser"
+password = "oranpassword"
+
+# Initialize logger
+log_file = "{{ log_file }}"
+logger = logging.getLogger('logger')
+logger.setLevel(logging.INFO)
+handler = RotatingFileHandler(log_file, maxBytes=30000, backupCount=2)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+# Assuming `testing` is defined before usage
+testing = False
+
+if not testing:
+    # Get uptime from RRH
+    rrh_uptime = get_uptime(hostname, username, password)
+    logger.info(f"Uptime from RRH: {rrh_uptime}")
