e57d2dae72fa07aec26fda1ff77ffde7cf26708a
Kirill Smelkov <kirr@nexedi.com>
2023-10-19 11:05:16 +0300

    .

--- software/ors-amarisoft/k/kslap.py
+++ software/ors-amarisoft/k/kslap.py
@@ -26,3 +26,38 @@ def instance_by_ref(slap, ref):
     inst = slap.registerOpenOrder().getInformation(partition_reference=ref)
     return inst
     """
+
+# iSIM adds to core a shared SIM instance with specified number.
+def iSIM(core, sim_n):
+    core_ref  = ref_of_instance(slap, core)
+    core_guid = core.getInstanceGuid()
+    core_sr   = core.getSoftwareRelease()
+    sim = request(core_sr,
+        software_type="core-network",
+        partition_reference="%s/sim%d" % (core_ref, sim_n),
+        shared=True,
+        filter_kw={"instance_guid": core_guid},
+        partition_parameter_kw={"_": json.dumps({
+            "sim_algo": "milenage",
+            "imsi": "001010000000%d" % sim_n,
+            "opc": "000102030405060708090A0B0C0D0E0F",
+            "amf": "0x9001",
+            "sqn": "000000000000",
+            "k": "00112233445566778899AABBCCDDEEFF",
+            "impu": "impu%d" % sim_n,
+            "impi": "impi%d@amarisoft.com" % sim_n,
+        })})
+    return sim
+
+# iENB adds to enb a shared instance with specified reference and parameters.
+def iENB(enb, ref, kw):
+    enb_ref  = ref_of_instance(slap, enb)
+    enb_guid = enb.getInstanceGuid()
+    enb_sr   = enb.getSoftwareRelease()
+    ishared = request(enb_sr,
+        software_type="enb",
+        partition_reference=ref,
+        shared=True,
+        filter_kw={"instance_guid": enb_guid},
+        partition_parameter_kw={"_": json.dumps(kw)})
+    return ishared
