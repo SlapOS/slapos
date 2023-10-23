654e9848118b5c6824c48bd80ba07fa0aa71d9b4
Kirill Smelkov <kirr@nexedi.com>
2023-10-23 15:04:02 +0300

    .

--- software/ors-amarisoft/ru/xexec.c
+++ software/ors-amarisoft/ru/xexec.c
@@ -21,11 +21,12 @@
 
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 
-int main(int argc, const char *argv) {
+int main(int argc, const char *argv[]) {
     if (argc < 2) {
         fprintf(stderr, "E: usage: xexec prog arguments...\n");
         exit(128);
