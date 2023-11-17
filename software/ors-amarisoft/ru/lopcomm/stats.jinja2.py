e8ad47757597dfef27225955d38dc059ded304b7
lu.xu <lu.xu@nexedi.com>
2023-11-17 10:25:18 +0100

    software/ors-amarisoft: remove redundant netconf supervision subsciption The Netconf subscriptions were added twice, causing the RU to reboot because there was no supervision check on the redundant subscription.

--- software/ors-amarisoft/ru/lopcomm/stats.jinja2.py
+++ software/ors-amarisoft/ru/lopcomm/stats.jinja2.py
@@ -1,24 +1,78 @@
 #!{{ python_path }}
 import time
 import sys
+import os
+import threading
+
 sys.path.append({{ repr(buildout_directory_path) }})
 from ncclient_common import LopcommNetconfClient
 
+# Shared variable to indicate error occurred
+error_occurred = False
+lock = threading.Lock()
+
+def get_notification_continuously(nc):
+    global error_occurred
+    try:
+        while not error_occurred:
+            nc.get_notification()
+            pass
+    except Exception as e:
+        with lock:
+            error_occurred = True
+            nc.logger.error(f'Error in get_notification_continuously: {e}')
+
+# supervision watchdog keeps on
+def run_supervision_reset_continuously(nc):
+    global error_occurred
+    netconf_check_file = '{{ is_netconf_connected }}'
+    interval = 60
+    margin   = 10
+    try:
+        while not error_occurred:
+            t0 = time.time()
+            replied = nc.supervision_reset(interval, margin)
+            if replied:
+                with open(netconf_check_file, "w") as f:
+                    f.write('')
+            elif os.path.exists(netconf_check_file):
+                os.remove(netconf_check_file)
+
+            t1 = time.time()
+            time.sleep(interval - (t1 - t0))
+    except Exception as e:
+        with lock:
+            error_occurred = True
+            nc.logger.error(f'Error in run_supervision_reset_continuously: {e}')
+
 if __name__ == '__main__':
-  nc = LopcommNetconfClient(
-    log_file="{{ log_file }}",
-    json_log_file="{{ json_log_file }}",
-    cfg_json_log_file="{{ cfg_json_log_file }}",
-    supervision_json_log_file="{{ supervision_json_log_file }}",
-    ncsession_json_log_file="{{ ncsession_json_log_file }}",
-    software_json_log_file="{{ software_json_log_file }}"
+    nc = LopcommNetconfClient(
+        log_file="{{ log_file }}",
+        json_log_file="{{ json_log_file }}",
+        cfg_json_log_file="{{ cfg_json_log_file }}",
+        supervision_json_log_file="{{ supervision_json_log_file }}",
+        ncsession_json_log_file="{{ ncsession_json_log_file }}",
+        software_json_log_file="{{ software_json_log_file }}",
+        supervision_reply_json_log_file="{{ supervision_reply_json_log_file }}"
     )
-  while True:
+    threads = []
+
     try:
         nc.connect("{{ netaddr.IPAddress(vtap.gateway) }}", 830, "oranuser", "oranpassword")
         nc.subscribe()
-        while True:
-          nc.get_notification()
+
+        notification_thread = threading.Thread(target=get_notification_continuously, args=(nc,))
+        supervision_thread = threading.Thread(target=run_supervision_reset_continuously, args=(nc,))
+
+        threads.append(notification_thread)
+        threads.append(supervision_thread)
+
+        for thread in threads:
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+
     except Exception as e:
         nc.logger.debug('Got exception, waiting 10 seconds before reconnecting...')
         nc.logger.debug(e)
