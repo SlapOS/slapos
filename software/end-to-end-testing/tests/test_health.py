4a5982869fb6884928b9c9db98d70a61e2b96287
lu.xu <lu.xu@nexedi.com>
2023-12-12 16:47:30 +0100

    wip

--- software/end-to-end-testing/tests/test_health.py
+++ software/end-to-end-testing/tests/test_health.py
@@ -1,21 +1,21 @@
 import slapos.testing.e2e as e2e
+import time
 
 class HealthTest(e2e.EndToEndTestCase):
-  instance_name = e2e.time.strftime('e2e-test-health-%Y-%B-%d-%H:%M:%S')
 
   @classmethod
-  def setupInstance(cls):
-    parameter_dict = {}
-    json_in_xml_parameters = {'_': json.dumps(parameter_dict)}
-    cls.partition = cls.request(
-      cls.product.slapmonitor,
-      cls.instance_name,
-      partition_parameter_kw=json_in_xml_parameters,
-      software_type='default'
-    )
-    cls.waitUntilGreen(instance_name)
-    cls.connection_dict = cls.getInstanceInfos(instance_name).connection_dict
-
   def test_health_promise_feed(self):
-    resp, url = self.waitUntilMonitorURLReady(instance_name=self.instance_name)
+    instance_name = e2e.time.strftime('e2e-test-health-%Y-%B-%d-%H:%M:%S')
+    product = self.product.slapmonitor
+    parameter_dict = {}
+    self.request(
+      self.product.slapmonitor,
+      instance_name,
+      software_type='default',
+      filter_kw = {"computer_guid": "COMP-4057"}
+    )
+    self.waitUntilGreen(instance_name)
+    self.connection_dict = self.getInstanceInfos(instance_name).connection_dict
+
+    resp, url = self.waitUntilMonitorURLReady(instance_name=instance_name)
     self.getMonitorPromises(resp.content)
