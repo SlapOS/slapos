daaec4639cdce08e34257e39fb72b564eb1ff0b2
Kirill Smelkov <kirr@nexedi.com>
2023-12-27 17:30:07 +0100

    .

--- software/ors-amarisoft/test/test.sh
+++ software/ors-amarisoft/test/test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -ex
 
+(cd .. && /usr/bin/python3 ../../update-hash)
+
 export SLAPOS_TEST_DEBUG=1
 export SLAPOS_TEST_VERBOSE=0
 export SLAPOS_TEST_SKIP_SOFTWARE_CHECK=1 
-export SLAPOS_TEST_SKIP_SOFTWARE_REBUILD=1
+export SLAPOS_TEST_SKIP_SOFTWARE_REBUILD=0
+export SLAPOS_TEST_LOG_DIRECTORY=`pwd`/snapshot
 time ../k/kpython_for_test -m unittest discover -v -k TestENBParameters
