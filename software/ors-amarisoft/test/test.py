0c1199dccd5d27acf7967b45617da5750ad7d2ba
Kirill Smelkov <kirr@nexedi.com>
2023-12-27 13:06:00 +0100

    .

--- software/ors-amarisoft/test/test.py
+++ software/ors-amarisoft/test/test.py
@@ -30,3 +30,69 @@ setUpModule, ORSTestCase = makeModuleSetUpAndTestCaseClass(
 # XXX uesim - {sdr,lopcomm,sunwave}·2
 
 # XXX core-network - skip - verified by ors
+
+"""
+class TestUELTEParameters(ORSTestCase):     # XXX adjust
+    @classmethod
+    def getInstanceParameterDict(cls):
+        return {'_': json.dumps(param_dict)}
+    @classmethod
+    def getInstanceSoftwareType(cls):
+        return "ue-lte"
+    def test_ue_lte_conf(self):
+        conf_file = glob.glob(os.path.join(
+          self.slap.instance_directory, '*', 'etc', 'ue.cfg'))[0]
+
+        with open(conf_file, 'r') as f:
+          conf = yaml.load(f)
+        self.assertEqual(conf['cell_groups'][0]['cells'][0]['dl_earfcn'], param_dict['dl_earfcn'])
+        self.assertEqual(conf['cell_groups'][0]['cells'][0]['n_antenna_dl'], param_dict['n_antenna_dl'])
+        self.assertEqual(conf['cell_groups'][0]['cells'][0]['n_antenna_ul'], param_dict['n_antenna_ul'])
+        self.assertEqual(conf['ue_list'][0]['rue_addr'], param_dict['rue_addr'])
+        self.assertEqual(conf['ue_list'][0]['imsi'], param_dict['imsi'])
+        self.assertEqual(conf['ue_list'][0]['K'], param_dict['k'])
+        self.assertEqual(conf['ue_list'][0]['sim_algo'], param_dict['sim_algo'])
+        self.assertEqual(conf['ue_list'][0]['opc'], param_dict['opc'])
+        self.assertEqual(conf['ue_list'][0]['amf'], int(param_dict['amf'], 16))
+        self.assertEqual(conf['ue_list'][0]['sqn'], param_dict['sqn'])
+        self.assertEqual(conf['ue_list'][0]['impu'], param_dict['impu'])
+        self.assertEqual(conf['ue_list'][0]['impi'], param_dict['impi'])
+        self.assertEqual(conf['tx_gain'], param_dict['tx_gain'])
+        self.assertEqual(conf['rx_gain'], param_dict['rx_gain'])
+
+        with open(conf_file, 'r') as f:
+            for l in f:
+                if l.startswith('#define N_RB_DL'):
+                    self.assertIn('50', l)
+
+class TestUENRParameters(ORSTestCase):      # XXX adjust
+    @classmethod
+    def getInstanceParameterDict(cls):
+        return {'_': json.dumps(param_dict)}
+    @classmethod
+    def getInstanceSoftwareType(cls):
+        return "ue-nr"
+    def test_ue_nr_conf(self):
+        conf_file = glob.glob(os.path.join(
+          self.slap.instance_directory, '*', 'etc', 'ue.cfg'))[0]
+
+        with open(conf_file, 'r') as f:
+          conf = yaml.load(f)
+        self.assertEqual(conf['cell_groups'][0]['cells'][0]['ssb_nr_arfcn'], param_dict['ssb_nr_arfcn'])
+        self.assertEqual(conf['cell_groups'][0]['cells'][0]['dl_nr_arfcn'], param_dict['dl_nr_arfcn'])
+        self.assertEqual(conf['cell_groups'][0]['cells'][0]['bandwidth'], '10 MHz')
+        self.assertEqual(conf['cell_groups'][0]['cells'][0]['band'], param_dict['nr_band'])
+        self.assertEqual(conf['cell_groups'][0]['cells'][0]['n_antenna_dl'], param_dict['n_antenna_dl'])
+        self.assertEqual(conf['cell_groups'][0]['cells'][0]['n_antenna_ul'], param_dict['n_antenna_ul'])
+        self.assertEqual(conf['ue_list'][0]['rue_addr'], param_dict['rue_addr'])
+        self.assertEqual(conf['ue_list'][0]['imsi'], param_dict['imsi'])
+        self.assertEqual(conf['ue_list'][0]['K'], param_dict['k'])
+        self.assertEqual(conf['ue_list'][0]['sim_algo'], param_dict['sim_algo'])
+        self.assertEqual(conf['ue_list'][0]['opc'], param_dict['opc'])
+        self.assertEqual(conf['ue_list'][0]['amf'], int(param_dict['amf'], 16))
+        self.assertEqual(conf['ue_list'][0]['sqn'], param_dict['sqn'])
+        self.assertEqual(conf['ue_list'][0]['impu'], param_dict['impu'])
+        self.assertEqual(conf['ue_list'][0]['impi'], param_dict['impi'])
+        self.assertEqual(conf['tx_gain'], param_dict['tx_gain'])
+        self.assertEqual(conf['rx_gain'], param_dict['rx_gain'])
+"""
