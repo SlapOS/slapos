c3bb74755c588a5c7175e6c2a9dbee4fd3407ccc
Kirill Smelkov <kirr@nexedi.com>
2024-01-09 15:13:31 +0100

    .

--- software/ors-amarisoft/test/test.sh
+++ software/ors-amarisoft/test/test.sh
@@ -5,7 +5,7 @@
 export SLAPOS_TEST_DEBUG=1
 export SLAPOS_TEST_VERBOSE=1
 export SLAPOS_TEST_SKIP_SOFTWARE_CHECK=1 
-export SLAPOS_TEST_SKIP_SOFTWARE_REBUILD=0
+export SLAPOS_TEST_SKIP_SOFTWARE_REBUILD=1
 
 rm -rf snapshot
 mkdir snapshot
@@ -18,5 +18,5 @@ export SLAPOS_TEST_LOG_DIRECTORY=`pwd`/snapshot
 #time ../k/kpython_for_test -m unittest discover -vf -k SimCard
 #time ../k/kpython_for_test -m unittest discover -vf
 #time ../k/kpython_for_test -m unittest discover -vf -k TestENB_CPRI
-time ../k/kpython_for_test -m unittest discover -vf -k TestENB_SDR
-#time ../k/kpython_for_test -m unittest discover -vf -k TestENB_Lopcomm
+#time ../k/kpython_for_test -m unittest discover -vf -k TestENB_SDR
+time ../k/kpython_for_test -m unittest discover -vf -k TestENB_Lopcomm
