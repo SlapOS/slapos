4a7caca885f13a67f167b5828369f07270d21f4a
Jérome Perrin <jerome@nexedi.com>
2024-01-15 10:45:22 +0900

    XXX 💥 publish dev slapos.cookbook

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '1.0.329'
+version = '1.0.330-dev'
 name = 'slapos.cookbook'
 long_description = open("README.rst").read()
 
