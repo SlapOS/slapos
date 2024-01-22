d12afaffcf0474e23aa68bc5af18def59c16c640
Jérome Perrin <jerome@nexedi.com>
2024-01-22 13:22:03 +0900

    Release slapos.cookbook (1.0.351)

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '1.0.329'
+version = '1.0.351'
 name = 'slapos.cookbook'
 long_description = open("README.rst").read()
 
