6a72dc27a6c673c2668209e662e8696e09be2768
Carlos Ramos Carreño <carlos.ramos@nexedi.com>
2024-07-10 21:50:55 +0900

    Add integration tests for GDAL.
    GDAL is a component used for a particular client and without a
    corresponding software release, so its compilation was not being tested
    in SlapOS software release tests.
    
    This commit adds GDAL to the list of tested software, so that it is not
    broken by accident.

---
+++ component/gdal/test/test.py
@@ -0,0 +1,54 @@
+##############################################################################
+#
+# Copyright (c) 2022 Nexedi SA and Contributors. All Rights Reserved.
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsibility of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# guarantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+import os
+from pathlib import Path
+import subprocess
+from typing import ClassVar
+
+from slapos.grid.utils import getPythonExecutableFromSoftwarePath, md5digest
+from slapos.testing.testcase import makeModuleSetUpAndTestCaseClass
+
+setUpModule, SlapOSInstanceTestCase = makeModuleSetUpAndTestCaseClass(
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), '..', 'test.cfg')
+    )
+)
+
+
+class GDALTestCase(SlapOSInstanceTestCase):
+    """Tests for the GDAL component."""
+
+    request_instance: ClassVar[bool] = False
+
+    def test_import(self) -> None:
+        """Test that the GDAL Python module is importable."""
+
+        software_path = Path(self.slap.software_directory) / md5digest(self.getSoftwareURL())
+        python_exe = software_path / "bin/python"
+
+        subprocess.check_call([python_exe, "-c", "import osgeo.gdal"])
