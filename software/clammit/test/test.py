8392651b81c9fd18a376840156ed2016dcb6ebfc
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2024-09-19 18:07:33 +0900

    fixup! clammit: new software release

--- software/clammit/test/test.py
+++ software/clammit/test/test.py
@@ -78,7 +78,7 @@ class ClammitTestCase(SlapOSInstanceTestCase):
 
     r = requests.post(
       self.connection_parameters['scan-url'],
-      files={'file': b'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'},
+      files={'file': b'X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'},
       verify=self.ca_cert,
     )
     self.assertEqual(r.status_code, 418)
