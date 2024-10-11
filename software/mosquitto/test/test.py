3c76dd8a9f9c17e1e5045a8c7ecaeecd7becb395
Jérome Perrin <jerome@nexedi.com>
2024-10-11 11:31:06 +0900

    software/mosquitto/test: fix flaky test
    This test is using two connection one with a client to subscribe to a
    topic and wait for message and another one with publish.single to
    publish to the topic.
    The test was failing from time to time because the publish might have
    happened after the client was subscribed.
    
    Refactor the test to use `loop` on the client to have more control
    and be able to wait for the client to be subscribed using the
    `on_subscribe` callback.
    
    The test is also factorized, instead of having the same test twice for
    IPv4 and IPv6, we pass the host as parameter.

--- software/mosquitto/test/test.py
+++ software/mosquitto/test/test.py
@@ -1,30 +1,26 @@
 import os
-import time
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 from slapos.testing.testcase import makeModuleSetUpAndTestCaseClass
 
 setUpModule, SlapOSInstanceTestCase = makeModuleSetUpAndTestCaseClass(
-    os.path.abspath(
-        os.path.join(os.path.dirname(__file__), '..', 'software.cfg')))
+  os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "software.cfg"))
+)
+
 
 class TestMosquitto(SlapOSInstanceTestCase):
-
   """
   Test if mosquitto service can publish and subscribe
   to specific topics with custom authentication ...
   """
 
-  def on_connect(self, client, userdata, flags, rc):
-    client.subscribe("test")
-    self.code = rc
+  def test_ipv4(self):
+    self._test(self.computer_partition.getConnectionParameterDict()["ipv4"])
 
-  def on_message(self, client, userdata, msg):
-    self.topic = msg.topic
-    self.payload = str(msg.payload.decode())
+  def test_ipv6(self):
+    self._test(self.computer_partition.getConnectionParameterDict()["ipv6"])
 
-  def test_topic_ipv4(self):
-    host = self.computer_partition.getConnectionParameterDict()["ipv4"]
+  def _test(self, host):
     username = self.computer_partition.getConnectionParameterDict()["username"]
     password = self.computer_partition.getConnectionParameterDict()["password"]
 
@@ -32,109 +28,45 @@ class TestMosquitto(SlapOSInstanceTestCase):
     payload = "Hello, World!"
 
     client = mqtt.Client()
-    client.on_connect = self.on_connect
-    client.on_message = self.on_message
-    client.username_pw_set(username=f"{username}", password=f"{password}")
-    client.connect(f"{host}", 1883, 10)
+    client.enable_logger(self.logger)
 
-    client.loop_start()
+    def on_connect(client, userdata, flags, rc):
+      client.subscribe(topic)
+      self.code = rc
+
+    client.on_connect = on_connect
+
+    def on_subscribe(client, userdata, mid, granted_qos, properties=None):
+      self.subscribed = True
+
+    client.on_subscribe = on_subscribe
+
+    def on_message(client, userdata, msg):
+      self.topic = msg.topic
+      self.payload = str(msg.payload.decode())
+
+    client.on_message = on_message
+
+    client.username_pw_set(username=username, password=password)
+    client.connect(host, 1883, 10)
+    self.subscribed = False  # will be set by on_subscribe
+    for _ in range(100):
+      client.loop()
+      if self.subscribed:
+        break
 
     publish.single(
       topic=topic,
       payload=payload,
-      hostname=f"{host}",
-      auth={ "username": f"{username}", "password": f"{password}" }
+      hostname=host,
+      auth={"username": username, "password": password},
     )
-
-    time.sleep(10)
-    client.loop_stop()
+    self.topic = None  # will be set by on_message
+    for _ in range(100):
+      client.loop()
+      if self.topic is not None:
+        break
 
     self.assertEqual(self.code, 0)
     self.assertEqual(self.topic, topic)
-
-  def test_payload_ipv4(self):
-    host = self.computer_partition.getConnectionParameterDict()["ipv4"]
-    username = self.computer_partition.getConnectionParameterDict()["username"]
-    password = self.computer_partition.getConnectionParameterDict()["password"]
-
-    topic = "test"
-    payload = "Hello, World!"
-
-    client = mqtt.Client()
-    client.on_connect = self.on_connect
-    client.on_message = self.on_message
-    client.username_pw_set(username=f"{username}", password=f"{password}")
-    client.connect(f"{host}", 1883, 10)
-
-    client.loop_start()
-
-    publish.single(
-      topic=topic,
-      payload=payload,
-      hostname=f"{host}",
-      auth={ "username": f"{username}", "password": f"{password}" }
-    )
-
-    time.sleep(10)
-    client.loop_stop()
-
-    self.assertEqual(self.code, 0)
-    self.assertEqual(self.payload, payload)
-
-  def test_topic_ipv6(self):
-    host = self.computer_partition.getConnectionParameterDict()["ipv6"]
-    username = self.computer_partition.getConnectionParameterDict()["username"]
-    password = self.computer_partition.getConnectionParameterDict()["password"]
-
-    topic = "test"
-    payload = "Hello, World!"
-
-    client = mqtt.Client()
-    client.on_connect = self.on_connect
-    client.on_message = self.on_message
-    client.username_pw_set(username=f"{username}", password=f"{password}")
-    client.connect(f"{host}", 1883, 10)
-
-    client.loop_start()
-
-    publish.single(
-      topic=topic,
-      payload=payload,
-      hostname=f"{host}",
-      auth={ "username": f"{username}", "password": f"{password}" }
-    )
-
-    time.sleep(10)
-    client.loop_stop()
-
-    self.assertEqual(self.code, 0)
-    self.assertEqual(self.topic, topic)
-
-  def test_payload_ipv6(self):
-    host = self.computer_partition.getConnectionParameterDict()["ipv6"]
-    username = self.computer_partition.getConnectionParameterDict()["username"]
-    password = self.computer_partition.getConnectionParameterDict()["password"]
-
-    topic = "test"
-    payload = "Hello, World!"
-
-    client = mqtt.Client()
-    client.on_connect = self.on_connect
-    client.on_message = self.on_message
-    client.username_pw_set(username=f"{username}", password=f"{password}")
-    client.connect(f"{host}", 1883, 10)
-
-    client.loop_start()
-
-    publish.single(
-      topic=topic,
-      payload=payload,
-      hostname=f"{host}",
-      auth={ "username": f"{username}", "password": f"{password}" }
-    )
-
-    time.sleep(10)
-    client.loop_stop()
-
-    self.assertEqual(self.code, 0)
     self.assertEqual(self.payload, payload)
