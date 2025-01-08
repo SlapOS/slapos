d4b78adc92a261dd2896c15f7a318d074ec66f91
Thomas Gambier <thomas.gambier@nexedi.com>
2025-01-08 17:13:52 +0100

    XXX: Dev version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '1.0.373'
+version = '1.0.373+tomo'
 name = 'slapos.cookbook'
 long_description = open("README.rst").read()
 
