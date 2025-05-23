79e69d655f8a4ffad3747fbee74962ac55bdba52
Jérome Perrin <jerome@nexedi.com>
2025-05-23 10:22:25 +0900

    slapos.cookbook: make bcrypt dependency optional
    This is only necessary to use bcrypt hashed passwords with
    slapos.cookbook:generate.password and nowadays bcrypt pulls rustc and
    llvm which we want want to avoid for lightweight softwares.
    
    The dependency is kept, to make sure bcrypt hashes still work, as long
    as the bcrypt dependency is installed, which will become more complex.

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ for f in sorted(glob.glob(os.path.join('slapos', 'recipe', 'README.*.rst'))):
 
 extras_require = {
     'test': (
+        'bcrypt',
         'jsonschema',
         'mock',
         'psycopg2',
@@ -72,7 +73,6 @@ setup(name=name,
         'zc.recipe.egg', # for scripts generation
         'pytz', # for timezone database
         'passlib',
-        'bcrypt',
         ],
       zip_safe=True,
       entry_points={
