f1d3a931181f5131532fa8d13a9ef4ee96057e5e
Jérome Perrin <jerome@nexedi.com>
2025-05-23 10:22:25 +0900

    slapos.cookbook: make bcrypt dependency optional
    This is only necessary to use bcrypt hashed passwords with
    slapos.cookbook:generate.password and nowadays bcrypt pulls rustc and
    llvm which we want want to avoid for lightweight softwares.
    
    The dependency is kept, to make sure bcrypt hashes still work, as long
    as the bcrypt dependency is installed, which will become more complex.

--- slapos/test/recipe/test_random.py
+++ slapos/test/recipe/test_random.py
@@ -127,6 +127,8 @@ class TestPassword(unittest.TestCase):
       passlib.hash.md5_crypt.verify(
         self.buildout['random']['passwd'], hashed))
 
+    # note: bcrypt passwords are only available if bcrypt python
+    # package is installed and made available to buildout.
     hashed = self.buildout['random']['passwd-bcrypt']
     self.assertTrue(
       passlib.hash.bcrypt.verify(
