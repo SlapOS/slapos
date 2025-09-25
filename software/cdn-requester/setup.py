dc87592b6adc53b8ede4aeec62fdf62e6ec86e1b
Cédric Le Ninivin <cedric.leninivin@nexedi.com>
2025-09-25 17:13:02 +0200

    WIP

--- software/cdn-requester/setup.py
+++ software/cdn-requester/setup.py
@@ -7,17 +7,11 @@ setup(
   install_requires=[
     'validators',
     'furl',
-    'orderedmultidict',
-    'caucase',
-    'python2-secrets',
+    'dnspython',
   ],
   entry_points={
     'zc.buildout': [
       'default = software:Recipe',
-    ],
-    'console_scripts': [
-      'smart-caucase-signer = software:smart_sign',
-      'caucase-csr-sign-check = software:caucase_csr_sign_check'
     ]
   }
 )
