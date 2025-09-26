cfda6ba6ad775188f06e8ab42a73f47135aeabcb
Xavier Thompson <xavier.thompson@nexedi.com>
2025-09-26 16:27:00 +0300

    Release slapos.cookbook (1.0.438)

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '1.0.431'
+version = '1.0.438'
 name = 'slapos.cookbook'
 long_description = open("README.rst").read()
 
