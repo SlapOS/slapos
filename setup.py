b10ed53c5501162ec14eecfb2ea6e7eadde7146a
Cédric Le Ninivin <cedric.leninivin@nexedi.com>
2026-03-17 13:48:59 +0100

    slapos.cookbook: version up to 1.0.468.dev0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '1.0.467'
+version = '1.0.468.dev0'
 name = 'slapos.cookbook'
 long_description = open("README.rst").read()
 
