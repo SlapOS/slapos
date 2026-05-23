b26c75062ac44f27fb0d77c657afc52a2de6be1c
Jérome Perrin <jerome@nexedi.com>
2026-05-23 13:24:52 +0900

    slapos/instancenode: fix some SyntaxError on python2
    ======================================================================
    ERROR: test_cdninstancenode (unittest.loader.ModuleImportFailure)
    ----------------------------------------------------------------------
    ImportError: Failed to import test module: test_cdninstancenode
    Traceback (most recent call last):
      File "/srv/slapgrid/slappart9/srv/shared/python2.7/dc0bb5ba81bfae65fc621c98b3fb93ec/lib/python2.7/unittest/loader.py", line 254, in _find_tests
        module = self._get_module_from_name(name)
      File "/srv/slapgrid/slappart9/srv/shared/python2.7/dc0bb5ba81bfae65fc621c98b3fb93ec/lib/python2.7/unittest/loader.py", line 232, in _get_module_from_name
        __import__(name)
      File "/srv/slapgrid/slappart9/t/euo/i/0/parts/slapos.cookbook/slapos/test/recipe/test_cdninstancenode.py", line 614
    SyntaxError: Non-ASCII character '\xe2' in file /srv/slapgrid/slappart9/t/euo/i/0/parts/slapos.cookbook/slapos/test/recipe/test_cdninstancenode.py on line 614, but no encoding declared; see http://python.org/dev/peps/pep-0263/ for details
    
    ======================================================================
    ERROR: test_instancenode (unittest.loader.ModuleImportFailure)
    ----------------------------------------------------------------------
    ImportError: Failed to import test module: test_instancenode
    Traceback (most recent call last):
      File "/srv/slapgrid/slappart9/srv/shared/python2.7/dc0bb5ba81bfae65fc621c98b3fb93ec/lib/python2.7/unittest/loader.py", line 254, in _find_tests
        module = self._get_module_from_name(name)
      File "/srv/slapgrid/slappart9/srv/shared/python2.7/dc0bb5ba81bfae65fc621c98b3fb93ec/lib/python2.7/unittest/loader.py", line 232, in _get_module_from_name
        __import__(name)
      File "/srv/slapgrid/slappart9/t/euo/i/0/parts/slapos.cookbook/slapos/test/recipe/test_instancenode.py", line 1173
    SyntaxError: Non-ASCII character '\xe2' in file /srv/slapgrid/slappart9/t/euo/i/0/parts/slapos.cookbook/slapos/test/recipe/test_instancenode.py on line 1173, but no encoding declared; see http://python.org/dev/peps/pep-0263/ for details
    
    ======================================================================
    ERROR: test_cdninstancenode (unittest.loader.ModuleImportFailure)
    ----------------------------------------------------------------------
    ImportError: Failed to import test module: test_cdninstancenode
    Traceback (most recent call last):
      File "/srv/slapgrid/slappart62/srv/shared/python2.7/b1476e6b88146831a8f55d6e881ef725/lib/python2.7/unittest/loader.py", line 254, in _find_tests
        module = self._get_module_from_name(name)
      File "/srv/slapgrid/slappart62/srv/shared/python2.7/b1476e6b88146831a8f55d6e881ef725/lib/python2.7/unittest/loader.py", line 232, in _get_module_from_name
        __import__(name)
      File "/srv/slapgrid/slappart62/t/eun/i/0/parts/slapos.cookbook/slapos/test/recipe/test_cdninstancenode.py", line 1684
        **self.options,
         ^
    SyntaxError: invalid syntax

--- slapos/test/recipe/test_cdninstancenode.py
+++ slapos/test/recipe/test_cdninstancenode.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import unittest
 import mock
 from slapos.recipe import cdninstancenode
@@ -1679,10 +1680,10 @@ class TestRecipe(unittest.TestCase):
 
   def test_validate_ssl_certificate_valid(self):
     """Test validation accepts valid SSL certificates"""
-    recipe = cdninstancenode.Recipe(self.buildout, 'test', {
-      **self.options,
-      'openssl-binary': '/usr/bin/openssl'
-    })
+    recipe = cdninstancenode.Recipe(self.buildout, 'test', dict(
+      self.options,
+      **{'openssl-binary': '/usr/bin/openssl'})
+    )
 
     valid_cert = """-----BEGIN CERTIFICATE-----
 MIIDXTCCAkWgAwIBAgIJAKL2Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z
@@ -1706,10 +1707,10 @@ MIIDXTCCAkWgAwIBAgIJAKL2Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z
 
   def test_validate_ssl_certificate_invalid(self):
     """Test validation fails for invalid SSL certificates"""
-    recipe = cdninstancenode.Recipe(self.buildout, 'test', {
-      **self.options,
-      'openssl-binary': '/usr/bin/openssl'
-    })
+    recipe = cdninstancenode.Recipe(self.buildout, 'test',  dict(
+      self.options,
+      **{'openssl-binary': '/usr/bin/openssl'})
+    )
 
     # Invalid certificate content
     invalid_cert = "not a valid certificate"
@@ -1730,10 +1731,10 @@ MIIDXTCCAkWgAwIBAgIJAKL2Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z
 
   def test_validate_ssl_key_cert_match(self):
     """Test validation checks SSL key and certificate match"""
-    recipe = cdninstancenode.Recipe(self.buildout, 'test', {
-      **self.options,
-      'openssl-binary': '/usr/bin/openssl'
-    })
+    recipe = cdninstancenode.Recipe(self.buildout, 'test',  dict(
+      self.options,
+      **{'openssl-binary': '/usr/bin/openssl'})
+    )
 
     # Mock matching moduli (key and cert match)
     matching_modulus = b'Modulus=ABCD1234\n'
@@ -1761,10 +1762,10 @@ MIIDXTCCAkWgAwIBAgIJAKL2Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z
 
   def test_validate_ssl_key_cert_mismatch(self):
     """Test validation fails when SSL key and certificate don't match"""
-    recipe = cdninstancenode.Recipe(self.buildout, 'test', {
-      **self.options,
-      'openssl-binary': '/usr/bin/openssl'
-    })
+    recipe = cdninstancenode.Recipe(self.buildout, 'test',  dict(
+      self.options,
+      **{'openssl-binary': '/usr/bin/openssl'})
+    )
 
     parameters = {
       'ssl_key': '-----BEGIN PRIVATE KEY-----\ntest key\n-----END PRIVATE KEY-----',
