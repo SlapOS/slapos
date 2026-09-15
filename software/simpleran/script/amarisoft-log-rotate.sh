6a08f9a711538bbb4d226ebed96ac28cc417a045
Joanne Hugé <joanne.huge@nexedi.com>
2026-09-15 08:17:57 +0000

    simpleran: add service to trim logs
    Previously this was done before launching lteenb before,
    which prevented logs to be deleted when lteenb would run
    for a long time without parameter change

--- software/simpleran/script/amarisoft-log-rotate.sh
+++ software/simpleran/script/amarisoft-log-rotate.sh
@@ -12,6 +12,7 @@ Required parameters:
  MAX_COMBINED_LOG_KB_SIZE         Max combined size of all Amarisoft logs, in kB
  MAX_COMBINED_STDOUT_LOG_KB_SIZE  Max combined size of all Amarisoft stdout logs, in kB
  MAX_STDOUT_LOG_KB_SIZE           Max individual size of Amarisoft stdout logs
+ MAX_LOG_FILE_NUMBER              Max number of Amarisoft logs
 ENDUSAGE
 1>&2;
 }
@@ -22,39 +23,54 @@ STDOUT_LOG=$3
 MAX_COMBINED_LOG_KB_SIZE=$4
 MAX_COMBINED_STDOUT_LOG_KB_SIZE=$5
 MAX_STDOUT_LOG_KB_SIZE=$6
+MAX_LOG_FILE_NUMBER=$7
+MAX_LOG_FILE_NUMBER=$((MAX_LOG_FILE_NUMBER/2))
 
-if [ $# -ne 6 ] ; then
+if [ $# -ne 7 ] ; then
   usage ; exit 1
 fi
 
-# Archive current log 
-stat $START_DATE && mv $LOG $LOG.$(cat $START_DATE)
+set -x
 
-# Split stdout log into smaller chunks
-if test $(du $STDOUT_LOG | cut -f1) -ge $MAX_STDOUT_LOG_KB_SIZE ; then
-  stat $START_DATE || date +"%Y%m%d.%T" > $START_DATE
-  head -c -$(($MAX_STDOUT_LOG_KB_SIZE/2))k $STDOUT_LOG > $STDOUT_LOG.$(cat $START_DATE)
-  tail -c  $(($MAX_STDOUT_LOG_KB_SIZE/2))k $STDOUT_LOG > $STDOUT_LOG.tmp
-  mv $STDOUT_LOG.tmp $STDOUT_LOG
-fi
+while true
+do {
+  # Split stdout log into smaller chunks
+  if test $(du $STDOUT_LOG | cut -f1) -ge $MAX_STDOUT_LOG_KB_SIZE ; then
+    stat $START_DATE > /dev/null 2>&1 || date +"%Y%m%d.%T" > $START_DATE
+    head -c -$(($MAX_STDOUT_LOG_KB_SIZE/2))k $STDOUT_LOG > $STDOUT_LOG.$(cat $START_DATE)
+    tail -c  $(($MAX_STDOUT_LOG_KB_SIZE/2))k $STDOUT_LOG > $STDOUT_LOG.tmp
+    mv $STDOUT_LOG.tmp $STDOUT_LOG
+    date +"%Y%m%d.%T" > $START_DATE
+  fi
 
-# Remove almost empty enb radio archive log files
-rm -f $(find $(dirname $LOG) -name "$(basename $LOG).*" -size -5k)
+  # Remove almost empty enb radio archive log files
+  rm -f $(find $(dirname $LOG) -name "$(basename $LOG).*" -size -5k)
 
-# Limit combined size for archived logs
-trim() {
-  stat $2* || return
-  i=-1
-  N=$(ls -1t $2* | wc -l)
-  TOTAL=$1
-  while [ $TOTAL -ge $1 ] && [ $((N-i)) -ge 1 ] ; do
-    i=$((i+1))
-    TOTAL=$(du -c $(ls -1t $2* | head -n$((N-i))) | tail -n1 | cut -f1)
-  done
-  rm -f $(ls -1t $2* | tail -n$i)
+  # Make sure there are not too much logs
+  LOG_COUNT=$(ls $LOG.* | wc -l)
+  if [ $LOG_COUNT -ge $MAX_LOG_FILE_NUMBER ] ; then
+    rm -f $(ls -1t $LOG.* | tail -n $((LOG_COUNT-MAX_LOG_FILE_NUMBER)))
+  fi
+  LOG_COUNT=$(ls $STDOUT_LOG.* | wc -l)
+  if [ $LOG_COUNT -ge $MAX_LOG_FILE_NUMBER ] ; then
+    rm -f $(ls -1t $STDOUT_LOG.* | tail -n $((LOG_COUNT-MAX_LOG_FILE_NUMBER)))
+  fi
+
+  # Limit combined size for archived logs
+  trim() {
+    stat > /dev/null 2>&1 $2* || return
+    i=-1
+    N=$(ls -1t $2* | wc -l)
+    TOTAL=$1
+    while [ $TOTAL -ge $1 ] && [ $((N-i)) -ge 1 ] ; do
+      i=$((i+1))
+      TOTAL=$(du -c $(ls -1t $2* | head -n$((N-i))) | tail -n1 | cut -f1)
+    done
+    rm -f $(ls -1t $2* | tail -n$i)
+  }
+  trim $MAX_COMBINED_LOG_KB_SIZE $LOG.
+  trim $MAX_COMBINED_STDOUT_LOG_KB_SIZE  $STDOUT_LOG.
+
+  sleep 1
 }
-trim $MAX_COMBINED_LOG_KB_SIZE $LOG.
-trim $MAX_COMBINED_STDOUT_LOG_KB_SIZE  $STDOUT_LOG.
-
-# Add start date
-date +"%Y%m%d.%T" > $START_DATE
+done
